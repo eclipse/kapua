f04fc2faa001579de7a7cfe65cbf40cce989bb06
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2016-10-21 14:07:30 +0200

    Fixed XML unmarshalling from console
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- console/src/main/java/org/eclipse/kapua/app/console/ConsoleJAXBContextProvider.java
+++ console/src/main/java/org/eclipse/kapua/app/console/ConsoleJAXBContextProvider.java
@@ -62,7 +62,6 @@ public class ConsoleJAXBContextProvider implements JAXBContextProvider {
                         KapuaTicon.class,
                         KapuaToption.class,
                         TscalarImpl.class,
-                        KapuaTscalar.class,
                         MetatypeXmlRegistry.class
                 }, null);
             }
