d564e1c10afa5a4c0a5104a4bca190a69a4c1861
Benjamin Cabé <benjamin@eclipse.org>
2016-11-04 10:35:40 +0100

    Bump Apache Tomcat version to 8.0.38 - fixing issue #76
    Signed-off-by: Benjamin Cabé <benjamin@eclipse.org>

--- dev-tools/src/main/bin/start-demo.sh
+++ dev-tools/src/main/bin/start-demo.sh
@@ -1,6 +1,6 @@
 export KAPUA_GIT_ROOT="$(pwd)"
 export ACTIVEMQ_VERSION=5.14.0
-export TOMCAT_VERSION=8.0.37
+export TOMCAT_VERSION=8.0.38
 cd ../vagrant
 echo 'creating base box'
 ./start.sh base-box
@@ -9,7 +9,7 @@ echo 'creating demo vagrant machine'
 cd $KAPUA_GIT_ROOT/../../../../
 mvn clean install -Psql -Pdeploy
 cd dev-tools/src/main/vagrant
-vagrant ssh -c "nohup /usr/local/kapua/apache-activemq-5.14.0/bin/activemq start xbean:/usr/local/kapua/apache-activemq-5.14.0/conf/activemq.xml >/dev/null 2>&1 &
-nohup /usr/local/kapua/apache-tomcat-8.0.37/bin/startup.sh >/dev/null 2>&1 &
+vagrant ssh -c "nohup /usr/local/kapua/apache-activemq-${ACTIVEMQ_VERSION}/bin/activemq start xbean:/usr/local/kapua/apache-activemq-5.14.0/conf/activemq.xml >/dev/null 2>&1 &
+nohup /usr/local/kapua/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh >/dev/null 2>&1 &
 ps -ef | grep active
 ps -ef | grep tomcat"
