c6d202221271f63b7c3e8734728edfdddedc4cfc
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2016-11-10 11:47:08 +0100

    Layout improvements
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- console-v2/src/layout/controllers/LayoutCtrl.ts
+++ console-v2/src/layout/controllers/LayoutCtrl.ts
@@ -4,6 +4,26 @@ export default class LayoutCtrl {
             title: "Welcome",
             iconClass: "fa fa-info",
             href: "#/welcome"
+        },
+        {
+            title: "Devices",
+            iconClass: "fa fa-hdd-o",
+            href: "#/devices"
+        },
+        {
+            title: "Users",
+            iconClass: "fa fa-users",
+            href: "#/users"
+        },
+        {
+            title: "Settings",
+            iconClass: "fa fa-cog",
+            href: "#/settings"
+        },
+        {
+            title: "Child Accounts",
+            iconClass: "fa fa-sitemap",
+            href: "#/child-accounts"
         }
     ];
 
