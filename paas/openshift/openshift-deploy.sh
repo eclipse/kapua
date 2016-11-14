de273cfcd426487edd340304ad227dacab2b35ee
Henryk Konsek <hekonsek@gmail.com>
2016-11-14 13:24:26 +0100

    Start ElasticSearch in OpenShift PaaS environment #80

---
+++ paas/openshift/openshift-deploy.sh
@@ -0,0 +1,21 @@
+###############################################################################
+# Copyright (c) 2016 Red Hat and/or its affiliates and others
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+###############################################################################
+
+#!/usr/bin/env bash
+
+echo 'Starting ElasticSearch server...'
+
+if [ -z "${ELASTIC_SEARCH_MEMORY}" ]; then
+  ELASTIC_SEARCH_MEMORY='512M'
+fi
+
+oc new-app -e ES_JAVA_OPTS="-Xms${ELASTIC_SEARCH_MEMORY} -Xmx${ELASTIC_SEARCH_MEMORY}" elasticsearch
+
+echo 'ElasticSearch server started.'
