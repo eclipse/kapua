fe0eb5ce793c4fb27db0c40b0360a391dd0505b0
Henryk Konsek <hekonsek@gmail.com>
2016-11-14 13:43:06 +0100

    ElasticSearch client should attempt to discover connection credentials via Kubernetes service discovery #81

---
+++ service/datastore/internal/src/test/java/org/eclipse/kapua/service/datastore/internal/elasticsearch/EsClientTest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Red Hat and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.kapua.service.datastore.internal.elasticsearch;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.junit.Test;
+
+import java.net.UnknownHostException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EsClientTest {
+
+    @Test
+    public void elascticSearchClientShouldDefaultToLocalhost() throws EsDatastoreException, UnknownHostException {
+        // When
+        TransportClient client = (TransportClient) EsClient.getcurrent();
+
+        // Then
+        String host = client.listedNodes().get(0).address().getHost();
+        assertThat(host).isEqualTo("127.0.0.1");
+    }
+
+}
