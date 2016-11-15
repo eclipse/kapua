2d864e88bb6a80cee7f8351ce63bbba416b4f7d6
Henryk Konsek <hekonsek@gmail.com>
2016-11-15 15:32:10 +0100

    Add H2 server to OpenShift deploy script [#84]

--- paas/openshift/openshift-deploy.sh
+++ paas/openshift/openshift-deploy.sh
@@ -17,5 +17,6 @@ if [ -z "${ELASTIC_SEARCH_MEMORY}" ]; then
 fi
 
 oc new-app -e ES_JAVA_OPTS="-Xms${ELASTIC_SEARCH_MEMORY} -Xmx${ELASTIC_SEARCH_MEMORY}" elasticsearch
+oc new-app hekonsek/h2
 
 echo 'ElasticSearch server started.'
