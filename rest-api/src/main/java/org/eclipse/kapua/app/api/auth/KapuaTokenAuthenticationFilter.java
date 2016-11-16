5b0d60da34d5ef0c942e8a01213db0b685c2c37c
coduz <alberto.codutti@eurotech.com>
2016-11-16 15:24:49 +0100

    Added support in REST API for new authentication flow for Kapua
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ rest-api/src/main/java/org/eclipse/kapua/app/api/auth/KapuaTokenAuthenticationFilter.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.app.api.auth;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.apache.shiro.web.util.WebUtils;
+import org.eclipse.kapua.app.api.settings.KapuaApiSetting;
+import org.eclipse.kapua.app.api.settings.KapuaApiSettingKeys;
+import org.eclipse.kapua.locator.KapuaLocator;
+import org.eclipse.kapua.service.authentication.AccessTokenCredentials;
+import org.eclipse.kapua.service.authentication.CredentialsFactory;
+
+public class KapuaTokenAuthenticationFilter extends AuthenticatingFilter {
+
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) {
+
+        //
+        // Extract token
+        KapuaApiSetting settings = KapuaApiSetting.getInstance();
+        String accessTokenHeaderName = settings.getString(KapuaApiSettingKeys.API_AUTHENTICATION_HEADER_ACCESS_TOKEN_NAME);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String tokenId = httpRequest.getHeader(accessTokenHeaderName);
+
+        //
+        // Build AccessToken for Shiro Auth
+        KapuaLocator locator = KapuaLocator.getInstance();
+        CredentialsFactory credentialsFactory = locator.getFactory(CredentialsFactory.class);
+        AccessTokenCredentials accessTokenCredentials = credentialsFactory.newAccessTokenCredentials(tokenId);
+
+        //
+        // Return token
+        return (AuthenticationToken) accessTokenCredentials;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        // if (log.isDebugEnabled()) {
+        // log.debug("Authentication required: sending 401 Authentication challenge response.");
+        // }
+        HttpServletResponse httpResponse = WebUtils.toHttp(response);
+        httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        // String authcHeader = getAuthcScheme() + " realm=\"" + getApplicationName() + "\"";
+        // httpResponse.setHeader(AUTHENTICATE_HEADER, authcHeader);
+        return false;
+    }
+
+}
