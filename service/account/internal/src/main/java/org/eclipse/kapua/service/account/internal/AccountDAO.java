ade565109632f33230e02f2e62dfde4d32c6d2ab
coduz <alberto.codutti@eurotech.com>
2016-11-16 15:21:03 +0100

    Adapted account service reference implementation to new Authentication api
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/account/internal/src/main/java/org/eclipse/kapua/service/account/internal/AccountDAO.java
+++ service/account/internal/src/main/java/org/eclipse/kapua/service/account/internal/AccountDAO.java
@@ -13,9 +13,9 @@
 package org.eclipse.kapua.service.account.internal;
 
 import org.eclipse.kapua.KapuaException;
+import org.eclipse.kapua.commons.jpa.EntityManager;
 import org.eclipse.kapua.commons.model.query.KapuaListResultImpl;
 import org.eclipse.kapua.commons.service.internal.ServiceDAO;
-import org.eclipse.kapua.commons.jpa.EntityManager;
 import org.eclipse.kapua.model.id.KapuaId;
 import org.eclipse.kapua.model.query.KapuaQuery;
 import org.eclipse.kapua.service.account.Account;
@@ -27,8 +27,7 @@ import org.eclipse.kapua.service.account.AccountCreator;
  * @since 1.0
  *
  */
-public class AccountDAO
-{
+public class AccountDAO {
 
     /**
      * Creates and return new Account
@@ -39,8 +38,7 @@ public class AccountDAO
      * @throws KapuaException
      */
     public static Account create(EntityManager em, AccountCreator accountCreator)
-        throws KapuaException
-    {
+            throws KapuaException {
         //
         // Create Account
 
@@ -57,7 +55,7 @@ public class AccountDAO
         organizationImpl.setCountry(accountCreator.getOrganizationCountry());
 
         AccountImpl accountImpl = new AccountImpl(accountCreator.getScopeId(),
-                                                  accountCreator.getName());
+                accountCreator.getName());
         accountImpl.setOrganization(organizationImpl);
 
         return ServiceDAO.create(em, accountImpl);
@@ -72,8 +70,7 @@ public class AccountDAO
      * @throws KapuaException
      */
     public static Account update(EntityManager em, Account account)
-        throws KapuaException
-    {
+            throws KapuaException {
         //
         // Update account
         AccountImpl accountImpl = (AccountImpl) account;
@@ -87,16 +84,14 @@ public class AccountDAO
      * @param em
      * @param accountId
      */
-    public static void delete(EntityManager em, KapuaId accountId)
-    {
+    public static void delete(EntityManager em, KapuaId accountId) {
         ServiceDAO.delete(em, AccountImpl.class, accountId);
     }
 
     /**
      * Finds the account by account identifier
      */
-    public static Account find(EntityManager em, KapuaId accountId)
-    {
+    public static Account find(EntityManager em, KapuaId accountId) {
         return em.find(AccountImpl.class, accountId);
     }
 
@@ -107,9 +102,8 @@ public class AccountDAO
      * @param name
      * @return
      */
-    public static Account findByName(EntityManager em, String name)
-    {
-        return ServiceDAO.findByName(em, AccountImpl.class, name);
+    public static Account findByName(EntityManager em, String name) {
+        return ServiceDAO.findByField(em, AccountImpl.class, "name", name);
     }
 
     /**
@@ -121,8 +115,7 @@ public class AccountDAO
      * @throws KapuaException
      */
     public static KapuaListResultImpl<Account> query(EntityManager em, KapuaQuery<Account> accountQuery)
-        throws KapuaException
-    {
+            throws KapuaException {
         return ServiceDAO.query(em, Account.class, AccountImpl.class, new KapuaListResultImpl<Account>(), accountQuery);
     }
 
@@ -135,8 +128,7 @@ public class AccountDAO
      * @throws KapuaException
      */
     public static long count(EntityManager em, KapuaQuery<Account> accountQuery)
-        throws KapuaException
-    {
+            throws KapuaException {
         return ServiceDAO.count(em, Account.class, AccountImpl.class, accountQuery);
     }
 
