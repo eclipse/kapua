c2c5b2b7be136db35db669fd963a6ba3d0c9952c
Dejan Bosanac <dejan@nighttale.net>
2016-11-18 17:19:22 +0100

    initial work on running kapua images in openshift

--- paas/openshift/openshift-deploy.sh
+++ paas/openshift/openshift-deploy.sh
@@ -10,6 +10,9 @@
 
 #!/usr/bin/env bash
 
+#oc login
+#oc new-project eclipse-kapua --description="Open source IoT Platform" --display-name="Eclipse Kapua"
+
 echo 'Starting ElasticSearch server...'
 
 if [ -z "${ELASTIC_SEARCH_MEMORY}" ]; then
@@ -17,6 +20,35 @@ if [ -z "${ELASTIC_SEARCH_MEMORY}" ]; then
 fi
 
 oc new-app -e ES_JAVA_OPTS="-Xms${ELASTIC_SEARCH_MEMORY} -Xmx${ELASTIC_SEARCH_MEMORY}" elasticsearch
-oc new-app hekonsek/h2 --name=sql
 
 echo 'ElasticSearch server started.'
+
+echo 'Staring SQL database'
+
+oc new-app hekonsek/h2 --name=sql
+
+echo 'SQL database started'
+
+
+## Build assembly module with
+## mvn -Pdocker
+
+##echo 'Starting broker'
+
+##oc new-app --docker-image kapua-broker:latest -name=kapua-broker -n eclipse-kapua
+
+##echo 'Broker started'
+
+##echo 'Starting web console'
+
+##oc new-app --docker-image kapua-console:latest -name=kapua-console -n eclipse-kapua
+
+##oc expose svc/kapua-console --hostname=kapua-console.com
+
+##echo 'Web console started'
+
+##echo 'Starting rest api'
+
+##oc new-app --docker-image kapua-api:latest -name=kapua-api -n eclipse-kapua
+
+##echo 'Rest api started'
