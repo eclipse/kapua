252cbec7f2c097fe6be049e37908286cab96bc88
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2016-11-25 11:27:16 +0100

    Provide updated jQuery module reference for Webpack
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- console-v2/ui/webpack.config.js
+++ console-v2/ui/webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
     },
     resolve: {
         // Add ".ts" and ".tsx" as a resolvable extension.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+        alias: {
+            "jquery": "angular-patternfly/node_modules/patternfly/node_modules/jquery"
+        }
     },
     module: {
         loaders: [
