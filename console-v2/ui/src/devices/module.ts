4c7c19ad2642757a00b5d4857ffa10225d4aba4f
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2016-12-20 14:29:09 +0100

    Added copyright headers
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- console-v2/ui/src/devices/module.ts
+++ console-v2/ui/src/devices/module.ts
@@ -1,3 +1,16 @@
+/*******************************************************************************
+* Copyright (c) 2011, 2016 Eurotech and/or its affiliates                       
+*                                                                               
+* All rights reserved. This program and the accompanying materials              
+* are made available under the terms of the Eclipse Public License v1.0         
+* which accompanies this distribution, and is available at                      
+* http://www.eclipse.org/legal/epl-v10.html                                     
+*                                                                               
+* Contributors:                                                                 
+*     Eurotech - initial API and implementation                                 
+*                                                                               
+*******************************************************************************/
+
 import DevicesListCtrl from "./controllers/DevicesListCtrl";
 import DeleteDevicesModalCtrl from "./controllers/DeleteDevicesModalCtrl";
 import DeviceDetailCtrl from "./controllers/DeviceDetailCtrl";
