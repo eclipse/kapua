65fac092dd318f06280ddaa71967e8c9c386cb0b
coduz <alberto.codutti@eurotech.com>
2016-12-20 15:17:32 +0100

    Added group service implementation
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/group/shiro/GroupPredicates.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.kapua.service.authorization.group.shiro;
+
+import org.eclipse.kapua.model.query.KapuaQuery;
+import org.eclipse.kapua.model.query.predicate.KapuaPredicate;
+
+/**
+ * {@link KapuaQuery} {@link KapuaPredicate} name for {@link Group} entity.
+ * 
+ * @since 1.0
+ * 
+ */
+public class GroupPredicates {
+
+    /**
+     * {@link Group} name
+     */
+    public static final String NAME = "name";
+
+}
