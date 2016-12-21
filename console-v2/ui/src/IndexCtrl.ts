9a587f28d32f0152de2f0d09424f5f323b89dcc9
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2016-12-21 19:38:32 +0100

    Toast Notifications on HTTP error
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- console-v2/ui/src/IndexCtrl.ts
+++ console-v2/ui/src/IndexCtrl.ts
@@ -1,4 +1,3 @@
-<!--
 /*******************************************************************************
 * Copyright (c) 2011, 2016 Eurotech and/or its affiliates                       
 *                                                                               
@@ -11,13 +10,31 @@
 *     Eurotech - initial API and implementation                                 
 *                                                                               
 *******************************************************************************/
--->
-<html ng-app="app" ng-controller="IndexCtrl as vm" ng-strict-di ng-class="{ 'layout-pf layout-pf-fixed': vm.showHeader }">
-    <head>
-        <base href="/">
-    </head>
-    <body>
-        <div pf-toast-notification-list notifications="vm.notifications" close-callback="vm.closeNotification2"></div>
-        <ui-view name="mainView"></ui-view>
-    </body>
-</html>
+export default class IndexCtrl {
+  private showHeader: boolean;
+  private notifications = [];
+  private closeNotification;
+
+  constructor(private $rootScope: angular.IRootScopeService, private Notifications) {
+    $rootScope.$on("$stateChangeSuccess", (
+      event: angular.IAngularEvent,
+      toState: angular.ui.IState,
+      toParams,
+      fromState: angular.ui.IState,
+      fromParams
+    ) => {
+      this.showHeader = toState.name.indexOf("kapua.") === 0;
+    });
+    this.notifications = Notifications.data;
+
+    // FIXME - To become a class method with PF4
+    this.closeNotification = (data) => {
+      this.Notifications.remove(data);
+    };
+  }
+
+  closeNotification2(data) {
+    console.log(this);
+    this.Notifications.remove(data);
+  }
+}
