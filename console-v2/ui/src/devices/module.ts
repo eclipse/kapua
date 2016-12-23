08745907a677c59ff32cc1477c8346d4db415510
HSL\mtomic <marko.tomic@comtrade.com>
2016-12-23 15:02:52 +0100

    Device details page - cards for bundles, commands, configurations and events added
    Signed-off-by: HSL\mtomic <marko.tomic@comtrade.com>

--- console-v2/ui/src/devices/module.ts
+++ console-v2/ui/src/devices/module.ts
@@ -15,6 +15,11 @@ import DevicesListCtrl from "./controllers/DevicesListCtrl";
 import DeleteDevicesModalCtrl from "./controllers/DeleteDevicesModalCtrl";
 import DeviceDetailCtrl from "./controllers/DeviceDetailCtrl";
 import DeviceDetailPackagesCtrl from "./controllers/DeviceDetailPackagesCtrl";
+import DeviceDetailBundlesCtrl from "./controllers/DeviceDetailBundlesCtrl";
+import DeviceDetailEventsCtrl from "./controllers/DeviceDetailEventsCtrl";
+import DeviceDetailCommandsCtrl from "./controllers/DeviceDetailCommandsCtrl";
+import DeviceDetailConfigurationsCtrl from "./controllers/DeviceDetailConfigurationsCtrl";
+
 
 import "./assets/styles/devices.scss";
 
@@ -49,9 +54,49 @@ angular.module("app.devices", [])
                             controller: "DeviceDetailPackagesCtrl as vm"
                         }
                     }
+                })
+                .state("kapua.devices.detail.bundles", {
+                    url: "/bundles",
+                    views: {
+                        "kapuaView@kapua": {
+                            template: require("./views/device-details/bundles.html"),
+                            controller: "DeviceDetailBundlesCtrl as vm"
+                        }
+                    }
+                })
+                .state("kapua.devices.detail.events", {
+                    url: "/events",
+                    views: {
+                        "kapuaView@kapua": {
+                            template: require("./views/device-details/events.html"),
+                            controller: "DeviceDetailEventsCtrl as vm"
+                        }
+                    }
+                })
+                .state("kapua.devices.detail.commands", {
+                    url: "/commands",
+                    views: {
+                        "kapuaView@kapua": {
+                            template: require("./views/device-details/commands.html"),
+                            controller: "DeviceDetailCommandsCtrl as vm"
+                        }
+                    }
+                })
+                .state("kapua.devices.detail.configurations", {
+                    url: "/configurations",
+                    views: {
+                        "kapuaView@kapua": {
+                            template: require("./views/device-details/configurations.html"),
+                            controller: "DeviceDetailConfigurationsCtrl as vm"
+                        }
+                    }
                 });
         }])
     .controller("DevicesListCtrl", ["$http", "$modal", DevicesListCtrl])
     .controller("DeleteDevicesModalCtrl", ["$modalInstance", "$http", "id", DeleteDevicesModalCtrl])
     .controller("DeviceDetailCtrl", ["$stateParams", "$http", DeviceDetailCtrl])
-    .controller("DeviceDetailPackagesCtrl", ["$stateParams", "$http", DeviceDetailPackagesCtrl]);
+    .controller("DeviceDetailPackagesCtrl", ["$stateParams", "$http", DeviceDetailPackagesCtrl])
+    .controller("DeviceDetailBundlesCtrl", ["$stateParams", "$http", DeviceDetailBundlesCtrl])
+    .controller("DeviceDetailEventsCtrl", ["$stateParams", "$http", DeviceDetailEventsCtrl])
+    .controller("DeviceDetailCommandsCtrl", ["$stateParams", "$http", DeviceDetailCommandsCtrl])
+    .controller("DeviceDetailConfigurationsCtrl", ["$stateParams", "$http", DeviceDetailConfigurationsCtrl]);
