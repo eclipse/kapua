45996376d37f8ffa6e2d4de739f61d90c02854c3
Jens Reimann <jreimann@redhat.com>
2017-02-07 15:10:00 +0100

    Improve deploying to OpenShift
    This change does improve a few things when deploying to OpenShift:
    
    * Test first if the project already exists
    * Set a readyness probe for the database
    * Start liquibase as an Openshift job in the same project in order to
      access the database
    * Finally expose the kapua-console service
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- dev-tools/src/main/openshift/openshift-deploy.sh
+++ dev-tools/src/main/openshift/openshift-deploy.sh
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2016 Red Hat and/or its affiliates and others
+# Copyright (c) 2016, 2017 Red Hat Inc and others
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
@@ -10,69 +10,82 @@
 
 #!/usr/bin/env bash
 
+set -e
+
+OPENSHIFT_PROJECT_NAME="eclipse-kapua"
+
+# print error and exit when necessary
+die() { printf "$@" "\n" 1>&2 ; exit 1; }
+
 #minishift start
 #eval $(minishift docker-env)
 
+# test if the project is already created ... fail otherwise 
+
+oc describe "project/$OPENSHIFT_PROJECT_NAME" &>/dev/null || die "Project '$OPENSHIFT_PROJECT_NAME' not created or OpenShift is unreachable. Try with:\n\n\toc new-project eclipse-kapua\n\n"
+
 #oc login
-#oc new-project eclipse-kapua --description="Open source IoT Platform" --display-name="Eclipse Kapua"
+#oc new-project "$OPENSHIFT_PROJECT_NAME" --description="Open source IoT Platform" --display-name="Eclipse Kapua"
 
 if [ -z "${DOCKER_ACCOUNT}" ]; then
-  DOCKER_ACCOUNT='kapua'
+  DOCKER_ACCOUNT=kapua
 fi
 
-echo 'Starting ElasticSearch server...'
+echo Creating ElasticSearch server...
 
 if [ -z "${ELASTIC_SEARCH_MEMORY}" ]; then
-  ELASTIC_SEARCH_MEMORY='512M'
+  ELASTIC_SEARCH_MEMORY=512M
 fi
 
-oc new-app -e ES_JAVA_OPTS="-Xms${ELASTIC_SEARCH_MEMORY} -Xmx${ELASTIC_SEARCH_MEMORY}" elasticsearch:2.4 -n eclipse-kapua
+oc new-app -e ES_JAVA_OPTS="-Xms${ELASTIC_SEARCH_MEMORY} -Xmx${ELASTIC_SEARCH_MEMORY}" elasticsearch:2.4 -n "$OPENSHIFT_PROJECT_NAME"
 
-echo 'ElasticSearch server started.'
+echo ElasticSearch server created
 
 ### SQL database
 
-echo 'Staring SQL database'
+echo Creating SQL database
 
-oc new-app ${DOCKER_ACCOUNT}/kapua-sql --name=sql -n eclipse-kapua
+oc new-app ${DOCKER_ACCOUNT}/kapua-sql --name=sql -n "$OPENSHIFT_PROJECT_NAME"
+oc set probe dc/sql --readiness --open-tcp=3306
 
-echo 'SQL database started'
+echo SQL database created
 
 ### Broker
 
-echo 'Starting broker'
+echo Creating broker
 
-oc new-app ${DOCKER_ACCOUNT}/kapua-broker:latest -name=kapua-broker -n eclipse-kapua
+oc new-app ${DOCKER_ACCOUNT}/kapua-broker:latest -name=kapua-broker -n "$OPENSHIFT_PROJECT_NAME"
 
-echo 'Broker started'
+echo Broker created
 
 ## Build assembly module with
 ## mvn -Pdocker
 
-echo 'Starting web console'
+echo Creating web console
 
-oc new-app ${DOCKER_ACCOUNT}/kapua-console:latest -n eclipse-kapua -eCOMMONS_DB_SCHEMA=' '
+oc new-app ${DOCKER_ACCOUNT}/kapua-console:latest -n "$OPENSHIFT_PROJECT_NAME" -eCOMMONS_DB_SCHEMA=' '
 
-echo 'Web console started.'
+echo Web console created
 
-echo 'Starting rest api'
+### REST API
 
-oc new-app ${DOCKER_ACCOUNT}/kapua-api:latest -n eclipse-kapua
+echo 'Creating Rest API'
 
-echo 'Rest api started'
+oc new-app ${DOCKER_ACCOUNT}/kapua-api:latest -n "$OPENSHIFT_PROJECT_NAME"
+
+echo 'Rest API created'
 
 ## Applying DB schema
 
-SQL_HOST=`oc get service | grep sql | awk '{print $2}'`
-docker run -it -e SQL_SERVICE_HOST=${SQL_HOST} ${DOCKER_ACCOUNT}/kapua-liquibase
+# Create batch job for liquibase
+oc set image -f liquibase_job.yml "liquibase=$DOCKER_ACCOUNT/kapua-liquibase:latest" --local --source=docker -o yaml | oc create -f -
 
 ## Start router
 
 #oc adm policy add-scc-to-user hostnetwork -z router
-
 #oc adm router --create
 
 ## Expose web console
 
-#oc expose svc/kapua-console
+oc expose svc/kapua-console
 
