11425e5a14e70ae0014a621ab7873cd548915313
Jens Reimann <jreimann@redhat.com>
2017-02-17 17:00:38 +0100

    tweak timeout a bit to be more gentle with startup times
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- dev-tools/src/main/openshift/openshift-deploy.sh
+++ dev-tools/src/main/openshift/openshift-deploy.sh
@@ -55,7 +55,7 @@ echo SQL database created
 echo Creating broker
 
 oc new-app ${DOCKER_ACCOUNT}/kapua-broker:latest -name=kapua-broker -n "$OPENSHIFT_PROJECT_NAME" '-eACTIVEMQ_OPTS=-Dcommons.db.connection.host=localhost -Dcommons.db.connection.port=3306 -Dcommons.db.schema='
-oc set probe dc/kapua-broker --readiness --open-tcp=1883
+oc set probe dc/kapua-broker --readiness --initial-delay-seconds=15 --open-tcp=1883
 
 echo Broker created
 
@@ -65,7 +65,7 @@ echo Broker created
 echo Creating web console
 
 oc new-app ${DOCKER_ACCOUNT}/kapua-console:latest -n "$OPENSHIFT_PROJECT_NAME" '-eCATALINA_OPTS=-Dcommons.db.connection.host=localhost -Dcommons.db.connection.port=3306 -Dcommons.db.schema='
-oc set probe dc/kapua-console --readiness --liveness --initial-delay-seconds=30 --get-url=http://:8080/console
+oc set probe dc/kapua-console --readiness --liveness --initial-delay-seconds=30 --timeout-seconds=10 --get-url=http://:8080/console
 
 echo Web console created
 
@@ -74,7 +74,7 @@ echo Web console created
 echo 'Creating Rest API'
 
 oc new-app ${DOCKER_ACCOUNT}/kapua-api:latest -n "$OPENSHIFT_PROJECT_NAME" '-eCATALINA_OPTS=-Dcommons.db.connection.host=localhost -Dcommons.db.connection.port=3306 -Dcommons.db.schema='
-oc set probe dc/kapua-api --readiness --liveness --initial-delay-seconds=30 --get-url=http://:8080/api
+oc set probe dc/kapua-api --readiness --liveness --initial-delay-seconds=30 --timeout-seconds=10 --get-url=http://:8080/api
 
 echo 'Rest API created'
 
