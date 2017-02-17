51e0282e6f506f018a94e69e739243c81669dbbd
coduz <alberto.codutti@eurotech.com>
2017-02-17 16:33:18 +0100

    Fixed copyrights headers and minor doc improvement
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/DeviceConnection.java
+++ service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/DeviceConnection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -18,15 +18,14 @@ import org.eclipse.kapua.model.id.KapuaId;
 /**
  * Device connection entity definition.
  * 
- * @since 1.0
+ * @since 1.0.0
  *
  */
-public interface DeviceConnection extends KapuaUpdatableEntity
-{
+public interface DeviceConnection extends KapuaUpdatableEntity {
+
     public static final String TYPE = "deviceConnection";
 
-    default public String getType()
-    {
+    default public String getType() {
         return TYPE;
     }
 
