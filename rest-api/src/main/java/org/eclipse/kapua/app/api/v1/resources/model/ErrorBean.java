f7cb71c2432ea825152ad931dee02ba981b3873c
coduz <alberto.codutti@eurotech.com>
2017-02-22 11:01:59 +0100

    Applied code formatting
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/model/ErrorBean.java
+++ rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/model/ErrorBean.java
@@ -19,23 +19,23 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "error")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class ErrorBean 
-{
-    @XmlElement(name="code")
-    private int    code;
+public class ErrorBean {
 
-    @XmlElement(name="reason")
+    @XmlElement(name = "code")
+    private int code;
+
+    @XmlElement(name = "reason")
     private String reason;
 
-    @XmlElement(name="message")
+    @XmlElement(name = "message")
     private String message;
 
     public ErrorBean() {
     }
 
     public ErrorBean(Response.Status status, String message) {
-        this.code    = status.getStatusCode();
-        this.reason  = status.toString();
+        this.code = status.getStatusCode();
+        this.reason = status.toString();
         this.message = message;
     }
 }
