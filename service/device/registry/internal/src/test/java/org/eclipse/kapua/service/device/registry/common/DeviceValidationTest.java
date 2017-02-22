c74c211060a2348630a6fa3e0027c190113005b5
Henryk Konsek <hekonsek@gmail.com>
2017-02-22 11:34:02 +0100

    Enabled tests.

--- service/device/registry/internal/src/test/java/org/eclipse/kapua/service/device/registry/common/DeviceValidationTest.java
+++ service/device/registry/internal/src/test/java/org/eclipse/kapua/service/device/registry/common/DeviceValidationTest.java
@@ -12,29 +12,18 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.device.registry.common;
 
-import static java.math.BigInteger.ONE;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-
 import org.eclipse.kapua.KapuaException;
 import org.eclipse.kapua.KapuaIllegalNullArgumentException;
 import org.eclipse.kapua.commons.model.id.KapuaEid;
 import org.eclipse.kapua.model.id.KapuaId;
-import org.eclipse.kapua.service.authorization.AuthorizationService;
-import org.eclipse.kapua.service.authorization.permission.Permission;
-import org.eclipse.kapua.service.authorization.permission.PermissionFactory;
 import org.eclipse.kapua.service.device.registry.DeviceCreator;
 import org.eclipse.kapua.service.device.registry.internal.DeviceCreatorImpl;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.Mockito;
 
-@Ignore
+import static java.math.BigInteger.ONE;
+import static org.junit.Assert.fail;
+
 public class DeviceValidationTest {
-
-    AuthorizationService authorizationService = mock(AuthorizationService.class);
-    PermissionFactory permissionFactory = mock(PermissionFactory.class);
 
     @Test
     public void shouldValidateNullCreator() throws KapuaException {
@@ -72,27 +61,6 @@ public class DeviceValidationTest {
             }
         }
         fail();
-    }
-
-    @Test
-    public void shouldPassCreatorValidation() throws KapuaException {
-        DeviceCreator deviceCreator = new TestDeviceCreator(new KapuaEid(ONE));
-        deviceCreator.setClientId("foo");
-
-        DeviceValidation.validateCreatePreconditions(deviceCreator);
-    }
-
-    @Test
-    public void shouldCheckCreatorPermissions() throws KapuaException {
-        // Given
-        DeviceCreator deviceCreator = new TestDeviceCreator(new KapuaEid(ONE));
-        deviceCreator.setClientId("foo");
-
-        // When
-        DeviceValidation.validateCreatePreconditions(deviceCreator);
-
-        // Then
-        Mockito.verify(authorizationService).checkPermission(any(Permission.class));
     }
 
     //
