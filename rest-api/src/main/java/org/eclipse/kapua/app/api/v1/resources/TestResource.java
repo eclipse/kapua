48d1ae28f786c5616f6825a31bb1e3af7df96f1f
coduz <alberto.codutti@eurotech.com>
2017-02-22 10:57:03 +0100

    Organized imports
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/TestResource.java
+++ rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/TestResource.java
@@ -16,8 +16,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import io.swagger.annotations.Api;
-
 /**
  * Root resource (exposed at "test" path)
  */
