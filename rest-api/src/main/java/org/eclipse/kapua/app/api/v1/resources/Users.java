8b56bda586b59022c5aaaa92c39b9fe8e379e88a
coduz <alberto.codutti@eurotech.com>
2017-02-22 10:59:11 +0100

    Organized imports
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/Users.java
+++ rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/Users.java
@@ -12,13 +12,13 @@
  *******************************************************************************/
 package org.eclipse.kapua.app.api.v1.resources;
 
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
