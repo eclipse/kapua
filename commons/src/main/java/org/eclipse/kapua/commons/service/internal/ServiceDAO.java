df0008d7f05b4e8370094a0f9e5490450dbb9583
coduz <alberto.codutti@eurotech.com>
2017-02-23 14:19:34 +0100

    Fixed attribute management for KapuaId instances
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- commons/src/main/java/org/eclipse/kapua/commons/service/internal/ServiceDAO.java
+++ commons/src/main/java/org/eclipse/kapua/commons/service/internal/ServiceDAO.java
@@ -575,7 +575,13 @@ public class ServiceDAO {
             throws KapuaException {
         Expression<Boolean> expr;
         String attrName = attrPred.getAttributeName();
+        SingularAttribute attribute = entityType.getSingularAttribute(attrName);
+
+        // Parse attributes
         Object attrValue = attrPred.getAttributeValue();
+        if (attrValue instanceof KapuaId && !(attrValue instanceof KapuaEid)) {
+            attrValue = new KapuaEid((KapuaId) attrValue);
+        }
 
         // Fields to query properties of sub attributes of the root entity
         SingularAttribute attribute;
