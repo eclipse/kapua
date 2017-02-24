870a4c5c7eb3dad53d3f3e65e8046ced05c97b9f
coduz <alberto.codutti@eurotech.com>
2017-02-24 15:34:03 +0100

    Added override of the default documentation to query method of DeviceEventService
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/event/DeviceEventFactory.java
+++ service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/event/DeviceEventFactory.java
@@ -57,5 +57,5 @@ public interface DeviceEventFactory extends KapuaObjectFactory
      * 
      * @return
      */
-    DeviceEventListResult newDeviceListResult();
+    DeviceEventListResult newDeviceEventListResult();
 }
