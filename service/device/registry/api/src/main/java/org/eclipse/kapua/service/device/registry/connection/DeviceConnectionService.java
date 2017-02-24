543d9a23555c4a89559065032b11640714ba3346
coduz <alberto.codutti@eurotech.com>
2017-02-24 17:23:00 +0100

    Refactored Device Connection REST API resource
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/DeviceConnectionService.java
+++ service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/DeviceConnectionService.java
@@ -14,13 +14,17 @@ package org.eclipse.kapua.service.device.registry.connection;
 
 import org.eclipse.kapua.KapuaException;
 import org.eclipse.kapua.model.id.KapuaId;
+import org.eclipse.kapua.model.query.KapuaQuery;
 import org.eclipse.kapua.service.KapuaEntityService;
 import org.eclipse.kapua.service.KapuaUpdatableEntityService;
+import org.eclipse.kapua.service.device.registry.Device;
+import org.eclipse.kapua.service.device.registry.DeviceListResult;
+import org.eclipse.kapua.service.device.registry.DeviceQuery;
 
 /**
  * DeviceConnectionService exposes APIs to retrieve Device connections under a scope.
  * It includes APIs to find, list, and update devices connections associated with a scope.
- *
+ * 
  * @since 1.0
  */
 public interface DeviceConnectionService extends KapuaEntityService<DeviceConnection, DeviceConnectionCreator>,
@@ -28,33 +32,45 @@ public interface DeviceConnectionService extends KapuaEntityService<DeviceConnec
 
     /**
      * Find the connection by client identifier
-     *
+     * 
      * @param scopeId
      * @param clientId
      * @return
      * @throws KapuaException
      */
     public DeviceConnection findByClientId(KapuaId scopeId, String clientId)
+        throws KapuaException;
+
+    /**
+     * Returns the {@link DeviceConnectionListResult} with elements matching the provided query.
+     * 
+     * @param query
+     *            The {@link DeviceConnectionQuery} used to filter results.
+     * @return The {@link DeviceConnectionListResult} with elements matching the query parameter.
+     * @throws KapuaException
+     * @since 1.0.0
+     */
+    public DeviceConnectionListResult query(KapuaQuery<DeviceConnection> query)
             throws KapuaException;
 
     /**
      * Updated the status of provided device connection to connected;
      * if a device connection for the provided clientId is not found,
      * a new device connection is created and updated.
-     *
+     * 
      * @param creator
      * @throws KapuaException
      */
     public void connect(DeviceConnectionCreator creator)
-            throws KapuaException;
+        throws KapuaException;
 
     /**
      * Register a device message when a client disconnects from the broker
-     *
+     * 
      * @param scopeId
      * @param clientId
      * @throws KapuaException
      */
     public void disconnect(KapuaId scopeId, String clientId)
-            throws KapuaException;
+        throws KapuaException;
 }
