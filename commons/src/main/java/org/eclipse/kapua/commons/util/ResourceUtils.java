de0bb62d424ce8f642b45530d02346891d7b3d86
Jens Reimann <jreimann@redhat.com>
2017-02-27 11:30:50 +0100

    Cleanup reading a URL
    Use Java's try-with-resource construct and Guava's CharStreams helper
    method. Provide a unit test for this.

--- commons/src/main/java/org/eclipse/kapua/commons/util/ResourceUtils.java
+++ commons/src/main/java/org/eclipse/kapua/commons/util/ResourceUtils.java
@@ -15,51 +15,52 @@ package org.eclipse.kapua.commons.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.net.URL;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.io.CharStreams;
+
 /**
  * Resource utilities
- * 
+ *
  * @since 1.0
  *
  */
-public class ResourceUtils 
-{
-    public static final Logger s_logger = LoggerFactory.getLogger(ResourceUtils.class);
+public class ResourceUtils {
+
+    public static final Logger logger = LoggerFactory.getLogger(ResourceUtils.class);
 
     /**
      * Get the URL of a resource
-     * 
-     * @param resource to locate
+     *
+     * @param resource
+     *            to locate
      * @return The URL to the resource, or {@code null} if it cannot be found
      */
     public static URL getResource(String resource) {
-        //Try with the Thread Context Loader.
+        // Try with the Thread Context Loader.
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        if(classLoader != null) {
+        if (classLoader != null) {
             URL url = classLoader.getResource(resource);
-            if(url != null) {
+            if (url != null) {
                 return url;
             }
         }
 
-        //Let's now try with the classloader that loaded this class.
+        // Let's now try with the classloader that loaded this class.
         classLoader = ResourceUtils.class.getClassLoader();
-        if(classLoader != null) {
+        if (classLoader != null) {
             URL url = classLoader.getResource(resource);
-            if(url != null) {
+            if (url != null) {
                 return url;
             }
         }
 
-        //Last ditch attempt. Get the resource from the classpath.
+        // Last ditch attempt. Get the resource from the classpath.
         return ClassLoader.getSystemResource(resource);
     }
-
 
     /**
      * Reads a resource fully and returns it as a string.
@@ -68,37 +69,9 @@ public class ResourceUtils
      * @return
      * @throws IOException
      */
-    public static String readResource(URL resourceUrl)
-    throws IOException {
-        String result = null;
-        BufferedReader resourceBr = null;
-        InputStreamReader resourceIsr = null;
-        try {
-
-            resourceIsr = new InputStreamReader(resourceUrl.openStream());
-            resourceBr  = new BufferedReader(resourceIsr);
-
-            int iRead = 0;
-            char[] buffer = new char[1024];
-            StringWriter sw = new StringWriter();
-            while ((iRead = resourceBr.read(buffer)) != -1) {
-                sw.write(buffer, 0, iRead);
-            }
-            result = sw.toString();
-        } finally {
-            if (resourceBr != null) {
-                try {
-                    resourceBr.close();
-                } catch (IOException e)  {
-                    s_logger.warn("Error closing reader", e);
-                }
-                try {
-                    resourceIsr.close();
-                } catch (IOException e)  {
-                    s_logger.warn("Error closing reader", e);
-                }
-            }
+    public static String readResource(URL resourceUrl) throws IOException {
+        try (BufferedReader resourceBr = new BufferedReader(new InputStreamReader(resourceUrl.openStream()))) {
+            return CharStreams.toString(resourceBr);
         }
-        return result;
     }
 }
