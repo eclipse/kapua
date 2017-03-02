dd597bcefebec0bf4b76ef8b98e378889900919b
Jens Reimann <jreimann@redhat.com>
2017-03-02 13:30:34 +0100

    #340: Backport EclipseLink 2.7.0 SLF4Logger
    This change backports the SLF4Logger from EclipseLink 2.7.0 and
    enables its use throughout Kapua.
    
    This allows for consolidated logging throughout the Kapua platform.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

---
+++ commons/src/main/java/org/eclipse/persistence/logging/slf4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * SLF4J logging adapter for EclipseLink
+ * <p>
+ * <strong>Note:</strong> This is backported from EclipeLink 2.7.0.
+ * Remove this once we switched to a final release of EclipseLink 2.7.0.
+ * </p>
+ */
+package org.eclipse.persistence.logging.slf4j;
