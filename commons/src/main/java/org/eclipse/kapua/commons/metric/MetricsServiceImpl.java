f68a6ccdfa53de275a35ad976367a1efedbc6457
Henryk Konsek <hekonsek@gmail.com>
2017-03-14 21:56:05 +0100

    Applied Kapua formatting.

--- commons/src/main/java/org/eclipse/kapua/commons/metric/MetricsServiceImpl.java
+++ commons/src/main/java/org/eclipse/kapua/commons/metric/MetricsServiceImpl.java
@@ -34,98 +34,97 @@ import com.codahale.metrics.Timer;
  */
 @KapuaProvider
 public class MetricsServiceImpl implements MetricsService {
-		
-	private static Logger logger = LoggerFactory.getLogger(MetricsServiceImpl.class);
-	
-	public final static String METRICS_NAME_FORMAT = "{0}.{1}.{2}";
-	
-	private final MetricRegistry metricRegistry;
+
+    private static Logger logger = LoggerFactory.getLogger(MetricsServiceImpl.class);
+
+    public final static String METRICS_NAME_FORMAT = "{0}.{1}.{2}";
+
+    private final MetricRegistry metricRegistry;
 
     /**
      * Default metric service constructor
      */
-	public MetricsServiceImpl() {
-		 metricRegistry = new MetricRegistry();
-	}
-	
+    public MetricsServiceImpl() {
+        metricRegistry = new MetricRegistry();
+    }
+
     @Override
-	public MetricRegistry getMetricRegistry() {
-		return metricRegistry;
-	}
-	
+    public MetricRegistry getMetricRegistry() {
+        return metricRegistry;
+    }
+
     @Override
-	public Counter getCounter(String module, String component, String... names) {
-		String name = getMetricName(module, component, names);
-		Counter counter = metricRegistry.getCounters().get(name);
-		if(counter == null) {
-			logger.debug("Creating a Counter: {}", name);
-			counter = metricRegistry.counter(name);
-		}
-		return counter;
-	}
-	
+    public Counter getCounter(String module, String component, String... names) {
+        String name = getMetricName(module, component, names);
+        Counter counter = metricRegistry.getCounters().get(name);
+        if (counter == null) {
+            logger.debug("Creating a Counter: {}", name);
+            counter = metricRegistry.counter(name);
+        }
+        return counter;
+    }
+
     @Override
-	public Histogram getHistogram(String module, String component, String... names) {
-		String name = getMetricName(module, component, names);
-		Histogram histogram = metricRegistry.getHistograms().get(name);
-		if(histogram == null) {
-			logger.debug("Creating a Histogram: {}", name);
-			histogram = metricRegistry.histogram(name);
-		}
-		return histogram;
-	}
-	
+    public Histogram getHistogram(String module, String component, String... names) {
+        String name = getMetricName(module, component, names);
+        Histogram histogram = metricRegistry.getHistograms().get(name);
+        if (histogram == null) {
+            logger.debug("Creating a Histogram: {}", name);
+            histogram = metricRegistry.histogram(name);
+        }
+        return histogram;
+    }
+
     @Override
-	public Timer getTimer(String module, String component, String... names) {
-		String name = getMetricName(module, component, names);
-		Timer timer = metricRegistry.getTimers().get(name);
-		if(timer == null) {
-			logger.debug("Creating a Timer: {}", name);
-			timer = metricRegistry.timer(name);
-		}
-		return timer;
-	}
-	
+    public Timer getTimer(String module, String component, String... names) {
+        String name = getMetricName(module, component, names);
+        Timer timer = metricRegistry.getTimers().get(name);
+        if (timer == null) {
+            logger.debug("Creating a Timer: {}", name);
+            timer = metricRegistry.timer(name);
+        }
+        return timer;
+    }
+
     @Override
-	public void registerGauge(Gauge<?> gauge, String module, String component, String... names) throws KapuaException {
-		String name = getMetricName(module, component, names);
-		if (metricRegistry.getGauges().get(name) != null) {
-			throw KapuaException.internalError(MessageFormat.format("A metric with the name {0} is already defined!", name));
-		}
-		else {
-			metricRegistry.register(name, gauge);
-		}
-	}
-	
+    public void registerGauge(Gauge<?> gauge, String module, String component, String... names) throws KapuaException {
+        String name = getMetricName(module, component, names);
+        if (metricRegistry.getGauges().get(name) != null) {
+            throw KapuaException.internalError(MessageFormat.format("A metric with the name {0} is already defined!", name));
+        } else {
+            metricRegistry.register(name, gauge);
+        }
+    }
+
     /**
      * Build the metric name based on module, component and metric names
-     * 
+     *
      * @param module
      * @param component
      * @param metricsName
      * @return
      */
-	private String getMetricName(String module, String component, String... metricsName) {
-		return MessageFormat.format(METRICS_NAME_FORMAT, new Object[]{module, component, convertToDotNotation(metricsName)});
-	}
-	
+    private String getMetricName(String module, String component, String... metricsName) {
+        return MessageFormat.format(METRICS_NAME_FORMAT, new Object[] { module, component, convertToDotNotation(metricsName) });
+    }
+
     /**
      * Convert the metric names to a concatenated dot separated string
-     * 
+     *
      * @param metricsName
      * @return
      */
-	private String convertToDotNotation(String... metricsName) {
-		StringBuilder builder = new StringBuilder();
-		boolean firstMetricName = true;
-		for(String s : metricsName) {
-			if (!firstMetricName) {
-				builder.append('.');
-			}
-			firstMetricName = false;
-		    builder.append(s);
-		}
-		return builder.toString();
-	}
-	
+    private String convertToDotNotation(String... metricsName) {
+        StringBuilder builder = new StringBuilder();
+        boolean firstMetricName = true;
+        for (String s : metricsName) {
+            if (!firstMetricName) {
+                builder.append('.');
+            }
+            firstMetricName = false;
+            builder.append(s);
+        }
+        return builder.toString();
+    }
+
 }
