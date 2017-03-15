0bd6ec0f728848648bedbdd4485b93a2c39b2046
HSL\mtomic <marko.tomic@comtrade.com>
2017-03-15 14:27:10 +0100

    Groups tab page, controller and view
    Signed-off-by: HSL\mtomic <marko.tomic@comtrade.com>

---
+++ console-v2/ui/src/devices/controllers/DeviceDetailGroupsCtrl.ts
@@ -0,0 +1,20 @@
+/*******************************************************************************
+* Copyright (c) 2016, 2017 Eurotech and/or its affiliates                      
+*                                                                               
+* All rights reserved. This program and the accompanying materials              
+* are made available under the terms of the Eclipse Public License v1.0         
+* which accompanies this distribution, and is available at                      
+* http://www.eclipse.org/legal/epl-v10.html                                     
+*                                                                               
+* Contributors:                                                                 
+*     Eurotech - initial API and implementation                                 
+*                                                                               
+*******************************************************************************/
+export default class DeviceDetailGroupsCtrl {
+  private deviceId: string;
+
+  constructor(private $stateParams: angular.ui.IStateParamsService,
+    private $http: angular.IHttpService) {
+    this.deviceId = $stateParams["id"];
+  }
+}
