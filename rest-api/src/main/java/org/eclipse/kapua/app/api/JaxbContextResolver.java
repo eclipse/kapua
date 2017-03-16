9a6dfd5d71a5629162701580e99b4811e6f497ad
coduz <alberto.codutti@eurotech.com>
2017-03-16 15:00:40 +0100

    Updated JAXB Context Resolver configuration
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- rest-api/src/main/java/org/eclipse/kapua/app/api/JaxbContextResolver.java
+++ rest-api/src/main/java/org/eclipse/kapua/app/api/JaxbContextResolver.java
@@ -23,6 +23,7 @@ import org.eclipse.kapua.app.api.v1.resources.model.CountResult;
 import org.eclipse.kapua.app.api.v1.resources.model.ErrorBean;
 import org.eclipse.kapua.model.config.metatype.KapuaTad;
 import org.eclipse.kapua.model.config.metatype.KapuaTicon;
+import org.eclipse.kapua.model.config.metatype.KapuaTmetadata;
 import org.eclipse.kapua.model.config.metatype.KapuaTocd;
 import org.eclipse.kapua.model.config.metatype.KapuaToption;
 import org.eclipse.kapua.service.account.Account;
@@ -77,6 +78,10 @@ import org.eclipse.kapua.service.authorization.role.RolePermissionQuery;
 import org.eclipse.kapua.service.authorization.role.RolePermissionXmlRegistry;
 import org.eclipse.kapua.service.authorization.role.RoleQuery;
 import org.eclipse.kapua.service.authorization.role.RoleXmlRegistry;
+import org.eclipse.kapua.service.datastore.ClientInfoXmlRegistry;
+import org.eclipse.kapua.service.datastore.model.ClientInfo;
+import org.eclipse.kapua.service.datastore.model.ClientInfoListResult;
+import org.eclipse.kapua.service.datastore.model.query.ClientInfoQuery;
 import org.eclipse.kapua.service.device.call.kura.model.bundle.KuraBundles;
 import org.eclipse.kapua.service.device.call.kura.model.configuration.KuraDeviceConfiguration;
 import org.eclipse.kapua.service.device.call.kura.model.deploy.KuraDeploymentPackage;
@@ -145,6 +150,7 @@ public class JaxbContextResolver implements ContextResolver<JAXBContext> {
                     KapuaTocd.class,
                     KapuaTad.class,
                     KapuaTicon.class,
+                    KapuaTmetadata.class,
                     KapuaToption.class,
 
                     // Account
@@ -154,12 +160,12 @@ public class JaxbContextResolver implements ContextResolver<JAXBContext> {
                     AccountQuery.class,
                     AccountXmlRegistry.class,
 
-                    User.class,
-                    UserCreator.class,
-                    UserListResult.class,
-                    UserQuery.class,
-                    UserXmlRegistry.class,
-
+                    // Data Client Info
+                    ClientInfo.class,
+                    ClientInfoListResult.class,
+                    ClientInfoQuery.class,
+                    ClientInfoXmlRegistry.class,
+                    
                     // Device
                     Device.class,
                     DeviceCreator.class,
@@ -279,7 +285,14 @@ public class JaxbContextResolver implements ContextResolver<JAXBContext> {
                     AccessRoleListResult.class,
                     AccessRoleCreator.class,
                     AccessRoleQuery.class,
-                    AccessRoleXmlRegistry.class
+                    AccessRoleXmlRegistry.class,
+                    
+                    // User
+                    User.class,
+                    UserCreator.class,
+                    UserListResult.class,
+                    UserQuery.class,
+                    UserXmlRegistry.class
 
             }, null);
         } catch (Exception e) {
