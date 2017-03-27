4b3575ace8fd0d3a7327904e940a7253faa7bc20
Henryk Konsek <hekonsek@gmail.com>
2017-03-27 18:05:02 +0200

    Added device registration to OpenShift regression suite.

--- qa-openshift/src/test/java/org/eclipse/kapua/qa/openshift/OpenShiftTest.java
+++ qa-openshift/src/test/java/org/eclipse/kapua/qa/openshift/OpenShiftTest.java
@@ -10,60 +10,66 @@
 package org.eclipse.kapua.qa.openshift;
 
 import org.apache.commons.io.IOUtils;
+import org.eclipse.kapua.kura.simulator.main.SimulatorRunner;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OpenShiftTest {
 
+    String oc = "/tmp/openshift/openshift-origin-server-v1.4.1+3f9807a-linux-64bit/oc";
+
     @Test
     public void shouldSuccessfullyDeployBroker() throws IOException {
-        String oc = "/tmp/openshift/openshift-origin-server-v1.4.1+3f9807a-linux-64bit/oc";
         Process process = new ProcessBuilder().command(oc, "get", "pod").redirectErrorStream(true).start();
         List<String> output = Arrays.asList(IOUtils.toString(process.getInputStream()).split("\\n"));
-        String brokerPod = output.stream().filter(line -> line.startsWith("kapua-broker-")).collect(Collectors.toList()).get(0);
+        String brokerPod = output.stream().filter(line -> line.startsWith("kapua-broker-")).collect(toList()).get(0);
         assertThat(brokerPod).contains("Running");
     }
 
     @Test
     public void shouldSuccessfullyDeployApi() throws IOException {
-        String oc = "/tmp/openshift/openshift-origin-server-v1.4.1+3f9807a-linux-64bit/oc";
         Process process = new ProcessBuilder().command(oc, "get", "pod").redirectErrorStream(true).start();
         List<String> output = Arrays.asList(IOUtils.toString(process.getInputStream()).split("\\n"));
-        String brokerPod = output.stream().filter(line -> line.startsWith("kapua-api-")).collect(Collectors.toList()).get(0);
+        String brokerPod = output.stream().filter(line -> line.startsWith("kapua-api-")).collect(toList()).get(0);
         assertThat(brokerPod).contains("Running");
     }
 
     @Test
     public void shouldSuccessfullyDeployConsole() throws IOException {
-        String oc = "/tmp/openshift/openshift-origin-server-v1.4.1+3f9807a-linux-64bit/oc";
         Process process = new ProcessBuilder().command(oc, "get", "pod").redirectErrorStream(true).start();
         List<String> output = Arrays.asList(IOUtils.toString(process.getInputStream()).split("\\n"));
-        String brokerPod = output.stream().filter(line -> line.startsWith("kapua-console-")).collect(Collectors.toList()).get(0);
+        String brokerPod = output.stream().filter(line -> line.startsWith("kapua-console-")).collect(toList()).get(0);
         assertThat(brokerPod).contains("Running");
     }
 
     @Test
     public void shouldSuccessfullyDeploySqlServer() throws IOException {
-        String oc = "/tmp/openshift/openshift-origin-server-v1.4.1+3f9807a-linux-64bit/oc";
         Process process = new ProcessBuilder().command(oc, "get", "pod").redirectErrorStream(true).start();
         List<String> output = Arrays.asList(IOUtils.toString(process.getInputStream()).split("\\n"));
-        String brokerPod = output.stream().filter(line -> line.startsWith("sql-")).collect(Collectors.toList()).get(0);
+        String brokerPod = output.stream().filter(line -> line.startsWith("sql-")).collect(toList()).get(0);
         assertThat(brokerPod).contains("Running");
     }
 
     @Test
     public void shouldSuccessfullyDeployElasticSearch() throws IOException {
-        String oc = "/tmp/openshift/openshift-origin-server-v1.4.1+3f9807a-linux-64bit/oc";
         Process process = new ProcessBuilder().command(oc, "get", "pod").redirectErrorStream(true).start();
         List<String> output = Arrays.asList(IOUtils.toString(process.getInputStream()).split("\\n"));
-        String brokerPod = output.stream().filter(line -> line.startsWith("elasticsearch-")).collect(Collectors.toList()).get(0);
+        String brokerPod = output.stream().filter(line -> line.startsWith("elasticsearch-")).collect(toList()).get(0);
         assertThat(brokerPod).contains("Running");
+    }
+
+    @Test
+    public void shouldRegisterDevice() throws Throwable {
+        Process process = new ProcessBuilder().command(oc, "get", "service").redirectErrorStream(true).start();
+        List<String> output = Arrays.asList(IOUtils.toString(process.getInputStream()).split("\\n"));
+        String brokerAddress = output.stream().filter( line -> line.contains("broker")).map( line -> line.split("\\s+")[1]).collect(toList()).get(0);
+        SimulatorRunner.main("-bh", brokerAddress, "-s", "10");
     }
 
 }
