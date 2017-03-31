5df93a82a1690d075a6a25fe22a36bba8d06ebe1
coduz <alberto.codutti@eurotech.com>
2017-03-31 14:48:06 +0200

    Applied code formatting
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/DataMessages.java
+++ rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/DataMessages.java
@@ -20,28 +20,21 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.DatatypeConverter;
 
-import org.eclipse.kapua.KapuaIllegalArgumentException;
-import org.eclipse.kapua.KapuaIllegalNullArgumentException;
 import org.eclipse.kapua.app.api.v1.resources.model.CountResult;
-import org.eclipse.kapua.app.api.v1.resources.model.MetricType;
 import org.eclipse.kapua.app.api.v1.resources.model.ScopeId;
 import org.eclipse.kapua.app.api.v1.resources.model.StorableEntityId;
 import org.eclipse.kapua.locator.KapuaLocator;
 import org.eclipse.kapua.service.datastore.DatastoreObjectFactory;
 import org.eclipse.kapua.service.datastore.MessageStoreService;
 import org.eclipse.kapua.service.datastore.internal.elasticsearch.MessageField;
-import org.eclipse.kapua.service.datastore.internal.elasticsearch.MetricInfoField;
 import org.eclipse.kapua.service.datastore.internal.model.query.AndPredicateImpl;
 import org.eclipse.kapua.service.datastore.internal.model.query.ChannelMatchPredicateImpl;
-import org.eclipse.kapua.service.datastore.internal.model.query.RangePredicateImpl;
 import org.eclipse.kapua.service.datastore.model.DatastoreMessage;
 import org.eclipse.kapua.service.datastore.model.MessageListResult;
 import org.eclipse.kapua.service.datastore.model.query.AndPredicate;
 import org.eclipse.kapua.service.datastore.model.query.ChannelMatchPredicate;
 import org.eclipse.kapua.service.datastore.model.query.MessageQuery;
-import org.eclipse.kapua.service.datastore.model.query.RangePredicate;
 import org.eclipse.kapua.service.datastore.model.query.StorableFetchStyle;
 import org.eclipse.kapua.service.datastore.model.query.TermPredicate;
 
@@ -81,10 +74,10 @@ public class DataMessages extends AbstractKapuaResource {
             @ApiParam(value = "The ScopeId in which to search results", required = true, defaultValue = DEFAULT_SCOPE_ID) @PathParam("scopeId") ScopeId scopeId,//
             @ApiParam(value = "The client id to filter results") @QueryParam("clientId") String clientId, //
             @ApiParam(value = "The channel to filter results. It allows '#' wildcard in last channel level") @QueryParam("channel") String channel,
-//            @ApiParam(value = "The metric name to filter results") @QueryParam("metricName") String metricName, //
-//            @ApiParam(value = "The metric type to filter results") @QueryParam("metricType") MetricType metricType, //
-//            @ApiParam(value = "The min metric value to filter results") @QueryParam("metricValueMin") String metricMinValue, //
-//            @ApiParam(value = "The max metric value to filter results") @QueryParam("metricValueMax") String metricMaxValue, //
+            // @ApiParam(value = "The metric name to filter results") @QueryParam("metricName") String metricName, //
+            // @ApiParam(value = "The metric type to filter results") @QueryParam("metricType") MetricType metricType, //
+            // @ApiParam(value = "The min metric value to filter results") @QueryParam("metricValueMin") String metricMinValue, //
+            // @ApiParam(value = "The max metric value to filter results") @QueryParam("metricValueMax") String metricMaxValue, //
             @ApiParam(value = "The result set offset", defaultValue = "0") @QueryParam("offset") @DefaultValue("0") int offset,//
             @ApiParam(value = "The result set limit", defaultValue = "50") @QueryParam("limit") @DefaultValue("50") int limit) //
     {
@@ -101,7 +94,7 @@ public class DataMessages extends AbstractKapuaResource {
                 andPredicate.getPredicates().add(channelPredicate);
             }
 
-//            manageMetricValueFiltering(andPredicate, metricName, metricType, metricMinValue, metricMaxValue);
+            // manageMetricValueFiltering(andPredicate, metricName, metricType, metricMinValue, metricMaxValue);
 
             MessageQuery query = datastoreObjectFactory.newDatastoreMessageQuery(scopeId);
             query.setPredicate(andPredicate);
@@ -201,73 +194,73 @@ public class DataMessages extends AbstractKapuaResource {
         return returnNotNullEntity(datastoreMessage);
     }
 
-//    /**
-//     * Manages the optional metric value filtering for {@link #simpleQuery(ScopeId, String, String, String, String, String, String, int, int)} invocation.
-//     * 
-//     * @param andPredicate
-//     *            The {@link AndPredicate} container
-//     * @param metricName
-//     *            The metric name to filter. Mandatory.
-//     * @param metricType
-//     *            The metric type to filter. Mandatory.
-//     * @param metricMinValue
-//     *            The metric minimum value to filter. It must be coherent with the type parameter.
-//     * @param metricMaxValue
-//     *            The metric maximum value to filter. It must be coherent with the type parameter.
-//     * @throws KapuaIllegalNullArgumentException
-//     *             If metricName or metricType parameter are null.
-//     * @throws KapuaIllegalArgumentException
-//     *             If metricType is not managed.
-//     */
-//    private <T extends Comparable<T>> void manageMetricValueFiltering(AndPredicate andPredicate, String metricName, MetricType metricType, String metricMinValue, String metricMaxValue)
-//            throws KapuaIllegalNullArgumentException, KapuaIllegalArgumentException {
-//
-//        if (metricName != null && metricType != null && metricMinValue != null && metricMaxValue != null) {
-//
-//            if (Strings.isNullOrEmpty(metricName)) {
-//                throw new KapuaIllegalNullArgumentException("metricName");
-//            }
-//
-//            T convertedMinValue;
-//            try {
-//                convertedMinValue = parseMetricValue(metricType, metricMinValue);
-//            } catch (NumberFormatException nfe) {
-//                throw new KapuaIllegalArgumentException("metricMinValue", metricMinValue);
-//            }
-//
-//            T convertedMaxValue;
-//            try {
-//                convertedMaxValue = parseMetricValue(metricType, metricMaxValue);
-//            } catch (NumberFormatException nfe) {
-//                throw new KapuaIllegalArgumentException("metricMinValue", metricMinValue);
-//            }
-//
-//            andPredicate.getPredicates().add(new RangePredicateImpl<>(MessageField.METRICS, convertedMinValue, convertedMaxValue));
-//        }
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    private <T> T parseMetricValue(MetricType metricType, String metricValue) throws KapuaIllegalArgumentException {
-//        Class<T> metricTypeClazz = metricType.getClazz();
-//
-//        T convertedValue;
-//        if (metricValue == null) {
-//            convertedValue = null;
-//        } else if (metricTypeClazz == String.class) {
-//            convertedValue = (T) metricValue;
-//        } else if (metricTypeClazz == Integer.class) {
-//            convertedValue = (T) new Integer(metricValue);
-//        } else if (metricTypeClazz == Long.class) {
-//            convertedValue = (T) new Long(metricValue);
-//        } else if (metricTypeClazz == Float.class) {
-//            convertedValue = (T) new Float(metricValue);
-//        } else if (metricTypeClazz == Double.class) {
-//            convertedValue = (T) new Double(metricValue);
-//        } else if (metricTypeClazz == Boolean.class) {
-//            convertedValue = (T) new Boolean(metricValue);
-//        } else {
-//            throw new KapuaIllegalArgumentException("metricType", metricTypeClazz.getSimpleName());
-//        }
-//        return convertedValue;
-//    }
+    // /**
+    // * Manages the optional metric value filtering for {@link #simpleQuery(ScopeId, String, String, String, String, String, String, int, int)} invocation.
+    // *
+    // * @param andPredicate
+    // * The {@link AndPredicate} container
+    // * @param metricName
+    // * The metric name to filter. Mandatory.
+    // * @param metricType
+    // * The metric type to filter. Mandatory.
+    // * @param metricMinValue
+    // * The metric minimum value to filter. It must be coherent with the type parameter.
+    // * @param metricMaxValue
+    // * The metric maximum value to filter. It must be coherent with the type parameter.
+    // * @throws KapuaIllegalNullArgumentException
+    // * If metricName or metricType parameter are null.
+    // * @throws KapuaIllegalArgumentException
+    // * If metricType is not managed.
+    // */
+    // private <T extends Comparable<T>> void manageMetricValueFiltering(AndPredicate andPredicate, String metricName, MetricType metricType, String metricMinValue, String metricMaxValue)
+    // throws KapuaIllegalNullArgumentException, KapuaIllegalArgumentException {
+    //
+    // if (metricName != null && metricType != null && metricMinValue != null && metricMaxValue != null) {
+    //
+    // if (Strings.isNullOrEmpty(metricName)) {
+    // throw new KapuaIllegalNullArgumentException("metricName");
+    // }
+    //
+    // T convertedMinValue;
+    // try {
+    // convertedMinValue = parseMetricValue(metricType, metricMinValue);
+    // } catch (NumberFormatException nfe) {
+    // throw new KapuaIllegalArgumentException("metricMinValue", metricMinValue);
+    // }
+    //
+    // T convertedMaxValue;
+    // try {
+    // convertedMaxValue = parseMetricValue(metricType, metricMaxValue);
+    // } catch (NumberFormatException nfe) {
+    // throw new KapuaIllegalArgumentException("metricMinValue", metricMinValue);
+    // }
+    //
+    // andPredicate.getPredicates().add(new RangePredicateImpl<>(MessageField.METRICS, convertedMinValue, convertedMaxValue));
+    // }
+    // }
+    //
+    // @SuppressWarnings("unchecked")
+    // private <T> T parseMetricValue(MetricType metricType, String metricValue) throws KapuaIllegalArgumentException {
+    // Class<T> metricTypeClazz = metricType.getClazz();
+    //
+    // T convertedValue;
+    // if (metricValue == null) {
+    // convertedValue = null;
+    // } else if (metricTypeClazz == String.class) {
+    // convertedValue = (T) metricValue;
+    // } else if (metricTypeClazz == Integer.class) {
+    // convertedValue = (T) new Integer(metricValue);
+    // } else if (metricTypeClazz == Long.class) {
+    // convertedValue = (T) new Long(metricValue);
+    // } else if (metricTypeClazz == Float.class) {
+    // convertedValue = (T) new Float(metricValue);
+    // } else if (metricTypeClazz == Double.class) {
+    // convertedValue = (T) new Double(metricValue);
+    // } else if (metricTypeClazz == Boolean.class) {
+    // convertedValue = (T) new Boolean(metricValue);
+    // } else {
+    // throw new KapuaIllegalArgumentException("metricType", metricTypeClazz.getSimpleName());
+    // }
+    // return convertedValue;
+    // }
 }
