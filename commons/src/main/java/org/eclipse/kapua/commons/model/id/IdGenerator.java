e5a6f488acaa4b7723a2401dbbdff3f59695a7f3
Jens Reimann <jreimann@redhat.com>
2017-04-03 12:01:43 +0200

    Apply code formatter and code style

--- commons/src/main/java/org/eclipse/kapua/commons/model/id/IdGenerator.java
+++ commons/src/main/java/org/eclipse/kapua/commons/model/id/IdGenerator.java
@@ -21,21 +21,20 @@ import org.eclipse.kapua.commons.setting.system.SystemSettingKey;
  * Generates random identifier
  *
  * @since 1.0
- * 
+ *
  */
-public class IdGenerator
-{
+public class IdGenerator {
+
     private final static SecureRandom secureRandom = new SecureRandom();
-    private static int                ID_SIZE      = SystemSetting.getInstance().getInt(SystemSettingKey.KAPUA_KEY_SIZE);
+    private final static int ID_SIZE = SystemSetting.getInstance().getInt(SystemSettingKey.KAPUA_KEY_SIZE);
 
     /**
      * Generate a {@link BigInteger} random value.<br>
      * For more detail refer to: {@link SystemSettingKey#KAPUA_KEY_SIZE}
-     * 
+     *
      * @return
      */
-    public static BigInteger generate()
-    {
+    public static BigInteger generate() {
         byte[] bytes = new byte[ID_SIZE];
         secureRandom.nextBytes(bytes);
         return new BigInteger(bytes);
