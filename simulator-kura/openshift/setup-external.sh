01e53d0ee4483d71cdf616500bf64b9fa796ea27
Jens Reimann <jreimann@redhat.com>
2017-04-05 13:48:54 +0200

    allow overriding the image version

--- simulator-kura/openshift/setup-external.sh
+++ simulator-kura/openshift/setup-external.sh
@@ -15,7 +15,9 @@ set -e
 : ${OPENSHIFT_PROJECT_NAME:=kura-simulator}
 : ${DOCKER_HUB_ACCOUNT:=ctron}
 : ${BROKER_URL:=tcp://kapua-broker:kapua-password@localhost:1883}
+: ${IMAGE_VERSION:=0.1.2}
 
 # Set up new simulator instance
 
-oc new-app -n "$OPENSHIFT_PROJECT_NAME" -f external-template.yml -p "DOCKER_ACCOUNT=$DOCKER_HUB_ACCOUNT" -p "BROKER_URL=$BROKER_URL" -p "IMAGE_VERSION=0.1.2"
+oc new-app -n "$OPENSHIFT_PROJECT_NAME" -f external-template.yml -p "DOCKER_ACCOUNT=$DOCKER_HUB_ACCOUNT" -p "BROKER_URL=$BROKER_URL" -p "IMAGE_VERSION=$IMAGE_VERSION"
+
