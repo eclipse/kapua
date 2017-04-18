e18092728e13b853d2d09b62bb8ff1638d84454e
coduz <alberto.codutti@eurotech.com>
2017-04-18 15:32:52 +0200

    Added REST api support to retrieve channel list
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/DeviceManagementAssets.java
+++ rest-api/src/main/java/org/eclipse/kapua/app/api/v1/resources/DeviceManagementAssets.java
@@ -19,8 +19,13 @@ import org.eclipse.kapua.app.api.v1.resources.model.ScopeId;
 import org.eclipse.kapua.locator.KapuaLocator;
 import org.eclipse.kapua.service.device.management.asset.DeviceAssetManagementService;
 import org.eclipse.kapua.service.device.management.asset.DeviceAssets;
+import org.eclipse.kapua.service.device.management.channel.DeviceChannel;
+import org.eclipse.kapua.service.device.management.channel.DeviceChannels;
 import org.eclipse.kapua.service.device.registry.Device;
 
+import java.util.List;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -35,7 +40,7 @@ import javax.ws.rs.core.Response;
 public class DeviceManagementAssets extends AbstractKapuaResource {
 
     private final KapuaLocator locator = KapuaLocator.getInstance();
-    private final DeviceAssetManagementService assetService = locator.getService(DeviceAssetManagementService.class);
+    private final DeviceAssetManagementService deviceManagementAssetService = locator.getService(DeviceAssetManagementService.class);
 
     /**
      * Returns the list of all the Assets configured on the device.
@@ -54,58 +59,38 @@ public class DeviceManagementAssets extends AbstractKapuaResource {
             @ApiParam(value = "The timeout of the operation in milliseconds") @QueryParam("timeout") Long timeout) {
         DeviceAssets deviceAssets = null;
         try {
-            deviceAssets = assetService.get(scopeId, deviceId, timeout);
+            deviceAssets = deviceManagementAssetService.get(scopeId, deviceId, timeout);
         } catch (Throwable t) {
             handleException(t);
         }
         return returnNotNullEntity(deviceAssets);
     }
-
-//    /**
-//     * Starts the asset
-//     *
-//     * @param scopeId  The {@link ScopeId} of the {@link Device}.
-//     * @param deviceId The {@link Device} ID.
-//     * @param assetId the ID of the asset to start
-//     * @param timeout  The timeout of the operation in milliseconds
-//     * @return HTTP 200 if operation has completed successfully.
-//     */
-//    @POST
-//    @Path("{assetId}/_start")
-//    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-//    public Response start(
-//            @ApiParam(value = "The ScopeId of the device.", required = true, defaultValue = DEFAULT_SCOPE_ID) @PathParam("scopeId") ScopeId scopeId,
-//            @ApiParam(value = "The id of the device", required = true) @PathParam("deviceId") EntityId deviceId,
-//            @ApiParam(value = "the ID of the asset to start", required = true) @PathParam("assetId") String assetId,
-//            @ApiParam(value = "The timeout of the operation in milliseconds") @QueryParam("timeout") Long timeout) {
-//        try {
-//            assetService.start(scopeId, deviceId, assetId, timeout);
-//        } catch (Throwable t) {
-//            handleException(t);
-//        }
-//        return Response.ok().build();
-//    }
-//
-//    /**
-//     * Stops the asset
-//     *
-//     * @param deviceId The {@link Device} ID.
-//     * @param assetId the ID of the asset to stop
-//     * @return HTTP 200 if operation has completed successfully.
-//     */
-//    @POST
-//    @Path("{assetId}/_stop")
-//    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-//    public Response stop(
-//            @ApiParam(value = "The ScopeId of the device.", required = true, defaultValue = DEFAULT_SCOPE_ID) @PathParam("scopeId") ScopeId scopeId,
-//            @ApiParam(value = "The id of the device", required = true) @PathParam("deviceId") EntityId deviceId,
-//            @ApiParam(value = "the ID of the asset to stop", required = true) @PathParam("assetId") String assetId,
-//            @ApiParam(value = "The timeout of the operation in milliseconds") @QueryParam("timeout") Long timeout) {
-//        try {
-//            assetService.stop(scopeId, deviceId, assetId, timeout);
-//        } catch (Throwable t) {
-//            handleException(t);
-//        }
-//        return Response.ok().build();
-//    }
+    
+    
+    /**
+     * Returns the list of all the Channels of the given asset.
+     *
+     * @param scopeId  The {@link ScopeId} of the {@link Device}.
+     * @param deviceId The id of the device
+     * @param timeout  The timeout of the operation in milliseconds
+     * @return The list of Assets
+     */
+    @GET
+    @Path("{assetName}")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @ApiOperation(value = "Gets a list of channels for an asset", notes = "Returns the list of all the channels available for the given asset name.", response = DeviceChannel.class)
+    public DeviceChannels getChannels(
+            @ApiParam(value = "The ScopeId of the device.", required = true, defaultValue = DEFAULT_SCOPE_ID) @PathParam("scopeId") ScopeId scopeId,
+            @ApiParam(value = "The id of the device", required = true) @PathParam("deviceId") EntityId deviceId,
+            @ApiParam(value = "The asset name", required = true) @PathParam("assetName") String assetName,
+            @ApiParam(value = "The channel name to retrieve. If not specified all channel will be retrieved", allowMultiple = true) @QueryParam("channelName") List<String> channelNames,
+            @ApiParam(value = "The timeout of the operation in milliseconds") @QueryParam("timeout") Long timeout) {
+        DeviceChannels deviceChannels = null;
+        try {
+            deviceChannels = deviceManagementAssetService.getChannels(scopeId, deviceId, assetName, channelNames, timeout);
+        } catch (Throwable t) {
+            handleException(t);
+        }
+        return returnNotNullEntity(deviceChannels);
+    }
 }
