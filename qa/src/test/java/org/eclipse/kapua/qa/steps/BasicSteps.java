61fdd2b074127bb57924f065fef0cf9f21431146
Jens Reimann <jreimann@redhat.com>
2017-04-25 15:35:07 +0200

    Add a message for the wait multiplier

--- qa/src/test/java/org/eclipse/kapua/qa/steps/BasicSteps.java
+++ qa/src/test/java/org/eclipse/kapua/qa/steps/BasicSteps.java
@@ -13,13 +13,26 @@ package org.eclipse.kapua.qa.steps;
 
 import static java.time.Duration.ofSeconds;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cucumber.api.java.Before;
 import cucumber.api.java.en.When;
 import cucumber.runtime.java.guice.ScenarioScoped;
 
 @ScenarioScoped
 public class BasicSteps {
 
+    private static final Logger logger = LoggerFactory.getLogger(BasicSteps.class);
+
     private static final double WAIT_MULTIPLIER = Double.parseDouble(System.getProperty("org.eclipse.kapua.qa.waitMultiplier", "1.0"));
+
+    @Before
+    public void checkWaitMultipier() {
+        if (WAIT_MULTIPLIER != 1.0d) {
+            logger.info("Wait multiplier active: {}", WAIT_MULTIPLIER);
+        }
+    }
 
     @When("I wait (\\d+) seconds?.*")
     public void waitSeconds(int seconds) throws InterruptedException {
