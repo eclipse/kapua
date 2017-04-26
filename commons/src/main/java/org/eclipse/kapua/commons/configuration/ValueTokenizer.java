da3d23647a2b21a7e3d0f3420d69916895d6ad57
Jens Reimann <jreimann@redhat.com>
2017-04-26 16:35:53 +0200

    Fix copyright header, this came from IBM

--- commons/src/main/java/org/eclipse/kapua/commons/configuration/ValueTokenizer.java
+++ commons/src/main/java/org/eclipse/kapua/commons/configuration/ValueTokenizer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ * Copyright (c) 2005, 2016 IBM Corporation and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Eurotech - initial API and implementation
+ *     IBM Corporation - initial API and implementation
+ *     Eurotech
  *******************************************************************************/
 package org.eclipse.kapua.commons.configuration;
 
