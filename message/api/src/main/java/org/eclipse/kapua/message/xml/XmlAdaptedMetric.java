3c11ee40bf9be7131b51324dad3dab53bb07c59b
coduz <alberto.codutti@eurotech.com>
2017-04-28 14:59:47 +0200

    Dropped specific Metric Type converters in favour of generics ones on Service API model
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- message/api/src/main/java/org/eclipse/kapua/message/xml/XmlAdaptedMetric.java
+++ message/api/src/main/java/org/eclipse/kapua/message/xml/XmlAdaptedMetric.java
@@ -13,50 +13,49 @@ package org.eclipse.kapua.message.xml;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.eclipse.kapua.model.type.ObjectValueConverter;
+import org.eclipse.kapua.model.xml.XmlAdaptedNameTypeValueObject;
 
 @XmlRootElement(name = "metric")
 @XmlAccessorType(XmlAccessType.PROPERTY)
-public class XmlAdaptedMetric {
-
-    private String name;
-    private Class<?> type;
-    private String value;
-
-    public XmlAdaptedMetric() {
-    }
-
-    @XmlElement(name = "name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @XmlElement(name = "type")
-    @XmlJavaTypeAdapter(MetricTypeXmlAdapter.class)
-    public Class<?> getType() {
-        return type;
-    }
-
-    public void setType(Class<?> type) {
-        this.type = type;
-    }
-
-    @XmlElement(name = "value")
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+public class XmlAdaptedMetric extends XmlAdaptedNameTypeValueObject {
+    //
+    // private String name;
+    // private Class<?> type;
+    // private String value;
+    //
+    // public XmlAdaptedMetric() {
+    // }
+    //
+    // @XmlElement(name = "name")
+    // public String getName() {
+    // return name;
+    // }
+    //
+    // public void setName(String name) {
+    // this.name = name;
+    // }
+    //
+    // @XmlElement(name = "type")
+    // @XmlJavaTypeAdapter(ObjectTypeXmlAdapter.class)
+    // public Class<?> getType() {
+    // return type;
+    // }
+    //
+    // public void setType(Class<?> type) {
+    // this.type = type;
+    // }
+    //
+    // @XmlElement(name = "value")
+    // public String getValue() {
+    // return value;
+    // }
+    //
+    // public void setValue(String value) {
+    // this.value = value;
+    // }
 
     public Object getCastedValue() {
         return ObjectValueConverter.fromString(getValue(), getType());
