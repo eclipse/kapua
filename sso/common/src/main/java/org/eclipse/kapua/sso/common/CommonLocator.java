50370eef0dbdac9a76284c037e39be2941d84f1b
Jens Reimann <jreimann@redhat.com>
2017-05-08 17:39:11 +0200

    Provide an abstraction layer for SSO

---
+++ sso/common/src/main/java/org/eclipse/kapua/sso/common/CommonLocator.java
@@ -0,0 +1,14 @@
+package org.eclipse.kapua.sso.common;
+
+import org.eclipse.kapua.sso.SingleSignOnProvider;
+
+public class CommonLocator implements AutoCloseable {
+
+    public SingleSignOnProvider getProvider() {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+}
