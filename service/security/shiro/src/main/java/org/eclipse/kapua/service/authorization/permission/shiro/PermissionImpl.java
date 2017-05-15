f6ad9d830c52062cd313885e0a25fecc836f8010
coduz <alberto.codutti@eurotech.com>
2017-05-15 16:13:00 +0200

    Added javadoc to explain the process of validating a forward-able permission
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/permission/shiro/PermissionImpl.java
+++ service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/permission/shiro/PermissionImpl.java
@@ -189,17 +189,29 @@ public class PermissionImpl extends WildcardPermission implements Permission, or
 
     /**
      * This methods needs to be overridden to support Access {@link Group} feature.<br>
-     *
+     * 
+     * <p>
      * {@link KapuaEntityService}s that access a specific {@link KapuaEntity} (i.e. {@link KapuaEntityService#create(KapuaEntityCreator)}, {@link KapuaEntityService#delete(KapuaId, KapuaId)})
      * can make the control taking in consideration of the {@link Group#getId()} parameter as it is known.<br>
-     *
+     * 
      * Instead, methods that access multiple {@link KapuaEntity}s (i.e. {@link KapuaEntityService#query(KapuaQuery)}, {@link KapuaEntityService#count(KapuaQuery)})
      * cannot make a direct control of the {@link Group#getId()} parameter as it is not known and they can be a lot.<br>
      * The access control then, is performed by hiding the data that a {@link Subject} cannot see instead of throwing {@link UnauthorizedException}.
-     *
+     * </p>
+     * 
+     * <p>
      * The access control for {@link KapuaEntityService#query(KapuaQuery)}, {@link KapuaEntityService#count(KapuaQuery)}) must specify that {@link Group#ANY} group assigned to the permission is
      * enough to pass the {@link AuthorizationService#checkPermission(Permission)}.
-     *
+     * </p>
+     * 
+     * <p>
+     * In case of the {@link Permission#getForwardable()} equals to {@code true}, more lookup is required.<br>
+     * If a parent account access the resources of one of its child accounts it won't have the direct permission to access it.
+     * A lookup of {@link Account#getParentAccountPath()} will be required to search if the current user scope id is
+     * one of the parent of the given {@link Permission#getTargetScopeId()}
+     * </p>
+     * 
+     * @since 1.0.0
      */
     @Override
     public boolean implies(org.apache.shiro.authz.Permission p) {
@@ -215,13 +227,41 @@ public class PermissionImpl extends WildcardPermission implements Permission, or
         boolean implies = super.implies(p);
 
         if (!implies && getForwardable()) {
-            implies = forwardPermission(p, permission);
+            implies = forwardPermission(p);
         }
 
         return implies;
     }
 
-    private boolean forwardPermission(org.apache.shiro.authz.Permission p, Permission permission) {
+    /**
+     * Checks {@code this} Permission against the given {@link Permission} parameter.<br>
+     * 
+     * <p>
+     * It tries to forward {@code this} Permission to the {@link #getTargetScopeId()} of the given {@link org.apache.shiro.authz.Permission} parameter.<br>
+     * This means that if the required permission has scope id 'B' and {@code this} {@link Permission} has scope id 'A',
+     * this methods search the {@link Account#getParentAccountPath()} of the scope id 'B' and checks the {@link Permission} forwarding {@code this} Permission
+     * to the same level of the given {@link org.apache.shiro.authz.Permission}.
+     * </p>
+     * 
+     * <p>
+     * <b>Example:</b>
+     * User 'A' in account 'A' has scopeId 'A' and this permission (A) "*:*:A:*".<br>
+     * Account 'A' has a child account 'B', then 'B' has this parent account path: '/A/B';<br>
+     * <br>
+     * User 'A' tries to access a resource of account 'B' an the direct check {@link org.apache.shiro.authz.Permission#implies(org.apache.shiro.authz.Permission)} fails.
+     * So this method searches the parent account path of account 'B', found that 'A' is a parent of 'B'
+     * so then {@code this} {@link Permission} is checked again with 'B' as scopeId.
+     * </p>
+     * 
+     * @param p
+     *            The permission to check against.
+     * @return {@code true} if this permission is forward-able and is valid when forwarded, {@code false otherwise}
+     * 
+     * @since 1.0.0
+     */
+    private boolean forwardPermission(org.apache.shiro.authz.Permission p) {
+        Permission permission = (Permission) p;
+
         try {
             Account account = KapuaSecurityUtils.doPrivileged(() -> accountService.find(permission.getTargetScopeId()));
 
@@ -237,7 +277,7 @@ public class PermissionImpl extends WildcardPermission implements Permission, or
                 }
             }
         } catch (KapuaException e) {
-            throw KapuaRuntimeException.internalError("Error while forwarding permission: " + p.toString());
+            throw KapuaRuntimeException.internalError(e, "Error while forwarding permission: " + p.toString());
         }
 
         return false;
