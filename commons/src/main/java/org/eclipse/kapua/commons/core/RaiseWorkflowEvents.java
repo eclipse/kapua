2c576224d2f3960a92e565018adf8ca9e9c05fb3
riccardomodanese <riccardo.modanese@eurotech.com>
2017-05-16 14:23:23 +0200

    fix workflow interceptor
    Signed-off-by: riccardomodanese <riccardo.modanese@eurotech.com>

--- commons/src/main/java/org/eclipse/kapua/commons/core/RaiseWorkflowEvents.java
+++ commons/src/main/java/org/eclipse/kapua/commons/core/RaiseWorkflowEvents.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,7 +16,38 @@ import java.lang.annotation.Target;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.ElementType;
 
+/**
+ * Workflow event definition
+ * 
+ * @since 1.0
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME) 
 @Target(ElementType.METHOD)
 public @interface RaiseWorkflowEvents {
+
+    /**
+     * Tell when the action should be executed
+     *
+     */
+    enum ACTION_PERFORMED_ON {
+        /**
+         * Before method call
+         */
+        BEFORE,
+        /**
+         * After method call
+         */
+        AFTER,
+        /**
+         * After only if the method execution fails
+         */
+        AFTER_IF_FAIL,
+        /**
+         * After only if the method execution not fails
+         */
+        AFTER_IF_SUCCESS
+    };
+
+    ACTION_PERFORMED_ON eventActionPerformedOn();
 }
--- commons/src/main/java/org/eclipse/kapua/commons/core/RaiseWorkflowInterceptor.java
+++ commons/src/main/java/org/eclipse/kapua/commons/core/RaiseWorkflowInterceptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,47 +13,73 @@ package org.eclipse.kapua.commons.core;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.eclipse.kapua.commons.core.RaiseWorkflowEvents.ACTION_PERFORMED_ON;
 import org.eclipse.kapua.locator.KapuaProvider;
 import org.eclipse.kapua.service.KapuaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Workflow event interceptor
+ * 
+ * @since 1.0
+ */
 @KapuaProvider
 @InterceptorBind(matchSublclassOf=KapuaService.class, matchAnnotatedWith=RaiseWorkflowEvents.class)
-public class RaiseLogEntry implements MethodInterceptor {
+public class RaiseWorkflowInterceptor implements MethodInterceptor {
 
-    private static final Logger logger = LoggerFactory.getLogger(RaiseLogEntry.class);
+    private static final Logger logger = LoggerFactory.getLogger(RaiseWorkflowInterceptor.class);
 
-    public RaiseLogEntry() {
-        logger.info("***** Interceptor {} created !!", RaiseLogEntry.class.getName());        
+    public RaiseWorkflowInterceptor() {
+        logger.info("***** Interceptor {} created !!", RaiseWorkflowInterceptor.class.getName());
     }
     
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        logger.info("***** Intercepted method before execution {}.{}(...)", invocation.getMethod().getDeclaringClass(), invocation.getMethod().getName());
-
-        Object obj = null;
-        Throwable throwable = null;
-        try {
-            obj = invocation.proceed();
-        } catch (Throwable t) {
-            throwable = t;
-        }        
-
-        String result = "succesful";
-        if (throwable!=null) {
-            result = "failed (with throw)";
-        }
+        // cannot be null since "matchAnnotatedWith=RaiseWorkflowEvents.class"
+        RaiseWorkflowEvents raiseWorkflowEvents = invocation.getMethod().getAnnotation(RaiseWorkflowEvents.class);
+        logger.info("***** Intercepted method before execution {}.{}(...) - performed on: {}",
+                new Object[] { invocation.getMethod().getDeclaringClass(), invocation.getMethod().getName(), raiseWorkflowEvents.eventActionPerformedOn() });
         
+        Object returnObject = null;
+        Throwable executionThrowable = null;
+
+        if (ACTION_PERFORMED_ON.BEFORE.equals(raiseWorkflowEvents.eventActionPerformedOn())) {
+            logger.debug("Send event before method call");
+            sendEvent(returnObject, raiseWorkflowEvents, invocation.getArguments());
+        }
+
+        try {
+            returnObject = invocation.proceed();
+        } catch (Throwable t) {
+            executionThrowable = t;
+        }
+
+        if (ACTION_PERFORMED_ON.AFTER.equals(raiseWorkflowEvents.eventActionPerformedOn())) {
+            logger.debug("Send event after method call");
+            sendEvent(returnObject, raiseWorkflowEvents, invocation.getArguments());
+        } else if (ACTION_PERFORMED_ON.AFTER_IF_FAIL.equals(raiseWorkflowEvents.eventActionPerformedOn()) && executionThrowable != null) {
+            logger.debug("Send event after method call failure");
+            sendEvent(returnObject, raiseWorkflowEvents, invocation.getArguments());
+        } else if (ACTION_PERFORMED_ON.AFTER_IF_SUCCESS.equals(raiseWorkflowEvents.eventActionPerformedOn()) && executionThrowable == null) {
+            logger.debug("Send event after method call success");
+            sendEvent(returnObject, raiseWorkflowEvents, invocation.getArguments());
+        }
+
         logger.info("***** Intercepted method after {} execution {}.{}(...)", 
-                result,
+                (executionThrowable != null ? String.format("failed (with exception) [%s]", executionThrowable.getMessage()) : "successful"),
                 invocation.getMethod().getDeclaringClass(), 
                 invocation.getMethod().getName());
 
-        if (throwable!=null) {
-           throw throwable;
+        if (executionThrowable != null) {
+            throw executionThrowable;
         }
 
-        return obj;
+        return returnObject;
     }
+
+    private void sendEvent(Object returnedValue, RaiseWorkflowEvents event, Object[] arguments) {
+
+    }
+
 }
