fd92b54c3323a00b4eabdba4e7b70cc8cf1b8551
Leonardo Gaube <leonardo.gaube@comtrade.com>
2017-06-06 15:28:56 +0200

    Added tests for password check and email check
    Added tests for checking passwords (length, permitted symbols...) and emails (format, length, symbols...).
    
    Signed-off-by: Leonardo Gaube <leonardo.gaube@comtrade.com>

--- commons/src/test/java/org/eclipse/kapua/commons/util/ArgumentValidatorTest.java
+++ commons/src/test/java/org/eclipse/kapua/commons/util/ArgumentValidatorTest.java
@@ -11,6 +11,8 @@
  *******************************************************************************/
 package org.eclipse.kapua.commons.util;
 
+import java.util.Random;
+
 import org.eclipse.kapua.KapuaIllegalArgumentException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -79,8 +81,8 @@ public class ArgumentValidatorTest extends Assert {
         String[] listOfPermittedStringsName = new String[]{permittedSymbols,"aBc1-_","acbd","1234","-_-_-"};
         int sizeOfPermittedStrings = listOfPermittedStringsName.length;
         // Negative tests
-        for(int i=0;i<sizeOfFalseStrings;i++){
-            try{
+        for (int i = 0; i < sizeOfFalseStrings; i++) {
+            try {
                 ArgumentValidator.match(listOfFalseStringsName[i], argRegExprName, "NAME_REGEXP_test_case");
                 fail("Exception expected for: " + listOfFalseStringsName[i]);
             } catch (KapuaIllegalArgumentException e) {
@@ -88,16 +90,14 @@ public class ArgumentValidatorTest extends Assert {
             }
         }
         // Positive tests
-        for(int i=0; i<sizeOfPermittedStrings; i++) {
-            try{
+        for (int i = 0; i < sizeOfPermittedStrings; i++) {
+            try {
                 ArgumentValidator.match(listOfPermittedStringsName[i], argRegExprName, "NAME_REGEXP_test_case");
-            }
-            catch(Exception ex){
+            } catch (Exception ex) {
                 fail("No exception expected for: " + listOfPermittedStringsName[i]);
             }
         }
     }
-    
 
     @Test
     public void testIllegalCharacterNameSpaceRegExp() throws Exception {
@@ -129,6 +129,56 @@ public class ArgumentValidatorTest extends Assert {
             }
             catch(Exception ex){
                 fail("No exception expected for: " + listOfPermittedStringsNameSpace[i]);
+            }
+        }
+    }
+    
+    @Test
+    public void testIllegalCharacterEmailRegExp() throws Exception {
+        String argRegExprEmailRegExp = "^(\\w+)([-+.][\\w]+)*@(\\w[-\\w]*\\.){1,5}([A-Za-z]){2,4}$";
+        StringBuilder namePart = new StringBuilder();
+        Random random = new Random();
+        String permittedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
+        for (int i = 0; i < 500; i++) {
+            namePart.append(permittedSymbols.charAt(random.nextInt(permittedSymbols.length())));
+        }
+        String longname = namePart.toString();
+        String[] listOfFalseStringsEmailRegExp = new String[] {"abc!.ABC_123@co.co","abc\".ABC_123@co.co",
+                "abc#.ABC_123@co.co","abc$.ABC_123@co.co","abc%.ABC_123@co.co","abc&.ABC_123@co.co","abc'.ABC_123@co.co",
+                "abc(.ABC_123@co.co","abc).ABC_123@co.co","abc=.ABC_123@co.co","abc?.ABC_123@co.co","abc*.ABC_123@co.co",
+                "abc:.ABC_123@co.co","abc;.ABC_123@co.co","abc¶.ABC_123@co.co","abc•.ABC_123@co.co","abcº.ABC_123@co.co",
+                "abc<.ABC_123@co.co","abc>.ABC_123@co.co","abc¡.ABC_123@co.co","abc™.ABC_123@co.co","abc[.ABC_123@co.co",
+                "abc].ABC_123@co.co","abc{.ABC_123@co.co","abc}.ABC_123@co.co","abc^.ABC_123@co.co","abcÈ.ABC_123@co.co",
+                "abc~.ABC_123@co.co","abc±.ABC_123@co.co","abc=.ABC_123@co.co","abc£.ABC_123@co.co","abc¢.ABC_123@co.co",
+                "abc∞.ABC_123@co.co","abc≤.ABC_123@co.co","abc≥.ABC_123@co.co","abc§.ABC_123@co.co","abc•.ABC_123@co.co",
+                "abc.ABC_123@co.co","abcÒ.ABC_123@co.co","abc«.ABC_123@co.co","abc◊.ABC_123@co.co","abcÑ.ABC_123@co.co",
+                "abcˆ.ABC_123@co.co","abc¯.ABC_123@co.co","abcÉ.ABC_123@co.co","abcØ.ABC_123@co.co","abc∏.ABC_123@co.co",
+                "abcÅ.ABC_123@co.co","abcÍ.ABC_123@co.co","abcÔ.ABC_123@co.co","abc¿.ABC_123@co.co","abcŒ.ABC_123@co.co",
+                "abc\\.ABC_123@co.co","abc„.ABC_123@co.co","abc‘.ABC_123@co.co","abc’.ABC_123@co.co","abc‚.ABC_123@co.co",
+                "abcæ.ABC_123@co.co","abcÆ.ABC_123@co.co","abc|.ABC_123@co.co","abcÈ.ABC_123@co.co","abcˇ.ABC_123@co.co",
+                "abc‹.ABC_123@co.co","abc›.ABC_123@co.co","abc€.ABC_123@co.co","abcı.ABC_123@co.co","abc–.ABC_123@co.co",
+                "abc°.ABC_123@co.co","abc·.ABC_123@co.co","abc.ABC_123@a.B._.1.-.C.co","abc@to.12.TO._-.ab.to.co",
+                "abc@com","abc@co.c","@co.co","@com.com","ab.@co.co","abc.ABC_123@abc.toolong"};
+        int sizeOfFalseStrings = listOfFalseStringsEmailRegExp.length;
+        String[] listOfPermittedStringsEmailRegExp = new String[] {"a@co.co","ab@co.co","abc@co.co","a.a@co.co","ab.ab@co.co",
+                "abc.abc@co.co", "1@co.co", "12@co.co", "123@co.co", "1.1@co.co", "12.12@co.co", "123.123@co.co", "a.1@co.co",
+                "abc.123@co.co", "abc.ABC-123+_@co.co", "ab.AB+12-_@12.co", "ab.AB+12-_@co12.co", "ab.AB+12-_@1.co", "ab.AB+12-_@12.co",
+                "ab.AB+12-_@123.co", "ab.AB+12-_@aB123.co", "ab.AB+12-_@ab-12.co", "abc123ABC@ABcde.coms", "aA1-_.aA1-_@a.A.1._1aB.co",
+                longname + "@co.co", "a.a.a.a.a.a.a.a.a.a@co.co", "abc.ABC_123@A12bc_.ab.12c.1234abc_.co", "abc.ABC_123@a.ab.co", };
+        int sizeOPermittedStrings = listOfPermittedStringsEmailRegExp.length;
+        for (int i = 0; i < sizeOfFalseStrings; i++) {
+            try {
+                ArgumentValidator.match(listOfFalseStringsEmailRegExp[i], argRegExprEmailRegExp, "EMAIL_test_case");
+                fail("Exception expected for: " + listOfFalseStringsEmailRegExp[i]);
+            } catch (KapuaIllegalArgumentException ex) {
+                // Expected
+            }
+        }
+        for (int i = 0; i < sizeOPermittedStrings; i++) {
+            try {
+                ArgumentValidator.match(listOfPermittedStringsEmailRegExp[i], argRegExprEmailRegExp, "EMAIL_test_case");
+            } catch (Exception ex) {
+                fail("No exception expected for: " + listOfPermittedStringsEmailRegExp[i]);
             }
         }
     }
