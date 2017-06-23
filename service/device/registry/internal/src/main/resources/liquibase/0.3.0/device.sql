2fb2752dd87893a72bd6cd1c67f2b3784326fba0
coduz <alberto.codutti@eurotech.com>
2017-06-23 16:48:37 +0200

    Added new sql script
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ service/device/registry/internal/src/main/resources/liquibase/0.3.0/device.sql
@@ -0,0 +1,21 @@
+-- *******************************************************************************
+-- Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+--
+-- All rights reserved. This program and the accompanying materials
+-- are made available under the terms of the Eclipse Public License v1.0
+-- which accompanies this distribution, and is available at
+-- http://www.eclipse.org/legal/epl-v10.html
+--
+-- Contributors:
+--     Eurotech - initial API and implementation
+-- *******************************************************************************
+
+-- liquibase formatted sql
+
+-- changeset device:1
+
+ALTER TABLE dvc_device DROP INDEX idx_device_preferred_user_id;
+
+ALTER TABLE dvc_device DROP COLUMN credentials_mode;
+
+ALTER TABLE dvc_device DROP COLUMN preferred_user_id;
