21c1be614101d301274cabc0220c09278a456a39
coduz <alberto.codutti@eurotech.com>
2017-06-26 15:57:30 +0200

    Added device connection options API model
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/option/DeviceConnectionOptionCreator.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.device.registry.connection.option;
+
+import org.eclipse.kapua.model.KapuaUpdatableEntityCreator;
+import org.eclipse.kapua.model.id.KapuaId;
+import org.eclipse.kapua.service.device.registry.ConnectionUserCouplingMode;
+
+/**
+ * Device connection options creator service definition.
+ *
+ * @since 1.0
+ */
+public interface DeviceConnectionOptionCreator extends KapuaUpdatableEntityCreator<DeviceConnectionOption> {
+
+    /**
+     * Get the device connection user coupling mode.
+     *
+     * @return
+     */
+    public ConnectionUserCouplingMode getUserCouplingMode();
+
+    /**
+     * Set the device connection user coupling mode.
+     *
+     * @param status
+     */
+    public void setUserCouplingMode(ConnectionUserCouplingMode userCouplingMode);
+
+    /**
+     * Get the reserved user identifier
+     *
+     * @return
+     */
+    public KapuaId getReservedUserId();
+
+    /**
+     * Set the reserved user identifier
+     *
+     * @param reservedUserId
+     */
+    public void setReservedUserId(KapuaId reservedUserId);
+}
---
+++ service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/connection/option/DeviceConnectionOptionPredicates.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.device.registry.connection.option;
+
+import org.eclipse.kapua.model.KapuaUpdatableEntityPredicates;
+
+/**
+ * Device connection options query predicates.
+ * 
+ * @since 1.0.0
+ */
+public interface DeviceConnectionOptionPredicates extends KapuaUpdatableEntityPredicates {
+
+}
