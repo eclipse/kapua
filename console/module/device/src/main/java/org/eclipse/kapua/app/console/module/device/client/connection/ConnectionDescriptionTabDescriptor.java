84aabd6c2b45b9cd6bbea443445013cf443593e1
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2017-07-28 17:04:58 +0200

    ConnectionView TabDescriptors
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

---
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/connection/ConnectionDescriptionTabDescriptor.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.app.console.module.device.client.connection;
+
+import org.eclipse.kapua.app.console.commons.client.views.AbstractTabDescriptor;
+import org.eclipse.kapua.app.console.commons.shared.model.GwtSession;
+import org.eclipse.kapua.app.console.module.device.shared.model.GwtDeviceConnection;
+
+public class ConnectionDescriptionTabDescriptor extends AbstractTabDescriptor<GwtDeviceConnection, ConnectionDescriptionTab, ConnectionView> {
+
+    @Override
+    public ConnectionDescriptionTab getTabViewInstance(ConnectionView view, GwtSession currentSession) {
+        return new ConnectionDescriptionTab();
+    }
+
+    @Override
+    public String getViewId() {
+        return "connection.description";
+    }
+
+    @Override
+    public Integer getOrder() {
+        return 100;
+    }
+
+    @Override
+    public Boolean isEnabled(GwtSession currentSession) {
+        return true;
+    }
+}
