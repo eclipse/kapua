1b123db9e540bc1f4f36c0747741bcc961898f92
coduz <alberto.codutti@eurotech.com>
2017-08-01 09:26:54 +0200

    Moved external module to be a separate maven project
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- dev-tools/src/main/bin/start-demo.sh
+++ dev-tools/src/main/bin/start-demo.sh
@@ -22,7 +22,7 @@ echo 'creating demo vagrant machine'
 
 cd $KAPUA_GIT_ROOT/../../../../
 
-mvn clean install -P deploy -D skipTests
+mvn clean install -Pdeploy,console -D skipTests
 
 cd dev-tools/src/main/vagrant
 
