5f48a6bf59fff0071a71e6f53ec5d40f71dcb915
coduz <alberto.codutti@eurotech.com>
2017-08-02 12:13:37 +0200

    Added SQL scripts
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ service/job/internal/src/main/resources/liquibase/0.3.0/job-configuration.sql
@@ -0,0 +1,59 @@
+-- *******************************************************************************
+-- Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+--
+-- All rights reserved. This program and the accompanying materials
+-- are made available under the terms of the Eclipse Public License v1.0
+-- which accompanies this distribution, and is available at
+-- http://www.eclipse.org/legal/epl-v10.html
+--
+-- Contributors:
+--     Eurotech - initial API and implementation
+-- *******************************************************************************
+
+-- liquibase formatted sql
+
+-- changeset job-configuration:1
+
+-- WARNING: to be kept in sync with kapua/commons/src/main/resources/liquibase/configuration.sql
+CREATE TABLE IF NOT EXISTS sys_configuration (
+  scope_id          		 BIGINT(21) 	  UNSIGNED,
+  id                         BIGINT(21) 	  UNSIGNED NOT NULL,
+  pid						 VARCHAR(255) 	  NOT NULL,
+  configurations			 TEXT,
+  created_on                 TIMESTAMP(3) 	  DEFAULT 0,
+  created_by                 BIGINT(21) 	  UNSIGNED NOT NULL,
+  modified_on                TIMESTAMP(3) 	  NOT NULL,
+  modified_by                BIGINT(21) 	  UNSIGNED NOT NULL,
+  optlock                    INT UNSIGNED,
+  attributes				 TEXT,
+  properties                 TEXT,
+  
+  PRIMARY KEY  (scope_id, id)
+  
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+INSERT INTO sys_configuration (
+	SCOPE_ID,
+    ID,
+    PID,
+    CONFIGURATIONS,
+    CREATED_ON,
+    CREATED_BY,
+    MODIFIED_ON,
+    MODIFIED_BY,
+    OPTLOCK,
+    ATTRIBUTES,
+    PROPERTIES)
+VALUES (1,
+        7,
+        'org.eclipse.kapua.service.job.JobService',
+        CONCAT('#', CURRENT_TIMESTAMP(), CHAR(13), CHAR(10),
+               'maxNumberChildEntities=0', CHAR(13), CHAR(10),
+               'infiniteChildEntities=true'),
+        CURRENT_TIMESTAMP(),
+        1,
+        CURRENT_TIMESTAMP(),
+        1,
+        0,
+        null,
+        null);
---
+++ service/job/internal/src/main/resources/liquibase/0.3.0/job_target.sql
@@ -0,0 +1,40 @@
+-- *******************************************************************************
+-- Copyright (c) 2017 Eurotech and/or its affiliates and others
+--
+-- All rights reserved. This program and the accompanying materials
+-- are made available under the terms of the Eclipse Public License v1.0
+-- which accompanies this distribution, and is available at
+-- http://www.eclipse.org/legal/epl-v10.html
+--
+-- Contributors:
+--     Eurotech - initial API and implementation
+-- *******************************************************************************
+
+-- liquibase formatted sql
+
+-- changeset job:1
+
+CREATE TABLE job_job_target (
+  scope_id          		BIGINT(21) 	  	UNSIGNED NOT NULL,
+  id                        BIGINT(21)		UNSIGNED NOT NULL,
+  created_on                TIMESTAMP(3)	DEFAULT 0,
+  created_by                BIGINT(21) 	  	UNSIGNED NOT NULL,
+  modified_on               TIMESTAMP(3)	NOT NULL,
+  modified_by               BIGINT(21)		UNSIGNED NOT NULL,
+  
+  name                      VARCHAR(255)	NOT NULL,
+
+  job_id          			BIGINT(21) 	  	UNSIGNED NOT NULL,
+  job_target_id          	BIGINT(21) 	  	UNSIGNED NOT NULL,
+  step_index	          	INT				UNSIGNED NOT NULL,
+  status					VARCHAR(64)		NOT NULL,
+  
+  optlock                   INT UNSIGNED,
+  attributes				TEXT,
+  properties                TEXT,
+  
+  PRIMARY KEY (id),
+  
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE INDEX idx_job_target_scope_id ON job_job_target (scope_id);
