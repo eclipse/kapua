b7e243dcde02c28252d3d05a05dc28ca49b9f690
coduz <alberto.codutti@eurotech.com>
2017-08-03 15:55:07 +0200

    Created a commons module to provide default implementations for job classes
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ job/commons/src/main/java/org/eclipse/kapua/service/job/commons/step/definition/AbstractTargetJobStepDefinition.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.job.commons.step.definition;
+
+import org.eclipse.kapua.commons.model.AbstractKapuaUpdatableEntity;
+import org.eclipse.kapua.service.job.commons.operation.DefaultTargetReader;
+import org.eclipse.kapua.service.job.commons.operation.DefaultTargetWriter;
+import org.eclipse.kapua.service.job.step.definition.JobStepDefinition;
+import org.eclipse.kapua.service.job.step.definition.JobStepType;
+
+public abstract class AbstractTargetJobStepDefinition extends AbstractKapuaUpdatableEntity implements JobStepDefinition {
+
+    private static final long serialVersionUID = 474195961081702478L;
+
+    @Override
+    public JobStepType getStepType() {
+        return JobStepType.TARGET;
+    }
+
+    @Override
+    public String getReaderName() {
+        return DefaultTargetReader.class.getName();
+    }
+
+    @Override
+    public String getWriterName() {
+        return DefaultTargetWriter.class.getName();
+    }
+
+}
