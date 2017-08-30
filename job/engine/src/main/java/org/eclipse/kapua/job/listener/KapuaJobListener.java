4a8008af7c910902bf293b871fb399586f0e401f
coduz <alberto.codutti@eurotech.com>
2017-08-30 16:22:59 +0200

    Added first tracking of Job Executions
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- job/engine/src/main/java/org/eclipse/kapua/job/listener/KapuaJobListener.java
+++ job/engine/src/main/java/org/eclipse/kapua/job/listener/KapuaJobListener.java
@@ -11,14 +11,22 @@
  *******************************************************************************/
 package org.eclipse.kapua.job.listener;
 
+import java.util.Date;
+
 import javax.batch.api.listener.AbstractJobListener;
 import javax.batch.api.listener.JobListener;
 import javax.batch.runtime.context.JobContext;
 import javax.inject.Inject;
 
+import org.eclipse.kapua.commons.security.KapuaSecurityUtils;
 import org.eclipse.kapua.locator.KapuaLocator;
+import org.eclipse.kapua.model.id.KapuaId;
 import org.eclipse.kapua.service.job.context.JobContextFactory;
 import org.eclipse.kapua.service.job.context.KapuaJobContext;
+import org.eclipse.kapua.service.job.execution.JobExecution;
+import org.eclipse.kapua.service.job.execution.JobExecutionCreator;
+import org.eclipse.kapua.service.job.execution.JobExecutionFactory;
+import org.eclipse.kapua.service.job.execution.JobExecutionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,16 +35,32 @@ public class KapuaJobListener extends AbstractJobListener implements JobListener
     private static final Logger LOG = LoggerFactory.getLogger(KapuaJobListener.class);
 
     private static final KapuaLocator LOCATOR = KapuaLocator.getInstance();
+
     private static final JobContextFactory JOB_CONTEXT_FACTORY = LOCATOR.getFactory(JobContextFactory.class);
+
+    private static final JobExecutionService JOB_EXECUTION_SERVICE = LOCATOR.getService(JobExecutionService.class);
+    private static final JobExecutionFactory JOB_EXECUTION_FACTORY = LOCATOR.getFactory(JobExecutionFactory.class);
 
     @Inject
     private JobContext jobContext;
+
+    private KapuaId jobExecutionId;
 
     @Override
     public void beforeJob() throws Exception {
         KapuaJobContext kapuaJobContext = JOB_CONTEXT_FACTORY.newJobContext(jobContext);
 
         LOG.info("JOB {} - Running before job...", kapuaJobContext.getJobId());
+
+        JobExecutionCreator jobExecutionCreator = JOB_EXECUTION_FACTORY.newCreator(kapuaJobContext.getScopeId());
+
+        jobExecutionCreator.setJobId(kapuaJobContext.getJobId());
+        jobExecutionCreator.setStartedOn(new Date());
+
+        JobExecution jobExecution = KapuaSecurityUtils.doPrivileged(() -> JOB_EXECUTION_SERVICE.create(jobExecutionCreator));
+
+        jobExecutionId = jobExecution.getId();
+
         LOG.info("JOB {} - Running before job... DONE!", kapuaJobContext.getJobId());
     }
 
@@ -45,6 +69,13 @@ public class KapuaJobListener extends AbstractJobListener implements JobListener
         KapuaJobContext kapuaJobContext = JOB_CONTEXT_FACTORY.newJobContext(jobContext);
 
         LOG.info("JOB {} - Running after job...", kapuaJobContext.getJobId());
+
+        JobExecution jobExecution = KapuaSecurityUtils.doPrivileged(() -> JOB_EXECUTION_SERVICE.find(kapuaJobContext.getScopeId(), jobExecutionId));
+
+        jobExecution.setEndedOn(new Date());
+
+        KapuaSecurityUtils.doPrivileged(() -> JOB_EXECUTION_SERVICE.update(jobExecution));
+
         LOG.info("JOB {} - Running after job... DONE!", kapuaJobContext.getJobId());
     }
 }
