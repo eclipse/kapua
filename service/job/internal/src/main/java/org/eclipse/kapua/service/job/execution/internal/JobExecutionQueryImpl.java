faf842a1599016c4f8e9a00c52ebf642f6431991
coduz <alberto.codutti@eurotech.com>
2017-08-30 16:26:03 +0200

    Implemented new JobExecutionsService
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ service/job/internal/src/main/java/org/eclipse/kapua/service/job/execution/internal/JobExecutionQueryImpl.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.job.execution.internal;
+
+import org.eclipse.kapua.commons.model.query.predicate.AbstractKapuaQuery;
+import org.eclipse.kapua.model.id.KapuaId;
+import org.eclipse.kapua.service.job.execution.JobExecution;
+import org.eclipse.kapua.service.job.execution.JobExecutionQuery;
+
+/**
+ * {@link JobExecutionDefinitionQuery} definition.
+ * 
+ * @since 1.0.0
+ * 
+ */
+public class JobExecutionQueryImpl extends AbstractKapuaQuery<JobExecution> implements JobExecutionQuery {
+
+    /**
+     * Constructor
+     *
+     * @param scopeId
+     */
+    public JobExecutionQueryImpl(KapuaId scopeId) {
+        super(scopeId);
+    }
+}
