43a4c307d40f15e08b659b37d43e8a5a9c820671
coduz <alberto.codutti@eurotech.com>
2017-09-04 14:31:11 +0200

    First implementation of the Kapua Scheduler API
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ service/scheduler/api/src/main/java/org/eclipse/kapua/service/scheduler/trigger/TriggerPredicates.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.scheduler.trigger;
+
+import org.eclipse.kapua.model.KapuaUpdatableEntityPredicates;
+
+public interface TriggerPredicates extends KapuaUpdatableEntityPredicates {
+
+    /**
+     * The {@link Trigger} name.
+     */
+    public static final String NAME = "name";
+}
