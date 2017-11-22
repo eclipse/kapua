d6cb38ded7157bbd480795a1e9199cccb099c20b
coduz <alberto.codutti@eurotech.com>
2017-11-22 10:59:39 +0100

    Fixed javadoc
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/security/authorization/api/src/main/java/org/eclipse/kapua/service/authorization/role/RoleCreator.java
+++ service/security/authorization/api/src/main/java/org/eclipse/kapua/service/authorization/role/RoleCreator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ * Copyright (c) 2016, 2017 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,8 +11,8 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.authorization.role;
 
-import java.security.Permissions;
-import java.util.Set;
+import org.eclipse.kapua.model.KapuaEntityCreator;
+import org.eclipse.kapua.service.authorization.permission.Permission;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -20,38 +20,35 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-
-import org.eclipse.kapua.model.KapuaEntityCreator;
-import org.eclipse.kapua.service.authorization.permission.Permission;
+import java.security.Permissions;
+import java.util.Set;
 
 /**
  * {@link Role} creator definition.<br>
  * It is used to create a new {@link Role} with {@link Permission}s associated
- * 
+ *
  * @since 1.0.0
  */
-@XmlRootElement(name="roleCreator")
+@XmlRootElement(name = "roleCreator")
 @XmlAccessorType(XmlAccessType.PROPERTY)
 @XmlType(propOrder = { "name",
-                      "permissions"
-                     }, 
-         factoryClass = RoleXmlRegistry.class,
-         factoryMethod = "newRoleCreator")
+        "permissions"
+},
+        factoryClass = RoleXmlRegistry.class,
+        factoryMethod = "newRoleCreator")
 public interface RoleCreator extends KapuaEntityCreator<Role> {
 
     /**
      * Sets the {@link Role} name.
-     * 
-     * @param name
-     *            The {@link Role} name.
-     * 
+     *
+     * @param name The {@link Role} name.
      * @since 1.0.0
      */
     public void setName(String name);
 
     /**
      * Gets the {@link Role} name.
-     * 
+     *
      * @return The {@link Role} name.
      * @since 1.0.0
      */
@@ -61,9 +58,8 @@ public interface RoleCreator extends KapuaEntityCreator<Role> {
     /**
      * Sets the set of {@link Permissions} to assign to the {@link Role} created entity.
      * It up to the implementation class to make a clone of the set or use the given set.
-     * 
-     * @param permissions
-     *            The set of {@link Permissions}.
+     *
+     * @param permissions The set of {@link Permissions}.
      * @since 1.0.0
      */
     public void setPermissions(Set<Permission> permissions);
@@ -71,7 +67,8 @@ public interface RoleCreator extends KapuaEntityCreator<Role> {
     /**
      * Gets the set of {@link Permission} added to this {@link Role}.
      * The implementation must return the reference of the set and not make a clone.
-     * 
+     *
+     * @param <P> The {@link Permission} class implementation.
      * @return The set of {@link Permission}.
      * @since 1.0.0
      */
