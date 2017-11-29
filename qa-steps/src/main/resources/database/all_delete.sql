33f1ea4c3f7999b97e7c508f0be3a468bd22f781
Uros Mesaric Kunst <uros.mesaric-kunst@comtrade.com>
2017-11-29 14:51:52 +0100

    Minor fix of all_delete with added jobs service.
    Some tests were failing because new domain was missing in integration test
    delete SQL script.
    
    Signed-off-by: Uros Mesaric Kunst <uros.mesaric-kunst@comtrade.com>

--- qa-steps/src/main/resources/database/all_delete.sql
+++ qa-steps/src/main/resources/database/all_delete.sql
@@ -18,7 +18,7 @@ DELETE FROM atht_access_token;
 
 DELETE FROM athz_role WHERE NOT (scope_id = 1 AND id = 1);
 
-DELETE FROM athz_role_permission WHERE NOT (scope_id = 1 AND role_id = 1 AND id IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16));
+DELETE FROM athz_role_permission WHERE NOT (scope_id = 1 AND role_id = 1 AND id IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17));
 
 DELETE FROM dvc_device_connection;
 
@@ -36,8 +36,8 @@ DELETE FROM athz_access_permission WHERE NOT (scope_id = 1 AND id = 1);
 
 DELETE FROM athz_access_role WHERE NOT (scope_id = 1 AND id = 1);
 
-DELETE FROM athz_domain_actions WHERE domain_id NOT IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+DELETE FROM athz_domain_actions WHERE domain_id NOT IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17);
 
-DELETE FROM athz_domain WHERE NOT (scope_id IS null AND id IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16));
+DELETE FROM athz_domain WHERE NOT (scope_id IS null AND id IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17));
 
 DELETE FROM athz_group;
