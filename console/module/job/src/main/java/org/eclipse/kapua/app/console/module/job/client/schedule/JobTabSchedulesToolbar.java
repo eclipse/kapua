b283689323f31f80e4b24575cb6d2db8ab0ede32
ct-goranpalibrk <30318334+ct-goranpalibrk@users.noreply.github.com>
2018-02-02 12:47:54 +0100

    add button in schedules disabled if no job is selected (#1354)
    * add button in schedules disabled if no job is selected
    
    Signed-off-by: CT\pgoran <goran.palibrk@comtrade.com>

--- console/module/job/src/main/java/org/eclipse/kapua/app/console/module/job/client/schedule/JobTabSchedulesToolbar.java
+++ console/module/job/src/main/java/org/eclipse/kapua/app/console/module/job/client/schedule/JobTabSchedulesToolbar.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -36,6 +36,13 @@ public class JobTabSchedulesToolbar extends EntityCRUDToolbar<GwtTrigger> {
     @Override
     protected void onRender(Element target, int index) {
         super.onRender(target, index);
+        if (jobId != null) {
+            addEntityButton.setEnabled(true);
+            refreshEntityButton.setEnabled(true);
+        } else {
+            addEntityButton.setEnabled(false);
+            refreshEntityButton.setEnabled(false);
+        }
     }
 
     @Override
