841ba27c200d53bf57281ec487eb173d34fcedea
mgogic <36038833+mgogic@users.noreply.github.com>
2018-02-06 15:40:59 +0100

    Fixed issue with 'x' button in Upload and Apply dialog in device section (#1370)
    Signed-off-by: mgogic <milan.gogic@comtrade.com>

--- console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/ui/dialog/FileUploadDialog.java
+++ console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/ui/dialog/FileUploadDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -61,6 +61,7 @@ public class FileUploadDialog extends Dialog {
         this.url = url;
         this.hiddenFields = hiddenFields;
         setButtonAlign(HorizontalAlignment.RIGHT);
+        setClosable(false);
     }
 
     @Override
