49b8ae9a8159bb993bb7895db737223b124ff9b9
coduz <alberto.codutti@eurotech.com>
2018-02-08 16:59:10 +0100

    Improved implementation of AndPredicate
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- commons/src/main/java/org/eclipse/kapua/commons/model/query/predicate/AndPredicate.java
+++ commons/src/main/java/org/eclipse/kapua/commons/model/query/predicate/AndPredicate.java
@@ -11,17 +11,19 @@
  *******************************************************************************/
 package org.eclipse.kapua.commons.model.query.predicate;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.google.common.collect.Lists;
 import org.eclipse.kapua.model.query.predicate.KapuaAndPredicate;
 import org.eclipse.kapua.model.query.predicate.KapuaPredicate;
 
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
- * Kapua sql and predicate reference implementation.
- * 
- * @since 1.0
+ * {@link KapuaAndPredicate} reference implementation.
  *
+ * @since 0.1.0
  */
 public class AndPredicate implements KapuaAndPredicate {
 
@@ -29,13 +31,37 @@ public class AndPredicate implements KapuaAndPredicate {
 
     /**
      * Constructor
+     *
+     * @since 0.1.0
      */
     public AndPredicate() {
-        this.predicates = new ArrayList<KapuaPredicate>();
+        setPredicates(new ArrayList<>());
     }
 
+    /**
+     * Constructor which accepts a not null array of {@link KapuaPredicate}s.
+     *
+     * @param predicates the {@link KapuaPredicate}s to add.
+     * @throws NullPointerException if the given parameter is {@code null}.
+     * @since 1.0.0
+     */
+    public AndPredicate(@NotNull KapuaPredicate... predicates) {
+        Objects.requireNonNull(predicates);
+
+        setPredicates(Lists.newArrayList(predicates));
+    }
+
+    /**
+     * Adds a new {@link KapuaPredicate} to this {@link KapuaAndPredicate}.
+     *
+     * @param predicate The {@link KapuaPredicate} to add
+     * @return {@code this} {@link AndPredicate}.
+     * @throws NullPointerException if the given parameter is {@code null}.
+     */
     @Override
-    public AndPredicate and(KapuaPredicate predicate) {
+    public AndPredicate and(@NotNull KapuaPredicate predicate) {
+        Objects.requireNonNull(predicates);
+
         this.predicates.add(predicate);
         return this;
     }
@@ -43,5 +69,9 @@ public class AndPredicate implements KapuaAndPredicate {
     @Override
     public List<KapuaPredicate> getPredicates() {
         return this.predicates;
+    }
+
+    private void setPredicates(List<KapuaPredicate> predicates) {
+        this.predicates = predicates;
     }
 }
