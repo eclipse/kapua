56a2aaa4b1b27ef359cf104b29e301bfcd626964
bojanutkovic <bojan.utkovic@comtrade.com>
2018-02-13 15:34:30 +0100

    access group combo fix (#1391)
    Signed-off-by: Bojan Utkovic <bojan.utkovic@comtrade.com>

--- console/module/authorization/src/main/java/org/eclipse/kapua/app/console/module/authorization/client/tabs/permission/PermissionAddDialog.java
+++ console/module/authorization/src/main/java/org/eclipse/kapua/app/console/module/authorization/client/tabs/permission/PermissionAddDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -200,10 +200,12 @@ public class PermissionAddDialog extends EntityAddEditDialog {
                         actionsCombo.setSimpleValue(allAction);
                         actionsCombo.enable();
 
-                        if (selectedDomain.getGroupable()) {
+                        if (allDomain.equals(selectedDomain)) {
+                            groupsCombo.setEnabled(true);
+                            groupsCombo.setValue(allGroup);
+                        } else if (selectedDomain.getGroupable()) {
                             groupsCombo.setEnabled(selectedDomain.getGroupable());
                             groupsCombo.setValue(allGroup);
-
                         } else {
                             groupsCombo.setEnabled(selectedDomain.getGroupable());
                             groupsCombo.setRawValue(MSGS.dialogAddPermissionGroupIdNotGroupable());
