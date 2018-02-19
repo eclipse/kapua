80c91330f6a79cff76da94ecb98fef4d9f6e3c9f
mgogic <milan.gogic@comtrade.com>
2018-02-19 15:18:19 +0100

    Fixed error if Tag Name too long
    Signed-off-by: mgogic <milan.gogic@comtrade.com>

--- console/module/tag/src/main/java/org/eclipse/kapua/app/console/module/tag/client/TagAddDialog.java
+++ console/module/tag/src/main/java/org/eclipse/kapua/app/console/module/tag/client/TagAddDialog.java
@@ -34,7 +34,7 @@ public class TagAddDialog extends EntityAddEditDialog {
 
     private final static GwtTagServiceAsync GWT_TAG_SERVICE = GWT.create(GwtTagService.class);
     private final static ConsoleTagMessages MSGS = GWT.create(ConsoleTagMessages.class);
-
+    private final static int MAX_FIELD_NAME_LENGTH = 255;
     protected TextField<String> tagNameField;
 
     public TagAddDialog(GwtSession currentSession) {
@@ -56,8 +56,12 @@ public class TagAddDialog extends EntityAddEditDialog {
     @Override
     public void submit() {
         GwtTagCreator gwtTagCreator = new GwtTagCreator();
+        StringBuilder sBuilder = new StringBuilder(tagNameField.getValue());
         gwtTagCreator.setScopeId(currentSession.getSelectedAccountId());
-        gwtTagCreator.setName(tagNameField.getValue());
+        if (tagNameField.getValue().length() > MAX_FIELD_NAME_LENGTH) {
+            sBuilder.delete(255, sBuilder.length());
+        }
+        gwtTagCreator.setName(sBuilder.toString());
         GWT_TAG_SERVICE.create(gwtTagCreator, new AsyncCallback<GwtTag>() {
 
             @Override
