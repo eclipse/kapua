7778bde305793fb8086749234ad906527994a850
coduz <alberto.codutti@eurotech.com>
2018-02-22 12:01:19 +0100

    Fixed command device management error handling
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- service/device/command/internal/src/main/java/org/eclipse/kapua/service/device/management/command/internal/DeviceCommandManagementServiceImpl.java
+++ service/device/command/internal/src/main/java/org/eclipse/kapua/service/device/management/command/internal/DeviceCommandManagementServiceImpl.java
@@ -11,10 +11,7 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.device.management.command.internal;
 
-import java.util.Date;
-
 import org.eclipse.kapua.KapuaException;
-import org.eclipse.kapua.commons.security.KapuaSecurityUtils;
 import org.eclipse.kapua.commons.util.ArgumentValidator;
 import org.eclipse.kapua.locator.KapuaLocator;
 import org.eclipse.kapua.locator.KapuaProvider;
@@ -27,29 +24,29 @@ import org.eclipse.kapua.service.device.management.KapuaMethod;
 import org.eclipse.kapua.service.device.management.command.DeviceCommandInput;
 import org.eclipse.kapua.service.device.management.command.DeviceCommandManagementService;
 import org.eclipse.kapua.service.device.management.command.DeviceCommandOutput;
+import org.eclipse.kapua.service.device.management.command.internal.exception.CommandExecuteManagementException;
 import org.eclipse.kapua.service.device.management.command.message.internal.CommandRequestChannel;
 import org.eclipse.kapua.service.device.management.command.message.internal.CommandRequestMessage;
 import org.eclipse.kapua.service.device.management.command.message.internal.CommandRequestPayload;
 import org.eclipse.kapua.service.device.management.command.message.internal.CommandResponseMessage;
 import org.eclipse.kapua.service.device.management.command.message.internal.CommandResponsePayload;
+import org.eclipse.kapua.service.device.management.commons.AbstractDeviceManagementServiceImpl;
 import org.eclipse.kapua.service.device.management.commons.DeviceManagementDomain;
 import org.eclipse.kapua.service.device.management.commons.call.DeviceCallExecutor;
-import org.eclipse.kapua.service.device.registry.event.DeviceEventCreator;
-import org.eclipse.kapua.service.device.registry.event.DeviceEventFactory;
-import org.eclipse.kapua.service.device.registry.event.DeviceEventService;
+import org.eclipse.kapua.service.device.management.response.KapuaResponsePayload;
+
+import java.util.Date;
 
 /**
- * Device command service implementation.
- * 
- * @since 1.0
+ * {@link DeviceCommandManagementService} implementation.
  *
+ * @since 1.0
  */
 @KapuaProvider
-public class DeviceCommandManagementServiceImpl implements DeviceCommandManagementService {
+public class DeviceCommandManagementServiceImpl extends AbstractDeviceManagementServiceImpl implements DeviceCommandManagementService {
 
     private static final Domain DEVICE_MANAGEMENT_DOMAIN = new DeviceManagementDomain();
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
     public DeviceCommandOutput exec(KapuaId scopeId, KapuaId deviceId, DeviceCommandInput commandInput, Long timeout)
             throws KapuaException {
@@ -99,30 +96,26 @@ public class DeviceCommandManagementServiceImpl implements DeviceCommandManageme
 
         //
         // Create event
-        DeviceEventService deviceEventService = locator.getService(DeviceEventService.class);
-        DeviceEventFactory deviceEventFactory = locator.getFactory(DeviceEventFactory.class);
-
-        DeviceEventCreator deviceEventCreator = deviceEventFactory.newCreator(scopeId, deviceId, responseMessage.getReceivedOn(), CommandAppProperties.APP_NAME.getValue());
-        deviceEventCreator.setPosition(responseMessage.getPosition());
-        deviceEventCreator.setSentOn(responseMessage.getSentOn());
-        deviceEventCreator.setAction(KapuaMethod.EXECUTE);
-        deviceEventCreator.setResponseCode(responseMessage.getResponseCode());
-        deviceEventCreator.setEventMessage(responseMessage.getPayload().toDisplayString());
-
-        KapuaSecurityUtils.doPrivileged(() -> deviceEventService.create(deviceEventCreator));
+        createDeviceEvent(scopeId, deviceId, commandRequestMessage, responseMessage);
 
         //
-        // Parse the response
-        CommandResponsePayload responsePayload = responseMessage.getPayload();
+        // Check response
+        if (responseMessage.getResponseCode().isAccepted()) {
+            CommandResponsePayload responsePayload = responseMessage.getPayload();
 
-        DeviceCommandOutput deviceCommandOutput = new DeviceCommandOutputImpl();
-        deviceCommandOutput.setExceptionMessage(responsePayload.getExceptionMessage());
-        deviceCommandOutput.setExceptionStack(responsePayload.getExceptionStack());
-        deviceCommandOutput.setExitCode(responsePayload.getExitCode());
-        deviceCommandOutput.setHasTimedout(false); // FIXME: implement track of timeout!!!
-        deviceCommandOutput.setStderr(responsePayload.getStderr());
-        deviceCommandOutput.setStdout(responsePayload.getStdout());
+            DeviceCommandOutput deviceCommandOutput = new DeviceCommandOutputImpl();
+            deviceCommandOutput.setExceptionMessage(responsePayload.getExceptionMessage());
+            deviceCommandOutput.setExceptionStack(responsePayload.getExceptionStack());
+            deviceCommandOutput.setExitCode(responsePayload.getExitCode());
+            deviceCommandOutput.setHasTimedout(false); // FIXME: implement track of timeout!!!
+            deviceCommandOutput.setStderr(responsePayload.getStderr());
+            deviceCommandOutput.setStdout(responsePayload.getStdout());
 
-        return deviceCommandOutput;
+            return deviceCommandOutput;
+        } else {
+            KapuaResponsePayload responsePayload = responseMessage.getPayload();
+
+            throw new CommandExecuteManagementException(responseMessage.getResponseCode(), responsePayload.getExceptionMessage(), responsePayload.getExceptionStack());
+        }
     }
 }
