e7873f5e89aa9bc04c5e529ece15e0cb4e4a0b89
bojanutkovic <bojan.utkovic@comtrade.com>
2018-02-28 09:27:25 +0100

    Required fields labels fixes (#1458)
    Required fields labels fixes.
    
    This fix to old UI issues that haven't been completed. Minor fix.
    
    Signed-off-by: Bojan Utkovic <bojan.utkovic@comtrade.com>

--- console/module/authorization/src/main/java/org/eclipse/kapua/app/console/module/authorization/client/tabs/role/AccessRoleAddDialog.java
+++ console/module/authorization/src/main/java/org/eclipse/kapua/app/console/module/authorization/client/tabs/role/AccessRoleAddDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -133,7 +133,7 @@ public class AccessRoleAddDialog extends EntityAddEditDialog {
         rolesCombo.setEditable(false);
         rolesCombo.setTypeAhead(false);
         rolesCombo.setAllowBlank(false);
-        rolesCombo.setFieldLabel(MSGS.dialogAddRoleComboName());
+        rolesCombo.setFieldLabel("* " + MSGS.dialogAddRoleComboName());
         rolesCombo.setTriggerAction(TriggerAction.ALL);
         rolesCombo.setStore(roleStore);
         rolesCombo.setDisplayField("name");
