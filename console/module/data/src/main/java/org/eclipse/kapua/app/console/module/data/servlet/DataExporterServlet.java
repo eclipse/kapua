096d06596d42d234c017e8364148a63de2de6918
coduz <alberto.codutti@eurotech.com>
2018-03-09 14:34:26 +0100

    Swiched to usage of appropriate datastore object factories
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- console/module/data/src/main/java/org/eclipse/kapua/app/console/module/data/servlet/DataExporterServlet.java
+++ console/module/data/src/main/java/org/eclipse/kapua/app/console/module/data/servlet/DataExporterServlet.java
@@ -11,25 +11,14 @@
  *******************************************************************************/
 package org.eclipse.kapua.app.console.module.data.servlet;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Date;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.kapua.KapuaEntityNotFoundException;
 import org.eclipse.kapua.KapuaIllegalAccessException;
 import org.eclipse.kapua.KapuaUnauthenticatedException;
 import org.eclipse.kapua.app.console.module.api.shared.util.GwtKapuaCommonsModelConverter;
 import org.eclipse.kapua.locator.KapuaLocator;
+import org.eclipse.kapua.service.datastore.DatastoreObjectFactory;
 import org.eclipse.kapua.service.datastore.MessageStoreService;
 import org.eclipse.kapua.service.datastore.internal.mediator.MessageField;
-import org.eclipse.kapua.service.datastore.internal.model.query.AndPredicateImpl;
-import org.eclipse.kapua.service.datastore.internal.model.query.MessageQueryImpl;
-import org.eclipse.kapua.service.datastore.internal.model.query.RangePredicateImpl;
 import org.eclipse.kapua.service.datastore.model.MessageListResult;
 import org.eclipse.kapua.service.datastore.model.query.AndPredicate;
 import org.eclipse.kapua.service.datastore.model.query.MessageQuery;
@@ -39,12 +28,23 @@ import org.eclipse.kapua.service.datastore.model.query.StorablePredicateFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+
 public class DataExporterServlet extends HttpServlet {
 
     private static final long serialVersionUID = 226461063207179649L;
     private static Logger logger = LoggerFactory.getLogger(DataExporterServlet.class);
 
     private static final KapuaLocator LOCATOR = KapuaLocator.getInstance();
+
+    private static final DatastoreObjectFactory DATASTORE_FACTORY = LOCATOR.getFactory(DatastoreObjectFactory.class);
+
     private static final StorablePredicateFactory STORABLE_PREDICATE_FACTORY = LOCATOR.getFactory(StorablePredicateFactory.class);
 
     @Override
@@ -72,7 +72,7 @@ public class DataExporterServlet extends HttpServlet {
             String sortDir = request.getParameter("sortDir");
             String topicOrDevice;
 
-            AndPredicate predicate = new AndPredicateImpl();
+            AndPredicate predicate = STORABLE_PREDICATE_FACTORY.newAndPredicate();
             if (topic != null) {
                 topicOrDevice = topic;
                 predicate.getPredicates().add(STORABLE_PREDICATE_FACTORY.newChannelMatchPredicate(topic.replaceFirst("/#$", "")));
@@ -109,10 +109,10 @@ public class DataExporterServlet extends HttpServlet {
             dataExporter.init(headers);
             KapuaLocator locator = KapuaLocator.getInstance();
             MessageStoreService messageService = locator.getService(MessageStoreService.class);
-            MessageQuery query = new MessageQueryImpl(GwtKapuaCommonsModelConverter.convertKapuaId(scopeIdString));
+            MessageQuery query = DATASTORE_FACTORY.newDatastoreMessageQuery(GwtKapuaCommonsModelConverter.convertKapuaId(scopeIdString));
             Date start = new Date(Long.valueOf(startDate));
             Date end = new Date(Long.valueOf(endDate));
-            predicate.getPredicates().add(new RangePredicateImpl(MessageField.TIMESTAMP, start, end));
+            predicate.getPredicates().add(STORABLE_PREDICATE_FACTORY.newRangePredicate(MessageField.TIMESTAMP.field(), start, end));
 
             if (sortField != null && sortDir != null) {
                 query.setSortFields(Collections.singletonList(SortField.of(SortDirection.valueOf(sortDir), sortField)));
--- console/module/data/src/main/java/org/eclipse/kapua/app/console/module/data/shared/util/GwtKapuaDataModelConverter.java
+++ console/module/data/src/main/java/org/eclipse/kapua/app/console/module/data/shared/util/GwtKapuaDataModelConverter.java
@@ -14,17 +14,23 @@ package org.eclipse.kapua.app.console.module.data.shared.util;
 import com.extjs.gxt.ui.client.data.PagingLoadConfig;
 import org.eclipse.kapua.app.console.module.api.shared.util.GwtKapuaCommonsModelConverter;
 import org.eclipse.kapua.app.console.module.data.shared.model.GwtDataChannelInfoQuery;
-import org.eclipse.kapua.service.datastore.internal.model.query.ChannelInfoQueryImpl;
+import org.eclipse.kapua.locator.KapuaLocator;
+import org.eclipse.kapua.service.datastore.DatastoreObjectFactory;
 import org.eclipse.kapua.service.datastore.model.query.ChannelInfoQuery;
 
 public class GwtKapuaDataModelConverter {
 
-    private GwtKapuaDataModelConverter() { }
+    private static final KapuaLocator LOCATOR = KapuaLocator.getInstance();
 
-        public static ChannelInfoQuery convertChannelInfoQuery(GwtDataChannelInfoQuery query, PagingLoadConfig pagingLoadConfig) {
-            ChannelInfoQueryImpl channelInfoQuery = new ChannelInfoQueryImpl(GwtKapuaCommonsModelConverter.convertKapuaId(query.getScopeId()));
-            channelInfoQuery.setOffset(pagingLoadConfig.getOffset());
-            channelInfoQuery.setLimit(pagingLoadConfig.getLimit());
-            return channelInfoQuery;
-        }
+    private static final DatastoreObjectFactory DATASTORE_FACTORY = LOCATOR.getFactory(DatastoreObjectFactory.class);
+
+    private GwtKapuaDataModelConverter() {
+    }
+
+    public static ChannelInfoQuery convertChannelInfoQuery(GwtDataChannelInfoQuery query, PagingLoadConfig pagingLoadConfig) {
+        ChannelInfoQuery channelInfoQuery = DATASTORE_FACTORY.newChannelInfoQuery(GwtKapuaCommonsModelConverter.convertKapuaId(query.getScopeId()));
+        channelInfoQuery.setOffset(pagingLoadConfig.getOffset());
+        channelInfoQuery.setLimit(pagingLoadConfig.getLimit());
+        return channelInfoQuery;
+    }
 }
