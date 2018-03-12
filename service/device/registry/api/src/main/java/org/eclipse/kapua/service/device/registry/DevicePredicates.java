f86c85f59d9eec7186630063da9dc43889641adc
Uroš Mesarič Kunst <uros.mesaric-kunst@comtrade.com>
2018-03-12 13:29:22 +0100

    Fixed query and predicate handling for device that doesn't have connection status
    If user selects UNKNOWN connection status it is mapped to query that
    matches connection_id to IS_NULL.
    
    Also when creating query value has to be set to NULL predicate and not
    java null value.
    
    Signed-off-by: Uros Mesaric Kunst <uros.mesaric-kunst@comtrade.com>

--- service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/DevicePredicates.java
+++ service/device/registry/api/src/main/java/org/eclipse/kapua/service/device/registry/DevicePredicates.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -38,6 +38,11 @@ public interface DevicePredicates extends KapuaUpdatableEntityPredicates {
      * Display name
      */
     public static final String DISPLAY_NAME = "displayName";
+
+    /**
+     * Connection id.
+     */
+    public static final String CONNECTION_ID = "connectionId";
 
     /**
      * Connection
