e3cd32143245aae879d5184828c6302357ed08b0
Uroš Mesarič Kunst <uros.mesaric-kunst@comtrade.com>
2018-03-12 15:51:59 +0100

    Password field in Configuration panel is also converted to KapuaTextField.
    Signed-off-by: Uros Mesaric Kunst <uros.mesaric-kunst@comtrade.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/configuration/DeviceConfigPanel.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/configuration/DeviceConfigPanel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -461,7 +461,7 @@ public class DeviceConfigPanel extends LayoutContainer {
     }
 
     private Field<?> paintPasswordConfigParameter(GwtConfigParameter param) {
-        TextField<String> field = new TextField<String>();
+        TextField<String> field = new KapuaTextField<String>();
         field.setName(param.getId());
         field.setValue((String) param.getValue());
         field.setAllowBlank(true);
