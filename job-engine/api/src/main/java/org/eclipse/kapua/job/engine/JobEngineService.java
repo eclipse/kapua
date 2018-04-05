fb8b5e0b51fdec21ab1f19cab55733576c147c18
coduz <alberto.codutti@eurotech.com>
2018-04-05 10:04:45 +0200

    Added Javadoc
    Signed-off-by: coduz <alberto.codutti@eurotech.com>
    
    Fixed javadoc
    
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- job-engine/api/src/main/java/org/eclipse/kapua/job/engine/JobEngineService.java
+++ job-engine/api/src/main/java/org/eclipse/kapua/job/engine/JobEngineService.java
@@ -26,12 +26,52 @@ public interface JobEngineService extends KapuaService, KapuaDomainService<JobDo
         return JOB_DOMAIN;
     }
 
+    /**
+     * Starts the {@link org.eclipse.kapua.service.job.Job}
+     *
+     * @param scopeId The scopeId of the {@link org.eclipse.kapua.service.job.Job}
+     * @param jobId   The id of the {@link org.eclipse.kapua.service.job.Job}
+     * @throws KapuaException if something goes bad when starting the job
+     * @since 1.0.0
+     */
     public void startJob(KapuaId scopeId, KapuaId jobId) throws KapuaException;
 
+    /**
+     * Starts the {@link org.eclipse.kapua.service.job.Job} with the given {@link JobStartOptions}.
+     *
+     * @param scopeId       The scopeId of the {@link org.eclipse.kapua.service.job.Job}
+     * @param jobId         The id of the {@link org.eclipse.kapua.service.job.Job}
+     * @param targetSublist The subset of {@link org.eclipse.kapua.service.job.targets.JobTarget}s on to include in this {@link org.eclipse.kapua.service.job.execution.JobExecution}
+     * @throws KapuaException if something goes bad when starting the job
+     * @since 1.0.0
+     */
+    public void startJob(KapuaId scopeId, KapuaId jobId, List<KapuaId> targetSublist) throws KapuaException;
+
+    /**
+     * Checks whether or not the {@link org.eclipse.kapua.service.job.Job} is running.
+     *
+     * @param scopeId The scopeId of the {@link org.eclipse.kapua.service.job.Job}
+     * @param jobId   The id of the {@link org.eclipse.kapua.service.job.Job}
+     * @return {@code true} if the {@link org.eclipse.kapua.service.job.Job} is currently running inside the {@link JobEngineService}
+     * @throws KapuaException if something goes bad when checking the status of the job
+     * @since 1.0.0
+     */
     public boolean isRunning(KapuaId scopeId, KapuaId jobId) throws KapuaException;
 
-    //    public void pauseJob(KapuaId scopeId, KapuaId jobId) throws KapuaException;
-
+    /**
+     * Stops the {@link org.eclipse.kapua.service.job.Job}.
+     * <p>
+     * This method does not wait fot the {@link org.eclipse.kapua.service.job.Job} to completely stop.
+     * It ask the {@link org.eclipse.kapua.service.job.Job} to stop but it can take some time to completely stop,
+     * depending on the current status of the {@link org.eclipse.kapua.service.job.Job}.
+     * <p>
+     * {@link JobEngineService#isRunning(KapuaId, KapuaId)} can be used to check the actual running status of the {@link org.eclipse.kapua.service.job.Job}
+     *
+     * @param scopeId The scopeId of the {@link org.eclipse.kapua.service.job.Job}
+     * @param jobId   The id of the {@link org.eclipse.kapua.service.job.Job}
+     * @throws KapuaException if something goes bad when checking the status of the job
+     * @since 1.0.0
+     */
     public void stopJob(KapuaId scopeId, KapuaId jobId) throws KapuaException;
 
 }
