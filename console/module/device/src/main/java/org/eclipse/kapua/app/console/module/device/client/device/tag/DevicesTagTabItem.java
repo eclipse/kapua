4e09c1ba8979a469daaa23c7ee67d893f486f47d
Uroš Mesarič Kunst <uros.mesaric-kunst@comtrade.com>
2018-04-09 11:33:06 +0200

    Adding Assigned Devices Tab to Tags (#1571)
    Query for searching devices with tag fixed, it is tag id that is searched
    for not group id. ANY is not needed as this is only used in group query.
    
    Signed-off-by: Uros Mesaric Kunst <uros.mesaric-kunst@comtrade.com>

---
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/tag/DevicesTagTabItem.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.app.console.module.device.client.device.tag;
+
+import org.eclipse.kapua.app.console.module.api.client.resources.icons.IconSet;
+import org.eclipse.kapua.app.console.module.api.client.resources.icons.KapuaIcon;
+import org.eclipse.kapua.app.console.module.api.client.ui.tab.KapuaTabItem;
+import org.eclipse.kapua.app.console.module.api.shared.model.session.GwtSession;
+import org.eclipse.kapua.app.console.module.tag.client.messages.ConsoleTagMessages;
+import org.eclipse.kapua.app.console.module.tag.shared.model.GwtTag;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Element;
+
+public class DevicesTagTabItem extends KapuaTabItem<GwtTag> {
+
+    private static final ConsoleTagMessages MSGS = GWT.create(ConsoleTagMessages.class);
+    TagSubjectGrid tagPermissionGrid;
+
+    public DevicesTagTabItem(GwtSession currentSession) {
+        super(currentSession, MSGS.tagsTabSubjectGridTitle(), new KapuaIcon(IconSet.SUPPORT));
+        tagPermissionGrid = new TagSubjectGrid(null, currentSession);
+        tagPermissionGrid.setRefreshOnRender(false);
+        setEnabled(false);
+    }
+
+    @Override
+    protected void onRender(Element parent, int index) {
+        super.onRender(parent, index);
+        setBorders(false);
+        add(tagPermissionGrid);
+    }
+
+    @Override
+    public void setEntity(GwtTag gwtTag) {
+        super.setEntity(gwtTag);
+        if (gwtTag != null) {
+            setEnabled(true);
+        } else {
+            setEnabled(false);
+        }
+        tagPermissionGrid.setEntity(gwtTag);
+    }
+
+    @Override
+    protected void doRefresh() {
+        if (selectedEntity != null){
+            tagPermissionGrid.refresh();
+        }
+    }
+}
+
