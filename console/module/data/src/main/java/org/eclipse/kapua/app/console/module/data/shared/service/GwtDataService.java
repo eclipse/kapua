7376ec0b63a0dfe50de039920124ba3be330b808
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2018-04-13 17:00:54 +0200

    Fixed parameter naming in GwtDataService
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- console/module/data/src/main/java/org/eclipse/kapua/app/console/module/data/shared/service/GwtDataService.java
+++ console/module/data/src/main/java/org/eclipse/kapua/app/console/module/data/shared/service/GwtDataService.java
@@ -15,6 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.google.gwt.user.client.rpc.RemoteService;
+
 import org.eclipse.kapua.app.console.module.api.client.GwtKapuaException;
 import org.eclipse.kapua.app.console.module.data.client.GwtTopic;
 import org.eclipse.kapua.app.console.module.data.client.util.GwtMessage;
@@ -25,6 +26,7 @@ import com.extjs.gxt.ui.client.data.LoadConfig;
 import com.extjs.gxt.ui.client.data.PagingLoadConfig;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
 import org.eclipse.kapua.app.console.module.data.shared.model.GwtDatastoreAsset;
 import org.eclipse.kapua.app.console.module.data.shared.model.GwtDatastoreDevice;
 import org.eclipse.kapua.app.console.module.data.shared.model.GwtHeader;
@@ -36,71 +38,70 @@ public interface GwtDataService extends RemoteService {
      * Return the Topics for a given account; the returned structure is ready to
      * be fed into the TreeGrid UI widget, so it contains all the topic children.
      *
-     * @param accountName
+     * @param scopeId
      * @return
      * @throws GwtKapuaException
      */
-    public List<GwtTopic> findTopicsTree(String accountName) throws GwtKapuaException;
+    List<GwtTopic> findTopicsTree(String scopeId) throws GwtKapuaException;
 
     PagingLoadResult<GwtTopic> findTopicsList(PagingLoadConfig config, GwtDataChannelInfoQuery query) throws GwtKapuaException;
 
     /**
-     *
      * @param config
-     * @param accountName
+     * @param scopeId
      * @return
      * @throws GwtKapuaException
      */
-    public ListLoadResult<GwtDatastoreDevice> findDevices(LoadConfig config, String accountName) throws GwtKapuaException;
+    ListLoadResult<GwtDatastoreDevice> findDevices(LoadConfig config, String scopeId) throws GwtKapuaException;
 
-    public ListLoadResult<GwtDatastoreAsset> findAssets(LoadConfig config, String accountName, GwtDatastoreDevice selectedDevice) throws GwtKapuaException;
+    ListLoadResult<GwtDatastoreAsset> findAssets(LoadConfig config, String scopeId, GwtDatastoreDevice selectedDevice) throws GwtKapuaException;
 
     /**
      * Return the Headers for a given account/topic pair.
      * The returned structure is ready to be fed into the Grid UI widget through a loader.
      *
      * @param config
-     * @param accountName
+     * @param scopeId
      * @param topic
      * @return
      * @throws GwtKapuaException
      */
-    public ListLoadResult<GwtHeader> findHeaders(LoadConfig config, String accountName, GwtTopic topic) throws GwtKapuaException;
+    ListLoadResult<GwtHeader> findHeaders(LoadConfig config, String scopeId, GwtTopic topic) throws GwtKapuaException;
 
     /**
      * Return the number Headers (int, long, float, double and so on. Not String, Boolean, Byte) for a given account/topic pair.
      * The returned structure is ready to be fed into the Grid UI widget through a loader.
      *
      * @param config
-     * @param accountName
+     * @param scopeId
      * @param topic
      * @return
      * @throws GwtKapuaException
      */
-    public ListLoadResult<GwtHeader> findNumberHeaders(LoadConfig config, String accountName, GwtTopic topic) throws GwtKapuaException;
+    ListLoadResult<GwtHeader> findNumberHeaders(LoadConfig config, String scopeId, GwtTopic topic) throws GwtKapuaException;
 
     /**
      * Return the Headers for a given account/topic pair.
      * The returned structure is ready to be fed into the Grid UI widget through a loader.
      *
      * @param config
-     * @param accountName
+     * @param scopeId
      * @param device
      * @return
      * @throws GwtKapuaException
      */
-    public ListLoadResult<GwtHeader> findHeaders(LoadConfig config, String accountName, GwtDatastoreDevice device) throws GwtKapuaException;
+    ListLoadResult<GwtHeader> findHeaders(LoadConfig config, String scopeId, GwtDatastoreDevice device) throws GwtKapuaException;
 
-    public PagingLoadResult<GwtMessage> findMessagesByTopic(PagingLoadConfig loadConfig, String accountName, GwtTopic topic, List<GwtHeader> headers, Date startDate, Date endDate)
+    PagingLoadResult<GwtMessage> findMessagesByTopic(PagingLoadConfig loadConfig, String scopeId, GwtTopic topic, List<GwtHeader> headers, Date startDate, Date endDate)
             throws GwtKapuaException;
 
-    public List<GwtMessage> findLastMessageByTopic(String accountName, int limit) throws GwtKapuaException;
+    List<GwtMessage> findLastMessageByTopic(String scopeId, int limit) throws GwtKapuaException;
 
-    public PagingLoadResult<GwtMessage> findMessagesByDevice(PagingLoadConfig loadConfig, String accountName, GwtDatastoreDevice device, List<GwtHeader> headers, Date startDate, Date endDate)
+    PagingLoadResult<GwtMessage> findMessagesByDevice(PagingLoadConfig loadConfig, String scopeId, GwtDatastoreDevice device, List<GwtHeader> headers, Date startDate, Date endDate)
             throws GwtKapuaException;
 
-    public PagingLoadResult<GwtMessage> findMessagesByAssets(PagingLoadConfig loadConfig, String accountName, GwtDatastoreAsset asset, List<GwtHeader> headers, Date startDate, Date endDate) throws GwtKapuaException;
+    PagingLoadResult<GwtMessage> findMessagesByAssets(PagingLoadConfig loadConfig, String scopeId, GwtDatastoreAsset asset, List<GwtHeader> headers, Date startDate, Date endDate) throws GwtKapuaException;
 
-    ListLoadResult<GwtHeader> findHeaders(LoadConfig config, String accountName, GwtDatastoreAsset gwtDatastoreAsset) throws GwtKapuaException;
+    ListLoadResult<GwtHeader> findHeaders(LoadConfig config, String scopeId, GwtDatastoreAsset gwtDatastoreAsset) throws GwtKapuaException;
 
 }
