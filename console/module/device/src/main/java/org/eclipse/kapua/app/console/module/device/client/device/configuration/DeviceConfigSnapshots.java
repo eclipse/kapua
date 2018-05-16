1a62c4f949d9e0b3cdaa304d56ae49b06cbb3bd6
mgogic <36038833+mgogic@users.noreply.github.com>
2018-05-16 10:25:46 +0200

    Fixed Download and Rollback buttons not disabled althought no snpashot selected (#1696)
    * Fixed Download and Rollback buttons not disabled altought no snapshot selected issue
    
    Signed-off-by: mgogic <milan.gogic@comtrade.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/configuration/DeviceConfigSnapshots.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/configuration/DeviceConfigSnapshots.java
@@ -378,6 +378,8 @@ public class DeviceConfigSnapshots extends LayoutContainer {
             } else {
                 toolBar.enable();
                 refreshButton.enable();
+                downloadButton.disable();
+                rollbackButton.disable();
                 reload();
             }
         }
