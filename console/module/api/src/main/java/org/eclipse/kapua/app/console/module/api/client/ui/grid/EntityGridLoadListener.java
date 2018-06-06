ccd101137e82f44621746f24955611901d83d64e
coduz <alberto.codutti@eurotech.com>
2018-06-06 14:00:21 +0200

    Fixed handling of selected item on multi-selectable grids
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/ui/grid/EntityGridLoadListener.java
+++ console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/ui/grid/EntityGridLoadListener.java
@@ -12,42 +12,80 @@
 package org.eclipse.kapua.app.console.module.api.client.ui.grid;
 
 import com.extjs.gxt.ui.client.data.LoadEvent;
+import com.extjs.gxt.ui.client.data.Loader;
 import com.extjs.gxt.ui.client.store.ListStore;
 import org.eclipse.kapua.app.console.module.api.client.util.KapuaLoadListener;
 import org.eclipse.kapua.app.console.module.api.shared.model.GwtEntityModel;
 
+import java.util.List;
+
 public class EntityGridLoadListener<M extends GwtEntityModel> extends KapuaLoadListener {
 
     private EntityGrid<M> entityGrid;
-    private M selectedEntity;
     private ListStore<M> entityStore;
+
+    private List<M> selectedEntities;
+    private boolean keepSelectedOnLoad = true;
 
     public EntityGridLoadListener(EntityGrid<M> entityGrid, ListStore<M> entityStore) {
         this.entityGrid = entityGrid;
         this.entityStore = entityStore;
     }
 
+    /**
+     * Keeps track of the selected item in the {@link EntityGrid} before the invocation of {@link Loader#load()}.
+     *
+     * @param le The {@link LoadEvent}
+     */
     @Override
     public void loaderBeforeLoad(LoadEvent le) {
-        selectedEntity = entityGrid.getSelectionModel().getSelectedItem();
+        selectedEntities = entityGrid.getSelectionModel().getSelectedItems();
     }
 
+    /**
+     * If {@link #keepSelectedOnLoad} is {@code true}, selects all entities which where selected before {@link Loader#load()}
+     * invocation if they are present in the new result set.
+     *
+     * @param le The {@link LoadEvent}.
+     */
     @Override
     public void loaderLoad(LoadEvent le) {
-        if (selectedEntity != null) {
+        if (isKeepSelectedOnLoad() && !selectedEntities.isEmpty()) {
             for (M e : entityStore.getModels()) {
-                if (selectedEntity.getId().equals(e.getId())) {
-                    entityGrid.getSelectionModel().select(e, true);
+                for (M se : selectedEntities) {
+                    if (se.getId().equals(e.getId())) {
+                        entityGrid.getSelectionModel().select(e, true);
+                        break;
+                    }
                 }
             }
         }
+
         entityGrid.loaded();
     }
 
     @Override
     public void loaderLoadException(LoadEvent le) {
         entityGrid.loaded();
+
         super.loaderLoadException(le);
     }
 
+    /**
+     * Returns whether or not the currently selected entities will be kept selected after the {@link Loader#load()}
+     *
+     * @return {@code true} if enabled,{@code false} otherwise.
+     */
+    public boolean isKeepSelectedOnLoad() {
+        return keepSelectedOnLoad;
+    }
+
+    /**
+     * Enables or disables the feature of remembering the selected items after the {@link Loader#load()} event.
+     *
+     * @param keepSelectedOnLoad {@code true} to enable, {@code false} to disable.
+     */
+    public void setKeepSelectedOnLoad(boolean keepSelectedOnLoad) {
+        this.keepSelectedOnLoad = keepSelectedOnLoad;
+    }
 }
