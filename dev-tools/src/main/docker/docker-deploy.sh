ec65a8f25fdeba753a5730ef77a3b098522b12d4
coduz <alberto.codutti@eurotech.com>
2018-06-19 15:30:01 +0200

    Removed Demo Vagrant machine
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- dev-tools/src/main/docker/docker-deploy.sh
+++ dev-tools/src/main/docker/docker-deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 ###############################################################################
-# Copyright (c) 2016, 2017 Red Hat Inc and others
+# Copyright (c) 2016, 2018 Red Hat Inc and others
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
--- dev-tools/src/main/vagrant/start.sh
+++ dev-tools/src/main/vagrant/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #*******************************************************************************
-# Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+# Copyright (c) 2011, 2018 Eurotech and/or its affiliates
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
@@ -13,7 +13,6 @@
 #******************************************************************************
 
 destroy_old_machines(){
-    vagrant destroy -f demo
     vagrant destroy -f develop
 }
 
@@ -27,26 +26,13 @@ start_develop(){
     echo "Follow the instructions to start the kapua components from the machine"
 }
 
-start_demo(){
-    echo 'Kapua vagrant demo machine ... starting'
-
-    vagrant up demo
-
-    echo 'Kapua vagrant demo machine... starting DONE'
-    echo "Please type 'vagrant ssh demo' to connect to the machine"
-    echo "Follow the instructions to deploy the kapua components into the machine"
-}
-
 print_usage(){
-    echo "Usage: $(basename $0) help|base-box|develop|demo" >&2
+    echo "Usage: $(basename $0) help|base-box|develop" >&2
 }
 
 if [ "$1" == 'develop' ]; then
     destroy_old_machines
     start_develop
-elif [ "$1" == 'demo' ]; then
-    destroy_old_machines
-    start_demo
 elif [ "$1" == 'base-box' ]; then
     sh baseBox/create.sh
 else
