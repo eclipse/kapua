63ad330a7bd75a522c49a6c857ba7dc13a0fa3d2
Uros Mesaric Kunst <uros.mesaric-kunst@comtrade.com>
2018-08-06 09:32:43 +0200

    Changed error handling to resamble other test steps. Error is logged and rethrown, so that test fails with error. In this case error is not expected for test itself, but test should fail.
    Signed-off-by: Uros Mesaric Kunst <uros.mesaric-kunst@comtrade.com>

--- qa-steps/src/main/java/org/eclipse/kapua/qa/steps/RestClientSteps.java
+++ qa-steps/src/main/java/org/eclipse/kapua/qa/steps/RestClientSteps.java
@@ -25,14 +25,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 import org.junit.Assert;
 
 @ScenarioScoped
-public class RestClientSteps extends Assert {
+public class RestClientSteps extends BaseQATests {
 
     private static final Logger logger = LoggerFactory.getLogger(RestClientSteps.class);
 
@@ -53,7 +51,7 @@ public class RestClientSteps extends Assert {
     }
 
     @When("^REST call at \"(.*)\"")
-    public void restCallStatusOfIndex(String resource) {
+    public void restCallStatusOfIndex(String resource) throws Exception {
 
         String host = (String) stepData.get("host");
         String port = (String) stepData.get("port");
@@ -61,7 +59,7 @@ public class RestClientSteps extends Assert {
             URL url = new URL("http://" + host + ":" + port + resource);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            assertFalse("Wrong response.", conn.getResponseCode() != 200);
+            Assert.assertFalse("Wrong response.", conn.getResponseCode() != 200);
             BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
             String output;
             StringBuilder sb = new StringBuilder();
@@ -70,12 +68,9 @@ public class RestClientSteps extends Assert {
             }
             conn.disconnect();
             stepData.put("restResponse", sb.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch ( IOException ioe) {
+            logger.error("Exception on REST call execution: " + resource);
+            throw ioe;
         }
     }
 
@@ -83,7 +78,7 @@ public class RestClientSteps extends Assert {
     public void restResponseContaining(String checkStr) {
 
         String restResponse = (String) stepData.get("restResponse");
-        assertTrue(String.format("Response %s doesn't include %s.", restResponse, checkStr),
+        Assert.assertTrue(String.format("Response %s doesn't include %s.", restResponse, checkStr),
                 restResponse.contains(checkStr));
     }
 
@@ -92,7 +87,7 @@ public class RestClientSteps extends Assert {
 
         String restResponse = (String) stepData.get("restResponse");
         Account account = (Account) stepData.get(var);
-        assertTrue(String.format("Response %s doesn't include %s.", restResponse, account.getId() + checkStr),
+        Assert.assertTrue(String.format("Response %s doesn't include %s.", restResponse, account.getId() + checkStr),
                 restResponse.contains(account.getId() + checkStr));
     }
 
