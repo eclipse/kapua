95ec1b767bb088c96be0cf534c7f857852e74078
JB Trystram <jbtrystram@redhat.com>
2018-10-02 12:06:48 +0200

    getting adaptaters props passed to the hono response

--- extras/hono-integration/src/main/java/org/eclipse/kapua/hono/KapuaTenantService.java
+++ extras/hono-integration/src/main/java/org/eclipse/kapua/hono/KapuaTenantService.java
@@ -14,6 +14,7 @@ package org.eclipse.kapua.hono;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.eclipse.hono.service.tenant.BaseTenantService;
 import org.eclipse.hono.util.TenantObject;
@@ -24,13 +25,13 @@ import org.eclipse.kapua.commons.security.KapuaSecurityUtils;
 import org.eclipse.kapua.commons.util.xml.JAXBContextProvider;
 import org.eclipse.kapua.commons.util.xml.XmlUtil;
 import org.eclipse.kapua.locator.KapuaLocator;
-import org.eclipse.kapua.model.id.KapuaId;
 import org.eclipse.kapua.service.account.Account;
-import org.eclipse.kapua.service.account.AccountCreator;
-import org.eclipse.kapua.service.account.AccountFactory;
 import org.eclipse.kapua.service.account.AccountService;
 
+import javax.security.auth.x500.X500Principal;
 import java.net.HttpURLConnection;
+import java.security.PublicKey;
+import java.util.Properties;
 
 public class KapuaTenantService extends BaseTenantService<Object> {
 
@@ -42,16 +43,6 @@ public class KapuaTenantService extends BaseTenantService<Object> {
         JAXBContextProvider brokerProvider = new BrokerJAXBContextProvider();
         XmlUtil.setContextProvider(brokerProvider);
         accountService = locator.getService(AccountService.class);
-
-        // Create standard account
-        AccountFactory accountFactory = locator.getFactory(AccountFactory.class);
-        AccountCreator accountCreator = accountFactory.newCreator(KapuaId.ONE, "DEFAULT_TENANT");
-        accountCreator.setOrganizationName("Hono default Tenant");
-        accountCreator.setOrganizationEmail("tenant@example.com");
-        try{
-            Account account = accountService.create(accountCreator);
-        }catch(Exception e) {
-        }
     }
 
     @Override
@@ -62,6 +53,8 @@ public class KapuaTenantService extends BaseTenantService<Object> {
 
     @Override
     public void get(String tenantId, Handler<AsyncResult<TenantResult<JsonObject>>> resultHandler) {
+
+        System.out.println("Getting tenant !");
         try {
             Account account = KapuaSecurityUtils.doPrivileged(() -> accountService.findByName(tenantId));
             if (account == null) {
@@ -71,7 +64,18 @@ public class KapuaTenantService extends BaseTenantService<Object> {
 
             TenantObject tenant = new TenantObject();
             tenant.setTenantId(tenantId);
+            Properties accountProps = account.getEntityProperties();
+            if (accountProps.containsKey("adapters")){
+                tenant.setAdapterConfigurations(new JsonArray()
+                        .add(accountProps.getProperty("adapters")));
+            }
+            if (accountProps.containsKey("trusted-ca")){
+                tenant.setTrustAnchor(
+                        (new JsonObject(accountProps.getProperty("trusted-ca"))).getString("public-key"),
+                        (new JsonObject(accountProps.getProperty("trusted-ca"))).getString("subject-dn"));
+            }
             tenant.setEnabled(true);
+            System.out.println("returning tenant : "+tenant);
             resultHandler.handle(Future.succeededFuture(TenantResult.from(HttpURLConnection.HTTP_OK,
                     JsonObject.mapFrom(tenant),
                     null)));
@@ -82,4 +86,37 @@ public class KapuaTenantService extends BaseTenantService<Object> {
         }
 
     }
+
+   /* @Override
+    public void get(X500Principal subjectDn, Handler<AsyncResult<TenantResult<JsonObject>>> resultHandler) {
+        try {
+            AccountListResult accountList = KapuaSecurityUtils.doPrivileged(() -> accountService.query(
+                    new AccountQuery(){
+
+                        @Override
+
+                    }
+                    )
+            )
+            if (accountList.isEmpty()) {
+                resultHandler.handle(Future.succeededFuture(TenantResult.from(HttpURLConnection.HTTP_NOT_FOUND)));
+                return;
+            }
+
+            TenantObject tenant = new TenantObject();
+            tenant.setTenantId(tenantId);
+            //tenant.setAdapterConfigurations(account.getEntityProperties().)
+            tenant.setEnabled(true);
+            System.out.println("returning tenant : "+tenant);
+            resultHandler.handle(Future.succeededFuture(TenantResult.from(HttpURLConnection.HTTP_OK,
+                    JsonObject.mapFrom(tenant),
+                    null)));
+
+        } catch (KapuaException ke) {
+            resultHandler.handle(Future.succeededFuture(TenantResult.from(HttpURLConnection.HTTP_INTERNAL_ERROR)));
+            return;
+        }
+
+    }
+    */
 }
