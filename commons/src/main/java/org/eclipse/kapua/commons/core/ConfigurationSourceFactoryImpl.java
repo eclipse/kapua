05b0f70e20f944812f077a077c9e8941f49ee6fa
stefanomorson <stefano.morson@eurotech.com>
2018-10-06 14:11:26 +0200

    external processors - added json to properties converter
    Signed-off-by: stefanomorson <stefano.morson@eurotech.com>

--- commons/src/main/java/org/eclipse/kapua/commons/core/ConfigurationSourceFactoryImpl.java
+++ commons/src/main/java/org/eclipse/kapua/commons/core/ConfigurationSourceFactoryImpl.java
@@ -13,9 +13,9 @@ package org.eclipse.kapua.commons.core;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.configuration.CompositeConfiguration;
@@ -25,10 +25,7 @@ import org.apache.commons.configuration.SystemConfiguration;
 import org.eclipse.kapua.commons.core.spi.ConfigurationSourceFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
-
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
 
 /**
  * Creates a {@link ConfigurationSource} from a JSON file specified by the invoker of 
@@ -39,28 +36,28 @@ import io.vertx.core.logging.LoggerFactory;
  */
 public class ConfigurationSourceFactoryImpl implements ConfigurationSourceFactory {
 
-    private static Logger logger = LoggerFactory.getLogger(ConfigurationSourceFactoryImpl.class);
-
     private static final String EXTERNAL_CONFIG_URL_PARAM = "kapua.application.config.url";
 
-    public ConfigurationSource create(ClassLoader classLoader, String name) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-//      JavaPropsMapper propsMapper = new JavaPropsMapper();
+    public ConfigurationSource create(ClassLoader classLoader, String name) throws IOException {
 
+        // Define a composite configuration with priority to system properties then
+        // environment variables then local resources.
         CompositeConfiguration compositeConfig = new CompositeConfiguration();
         compositeConfig.addConfiguration(new SystemConfiguration());
         compositeConfig.addConfiguration(new EnvironmentConfiguration());
-        HashMap<String, Object> m = null;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        HashMap<String, Object> object = null;
         if (compositeConfig.containsKey(EXTERNAL_CONFIG_URL_PARAM)) {
-            m = readValueFromUrl(mapper, compositeConfig.getString(EXTERNAL_CONFIG_URL_PARAM));
+            object = readValueFromUrl(objectMapper, compositeConfig.getString(EXTERNAL_CONFIG_URL_PARAM));
         } else {
-            m = readValueFromResource(mapper, classLoader, name);
+            object = readValueFromResource(objectMapper, classLoader, name);
         }
-//            String q = propsMapper.writeValueAsString(m);
-//            Properties props = new Properties();
-//            props.load(new StringReader(q));
+
+        JavaPropsMapper propsMapper = new JavaPropsMapper();
+        String q = propsMapper.writeValueAsString(object);
         Properties props = new Properties();
-        toProperties(m, "", props);
+        props.load(new StringReader(q));
 
         compositeConfig.addConfiguration(ConfigurationConverter.getConfiguration(props));
         CompositeSource compositeSource = new CompositeSource(compositeConfig);
@@ -68,7 +65,7 @@ public class ConfigurationSourceFactoryImpl implements ConfigurationSourceFactor
     }
 
     @SuppressWarnings("unchecked")
-    private HashMap<String, Object> readValueFromUrl(ObjectMapper mapper, String urlStr) throws Exception {
+    private HashMap<String, Object> readValueFromUrl(ObjectMapper mapper, String urlStr) throws IOException {
         InputStream inputStream = null;
         HashMap<String, Object> map = null;
         try {
@@ -96,32 +93,5 @@ public class ConfigurationSourceFactoryImpl implements ConfigurationSourceFactor
             }
         }
         return map;
-    }
-
-    // TODO Replace with jackson JavaPropsMapper
-    private void toProperties(Map<String, Object> map, String prefix, Properties props) {
-        for(String key:map.keySet()) {
-            Object value = map.get(key);
-            String propKey = prefix + (prefix.isEmpty() ? "" : ".") + key;
-            if (value == null) {
-                props.setProperty(propKey, "");   
-            } else if (value instanceof String) {
-                props.setProperty(propKey, (String) value);  
-            } else if (value instanceof Boolean) {
-                props.setProperty(propKey, Boolean.toString((Boolean) value));
-            } else if (value instanceof Integer) {
-                props.setProperty(propKey, Integer.toString((int) value));
-            } else if (value instanceof Long) {
-                props.setProperty(propKey, Long.toString((long) value));
-            } else if (value instanceof Float) {
-                props.setProperty(propKey, Float.toString((float) value));
-            } else if (value instanceof Double) {
-                props.setProperty(propKey, Double.toString((double) value));
-            } else if (value instanceof Map) {
-                toProperties((Map<String, Object>)value, propKey, props);
-            } else {
-                throw new RuntimeException("Un recognized value type: " + value.toString());
-            }
-        }
     }
 }
