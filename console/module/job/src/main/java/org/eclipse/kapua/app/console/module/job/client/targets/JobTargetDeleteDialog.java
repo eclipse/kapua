a99be2b82fedcbf09461654596014ae3b246536c
ct-ajovanovic <aleksandra.jovanovic@comtrade.com>
2018-12-03 13:13:06 +0100

    Modified header message for Target delete dialog
    Signed-off-by: ct-ajovanovic <aleksandra.jovanovic@comtrade.com>

--- console/module/job/src/main/java/org/eclipse/kapua/app/console/module/job/client/targets/JobTargetDeleteDialog.java
+++ console/module/job/src/main/java/org/eclipse/kapua/app/console/module/job/client/targets/JobTargetDeleteDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -34,7 +34,7 @@ public class JobTargetDeleteDialog extends EntityDeleteDialog {
 
     @Override
     public String getHeaderMessage() {
-        return JOB_MSGS.dialogDeleteTargetHeader(gwtJobTarget.getJobTargetId());
+        return JOB_MSGS.dialogDeleteTargetHeader(gwtJobTarget.getClientId());
     }
 
     @Override
