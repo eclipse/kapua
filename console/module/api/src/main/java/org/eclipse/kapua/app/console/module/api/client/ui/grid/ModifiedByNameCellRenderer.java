cc6efd9d848e81ae4a024ca5574e1594c63f6233
ct-ajovanovic <aleksandra.jovanovic@comtrade.com>
2018-12-17 13:52:16 +0100

    Added CreatedByNameCellRenderer and ModifiedByNameCellRenderer
    Signed-off-by: ct-ajovanovic <aleksandra.jovanovic@comtrade.com>

---
+++ console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/ui/grid/ModifiedByNameCellRenderer.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.app.console.module.api.client.ui.grid;
+
+import org.eclipse.kapua.app.console.module.api.client.messages.ConsoleMessages;
+import org.eclipse.kapua.app.console.module.api.shared.model.GwtUpdatableEntityModel;
+
+import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.widget.grid.ColumnData;
+import com.extjs.gxt.ui.client.widget.grid.Grid;
+import com.extjs.gxt.ui.client.widget.grid.GridCellRenderer;
+import com.google.gwt.core.client.GWT;
+
+public class ModifiedByNameCellRenderer<M extends GwtUpdatableEntityModel> implements GridCellRenderer<M> {
+
+    private static final ConsoleMessages CMSGS = GWT.create(ConsoleMessages.class);
+
+    @Override
+    public Object render(M model, String property, ColumnData config, int rowIndex, int colIndex, ListStore<M> store,
+            Grid<M> grid) {
+        return model.getModifiedByName() != null ? model.getModifiedByName() : CMSGS.notAvailable();
+    }
+}
