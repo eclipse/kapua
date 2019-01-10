f332dbcc1fe4a787a60c0996c479c4036779f518
ct-goranpalibrk <30318334+ct-goranpalibrk@users.noreply.github.com>
2019-01-10 09:06:56 +0100

    unnecessary scroll removed (#2281)
    Signed-off-by: CT\pgoran <goran.palibrk@comtrade.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/history/DeviceTabHistory.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/history/DeviceTabHistory.java
@@ -12,7 +12,6 @@
 package org.eclipse.kapua.app.console.module.device.client.device.history;
 
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
-import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.Style.SelectionMode;
 import com.extjs.gxt.ui.client.Style.SortDir;
 import com.extjs.gxt.ui.client.data.BasePagingLoadConfig;
@@ -117,7 +116,6 @@ public class DeviceTabHistory extends KapuaTabItem<GwtDevice> {
         devicesHistoryPanel.setBodyBorder(true);
         devicesHistoryPanel.setHeaderVisible(false);
         devicesHistoryPanel.setLayout(new FitLayout());
-        devicesHistoryPanel.setScrollMode(Scroll.AUTO);
         devicesHistoryPanel.setTopComponent(toolBar);
         devicesHistoryPanel.add(grid);
         devicesHistoryPanel.setBottomComponent(pagingToolBar);
