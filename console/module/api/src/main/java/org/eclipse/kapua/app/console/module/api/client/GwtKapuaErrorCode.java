1f6512e97794e61cd7c96d23b6788d8f5115ace3
ct-anaalbic <45655642+ct-anaalbic@users.noreply.github.com>
2019-01-16 15:26:03 +0100

    changed error message when end time is before start time in job schedules (#2283)
    Signed-off-by: ana.albic.comtrade <Ana.Albic@comtrade.com>

--- console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/GwtKapuaErrorCode.java
+++ console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/GwtKapuaErrorCode.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -43,5 +43,6 @@ public enum GwtKapuaErrorCode {
     MAX_NUMBER_OF_ITEMS_REACHED,
     OPERATION_NOT_ALLOWED_ON_ADMIN_USER,
     RETRY_AND_CRON_BOTH_SELECTED,
+    END_BEFORE_START_TIME_ERROR,
     SAME_START_AND_DATE
 }
--- service/scheduler/quartz/src/main/java/org/eclipse/kapua/service/scheduler/trigger/quartz/TriggerServiceImpl.java
+++ service/scheduler/quartz/src/main/java/org/eclipse/kapua/service/scheduler/trigger/quartz/TriggerServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,7 +11,10 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.scheduler.trigger.quartz;
 
+import java.sql.Date;
+import java.util.TimeZone;
 import org.eclipse.kapua.KapuaDuplicateNameException;
+import org.eclipse.kapua.KapuaEndBeforeStartTimeException;
 import org.eclipse.kapua.KapuaEntityNotFoundException;
 import org.eclipse.kapua.KapuaErrorCodes;
 import org.eclipse.kapua.KapuaException;
@@ -51,8 +54,6 @@ import org.quartz.TriggerBuilder;
 import org.quartz.TriggerKey;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.util.TimeZone;
-
 /**
  * {@link TriggerService} implementation.
  *
@@ -83,6 +84,7 @@ public class TriggerServiceImpl extends AbstractKapuaConfigurableResourceLimited
         ArgumentValidator.notNull(triggerCreator, "triggerCreator");
         ArgumentValidator.notNull(triggerCreator.getScopeId(), "triggerCreator.scopeId");
         ArgumentValidator.notEmptyOrNull(triggerCreator.getName(), "triggerCreator.name");
+        ArgumentValidator.notNull(triggerCreator.getStartsOn(), "triggerCreator.startsOn");
 
         //
         // Check Access
@@ -103,6 +105,14 @@ public class TriggerServiceImpl extends AbstractKapuaConfigurableResourceLimited
 
         if (triggerCreator.getStartsOn().equals(triggerCreator.getEndsOn()) && triggerCreator.getStartsOn().getTime() == (triggerCreator.getEndsOn().getTime())) {
             throw new KapuaException(KapuaErrorCodes.SAME_START_AND_DATE);
+        }
+
+        if (triggerCreator.getEndsOn() != null) {
+            Date startTime = new Date(triggerCreator.getStartsOn().getTime());
+            Date endTime = new Date(triggerCreator.getEndsOn().getTime());
+            if (startTime.after(endTime)) {
+                throw new KapuaEndBeforeStartTimeException();
+            }
         }
 
         //
