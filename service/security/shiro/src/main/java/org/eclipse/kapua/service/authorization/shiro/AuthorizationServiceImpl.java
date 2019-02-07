91ca836e6136fb88aa07abce7f250913bcf77d99
ct-anaalbic <45655642+ct-anaalbic@users.noreply.github.com>
2019-02-07 15:19:48 +0100

    changed error message when user's permisions are revoked (#2347)
    Signed-off-by: ana.albic.comtrade <Ana.Albic@comtrade.com>

--- service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/shiro/AuthorizationServiceImpl.java
+++ service/security/shiro/src/main/java/org/eclipse/kapua/service/authorization/shiro/AuthorizationServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,7 @@
 package org.eclipse.kapua.service.authorization.shiro;
 
 import org.apache.shiro.SecurityUtils;
+import org.eclipse.kapua.KapuaErrorCodes;
 import org.eclipse.kapua.KapuaException;
 import org.eclipse.kapua.KapuaUnauthenticatedException;
 import org.eclipse.kapua.commons.security.KapuaSecurityUtils;
@@ -19,7 +20,6 @@ import org.eclipse.kapua.commons.security.KapuaSession;
 import org.eclipse.kapua.locator.KapuaProvider;
 import org.eclipse.kapua.service.authorization.AuthorizationService;
 import org.eclipse.kapua.service.authorization.permission.Permission;
-import org.eclipse.kapua.service.authorization.shiro.exception.SubjectUnauthorizedException;
 
 /**
  * {@link AuthorizationService} implementation.
@@ -45,7 +45,7 @@ public class AuthorizationServiceImpl implements AuthorizationService {
     public void checkPermission(Permission permission)
             throws KapuaException {
         if (!isPermitted(permission)) {
-            throw new SubjectUnauthorizedException(permission);
+            throw new KapuaException(KapuaErrorCodes.PERMISSIONS_REVOKED);
         }
     }
 }
