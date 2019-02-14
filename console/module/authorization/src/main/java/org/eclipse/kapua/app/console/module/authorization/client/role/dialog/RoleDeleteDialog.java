cf796d5c7036ccaa646e8b6b44ef4a6650cdf6ba
Aleksandra Jovanovic <35954696+ct-ajovanovic@users.noreply.github.com>
2019-02-14 12:01:15 +0100

    Submit button gets disabled fix (#2397)
    Signed-off-by: Aleksandra Jovanovic <aleksandra.jovanovic@comtrade.com>

--- console/module/authorization/src/main/java/org/eclipse/kapua/app/console/module/authorization/client/role/dialog/RoleDeleteDialog.java
+++ console/module/authorization/src/main/java/org/eclipse/kapua/app/console/module/authorization/client/role/dialog/RoleDeleteDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -33,6 +33,7 @@ public class RoleDeleteDialog extends EntityDeleteDialog {
         this.gwtRole = gwtRole;
 
         DialogUtils.resizeDialog(this, 300, 135);
+        setDisabledFormPanelEvents(true);
     }
 
     @Override
