67ae044787eaa069e4a84d18eeed0facef5431f7
Aleksandra Jovanovic <aleksandra.jovanovic@comtrade.com>
2019-02-25 14:04:36 +0100

    Updated permission check when enabling Assigned Devices tab in Tags
    Signed-off-by: Aleksandra Jovanovic <aleksandra.jovanovic@comtrade.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/tag/DevicesTagsTabItemDescriptor.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/tag/DevicesTagsTabItemDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ * Copyright (c) 2018, 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,7 +13,6 @@ package org.eclipse.kapua.app.console.module.device.client.device.tag;
 
 import org.eclipse.kapua.app.console.module.api.client.ui.view.descriptor.AbstractEntityTabDescriptor;
 import org.eclipse.kapua.app.console.module.api.shared.model.session.GwtSession;
-import org.eclipse.kapua.app.console.module.authorization.shared.model.permission.AccessInfoSessionPermission;
 import org.eclipse.kapua.app.console.module.device.shared.model.permission.DeviceSessionPermission;
 import org.eclipse.kapua.app.console.module.tag.client.TagView;
 import org.eclipse.kapua.app.console.module.tag.shared.model.GwtTag;
@@ -37,7 +36,7 @@ public class DevicesTagsTabItemDescriptor extends AbstractEntityTabDescriptor<Gw
 
     @Override
     public Boolean isEnabled(GwtSession currentSession) {
-        return currentSession.hasPermission(DeviceSessionPermission.read()) && currentSession.hasPermission(AccessInfoSessionPermission.read());
+        return currentSession.hasPermission(DeviceSessionPermission.read());
     }
 }
 
