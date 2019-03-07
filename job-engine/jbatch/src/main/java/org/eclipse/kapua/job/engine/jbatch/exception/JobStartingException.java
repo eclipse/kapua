d1748399ca586e0b2ffbd46ec47aef35ba6732a5
ct-goranpalibrk <30318334+ct-goranpalibrk@users.noreply.github.com>
2019-03-07 15:26:32 +0100

    change error message when job starts (#2437)
    * change error message when job starts
    
    Signed-off-by: CT\pgoran <goran.palibrk@comtrade.com>
    
    * solved PR changes
    
    Signed-off-by: CT\pgoran <goran.palibrk@comtrade.com>

--- job-engine/jbatch/src/main/java/org/eclipse/kapua/job/engine/jbatch/exception/JobStartingException.java
+++ job-engine/jbatch/src/main/java/org/eclipse/kapua/job/engine/jbatch/exception/JobStartingException.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ * Copyright (c) 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,6 +16,6 @@ import org.eclipse.kapua.model.id.KapuaId;
 public class JobStartingException extends JobEngineException {
 
     public JobStartingException(Throwable t, KapuaId scopeId, KapuaId jobId) {
-        super(KapuaJobEngineErrorCodes.JOB_STARTING, t, scopeId, jobId);
+        super(KapuaJobEngineErrorCodes.JOB_STARTING_ERROR, t, scopeId, jobId);
     }
 }
