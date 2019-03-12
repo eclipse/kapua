1e342631009c562db936111763d067550d2a0a2c
CT\pgoran <goran.palibrk@comtrade.com>
2019-03-12 11:43:36 +0100

    reset filter fields after user input
    Signed-off-by: CT\pgoran <goran.palibrk@comtrade.com>

--- console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/ui/widget/EntityCRUDToolbar.java
+++ console/module/api/src/main/java/org/eclipse/kapua/app/console/module/api/client/ui/widget/EntityCRUDToolbar.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -171,6 +171,7 @@ public class EntityCRUDToolbar<M extends GwtEntityModel> extends ToolBar {
 
             @Override
             public void componentSelected(ButtonEvent ce) {
+                filterPanel.resetFields();
                 KapuaDialog dialog = getAddDialog();
                 if (dialog != null) {
                     dialog.addListener(Events.Hide, getHideDialogListener());
