15077de85c8e15b6da0cbfc796b6b33470064f08
Aleksandra Jovanovic <aleksandra.jovanovic@comtrade.com>
2019-03-29 11:47:02 +0100

    Different way of displaying Event message information on Events tab
    Signed-off-by: Aleksandra Jovanovic <aleksandra.jovanovic@comtrade.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/history/DeviceTabHistory.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/client/device/history/DeviceTabHistory.java
@@ -22,9 +22,13 @@ import com.extjs.gxt.ui.client.data.PagingLoadConfig;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
 import com.extjs.gxt.ui.client.data.RpcProxy;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.IconButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.HorizontalPanel;
+import com.extjs.gxt.ui.client.widget.Text;
+import com.extjs.gxt.ui.client.widget.button.ToolButton;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 import com.extjs.gxt.ui.client.widget.grid.ColumnData;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
@@ -37,6 +41,10 @@ import com.extjs.gxt.ui.client.widget.toolbar.SeparatorToolItem;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.extjs.gxt.ui.client.widget.treegrid.TreeGridCellRenderer;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
@@ -194,17 +202,48 @@ public class DeviceTabHistory extends KapuaTabItem<GwtDevice> {
         TreeGridCellRenderer<GwtDeviceEvent> eventMessageRenderer = new TreeGridCellRenderer<GwtDeviceEvent>() {
 
             @Override
-            public Object render(GwtDeviceEvent model, String property, ColumnData config, int rowIndex, int colIndex, ListStore<GwtDeviceEvent> store, Grid<GwtDeviceEvent> grid) {
-                StringBuilder message = new StringBuilder("");
+            public Object render(final GwtDeviceEvent model, String property, ColumnData config, int rowIndex, int colIndex, ListStore<GwtDeviceEvent> store, Grid<GwtDeviceEvent> grid) {
+                final HorizontalPanel horizontalPanel = new HorizontalPanel();
+                final Text cellText = new Text();
+                cellText.setStyleName("x-grid3-cell");
+                cellText.setText(model.getUnescapedEventMessage());
 
-                if (model.getEventMessage() != null) {
-                    message.append("<label title='")
-                            .append(model.getUnescapedEventMessage())
-                            .append("'>")
-                            .append(model.getUnescapedEventMessage())
-                            .append("</label>");
-                }
-                return message.toString();
+                // Search button
+                final ToolButton searchButton = new ToolButton("x-tool-search", new SelectionListener<IconButtonEvent>() {
+
+                    @Override
+                    public void componentSelected(IconButtonEvent ce) {
+                        final EventMessageDetailsDialog dialog = new EventMessageDetailsDialog(model.getReceivedOnFormatted(), model.getEventType(), model.getUnescapedEventMessage());
+                        dialog.show();
+                    }
+                });
+
+                searchButton.setStyleAttribute("margin-right", "5px");
+                searchButton.setToolTip(MSGS.showDetails());
+                horizontalPanel.add(searchButton);
+                horizontalPanel.add(cellText);
+                searchButton.hide();
+
+                // Show search button on mouse over
+                horizontalPanel.addDomHandler(new MouseOverHandler() {
+
+                    @Override
+                    public void onMouseOver(MouseOverEvent arg0) {
+                        if (model.getUnescapedEventMessage() != null && !model.getUnescapedEventMessage().isEmpty()) {
+                            searchButton.show();
+                        }
+                    }
+                }, MouseOverEvent.getType());
+
+                // Hide search button on mouse out
+                horizontalPanel.addDomHandler(new MouseOutHandler() {
+
+                    @Override
+                    public void onMouseOut(MouseOutEvent arg0) {
+                        searchButton.hide();
+                    }
+                }, MouseOutEvent.getType());
+                return horizontalPanel;
             }
         };
 
