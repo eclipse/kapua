f3de383f11098768a525eb816461deb7e663e0ac
Gianluca Barbon <gianluca.barbon@eurotech.com>
2019-05-13 16:49:22 +0200

    Added Docker deployment scripts for Windows PowerShell
    Signed-off-by: Gianluca Barbon <gianluca.barbon@eurotech.com>

---
+++ deployment/docker/win/docker-undeploy.ps1
@@ -0,0 +1,22 @@
+###############################################################################
+# Copyright (c) 2019 Eurotech and/or its affiliates and others
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Eurotech - initial implementation
+###############################################################################
+
+$script_dir = Split-Path (Get-Variable MyInvocation).Value.MyCommand.Path
+$common_path = $script_dir + '\docker-common.ps1'
+
+. $common_path
+
+Write-Host "Undeploying Eclipse Kapua..."
+
+docker-compose -f $script_dir'\..\compose\docker-compose.yml' down
+
+Write-Host "Undeploying Eclipse Kapua... DONE!"
