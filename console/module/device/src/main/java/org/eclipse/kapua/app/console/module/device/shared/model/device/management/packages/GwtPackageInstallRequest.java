ac193ac2100060805bfa20e2a426addfd794444c
coduz <alberto.codutti@eurotech.com>
2019-09-20 09:34:04 +0200

    Added new parameters to Package Install Dialog in console
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/shared/model/device/management/packages/GwtPackageInstallRequest.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/shared/model/device/management/packages/GwtPackageInstallRequest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ * Copyright (c) 2016, 2019 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -43,7 +43,6 @@ public class GwtPackageInstallRequest extends KapuaBaseModel {
 
     public void setPackageName(String packageName) {
         set("packageName", packageName);
-
     }
 
     public String getPackageName() {
@@ -56,6 +55,46 @@ public class GwtPackageInstallRequest extends KapuaBaseModel {
 
     public String getPackageVersion() {
         return get("packageVersion");
+    }
+
+    public void setUsername(String username) {
+        set("username", username);
+    }
+
+    public String getUsername() {
+        return get("username");
+    }
+
+    public void setPassword(String password) {
+        set("password", password);
+    }
+
+    public String getPassword() {
+        return get("password");
+    }
+
+    public void setFileHash(String fileHash) {
+        set("fileHash", fileHash);
+    }
+
+    public String getFileHash() {
+        return get("fileHash");
+    }
+
+    public void setFileType(GwtFileType fileType) {
+        setFileType(fileType.name());
+    }
+
+    public void setFileType(String fileType) {
+        set("fileType", fileType);
+    }
+
+    public String getFileType() {
+        return get("fileType");
+    }
+
+    public GwtFileType gwtFileTypeEnum() {
+        return GwtFileType.valueOf(getFileType());
     }
 
     public void setReboot(Boolean reboot) {
@@ -74,4 +113,46 @@ public class GwtPackageInstallRequest extends KapuaBaseModel {
         return get("rebootDelay");
     }
 
+    //
+    // Advanced
+
+    public void setBlockSize(Integer blockSize) {
+        set("blockSize", blockSize);
+    }
+
+    public Integer getBlockSize() {
+        return get("blockSize");
+    }
+
+    public void setBlockDelay(Integer blockDelay) {
+        set("blockDelay", blockDelay);
+    }
+
+    public Integer getBlockDelay() {
+        return get("blockDelay");
+    }
+
+    public void setBlockTimeout(Integer blockTimeout) {
+        set("blockTimeout", blockTimeout);
+    }
+
+    public Integer getBlockTimeout() {
+        return get("blockTimeout");
+    }
+
+    public void setNotifyBlockSize(Integer notifyBlockSize) {
+        set("notifyBlockSize", notifyBlockSize);
+    }
+
+    public Integer getNotifyBlockSize() {
+        return get("notifyBlockSize");
+    }
+
+    public void setInstallVerifierURI(String installVerifierURI) {
+        set("installVerifierURI", installVerifierURI);
+    }
+
+    public String getInstallVerifierURI() {
+        return get("installVerifierURI");
+    }
 }
