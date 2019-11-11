51fa0d05285e376ca2f7587d6e1f9438b3656768
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2019-11-11 16:13:30 +0100

    Rebase on 1.2.0-SNAPSHOT fixes
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

---
+++ assembly/job-engine/entrypoint/run-job-engine.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+java -Dcommons.db.schema.update=true \
+     -Dcommons.db.schema.update=true \
+     -Dcommons.db.connection.host=${SQL_DB_ADDR} \
+     -Dcommons.db.connection.port=${SQL_DB_PORT} \
+     -Dcommons.eventbus.url=${SERVICE_BROKER_ADDR} \
+     -jar /kapua-service-app-job-engine-*.jar
