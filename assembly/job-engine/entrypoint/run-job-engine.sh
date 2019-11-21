658d40da02e98d22e6f36b5b51fc80559b29c401
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2019-11-21 15:40:11 +0100

    TriggerServiceImpl initialization moved in constructor
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- assembly/job-engine/entrypoint/run-job-engine.sh
+++ assembly/job-engine/entrypoint/run-job-engine.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 java -Dcommons.db.schema.update=true \
-     -Dcommons.db.schema.update=true \
      -Dcommons.db.connection.host=${SQL_DB_ADDR} \
      -Dcommons.db.connection.port=${SQL_DB_PORT} \
      -Dcommons.eventbus.url=${SERVICE_BROKER_ADDR} \
