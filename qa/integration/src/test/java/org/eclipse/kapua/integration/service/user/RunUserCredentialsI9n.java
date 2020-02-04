52dac940090bdc1d9bea69d385b07470cf251c3c
code-incubus <nemanja.milutinovic@comtrade.com>
2020-02-04 09:36:00 +0100

    Added test scenarios for user password credential
    Signed-off-by: code-incubus <nemanja.milutinovic@comtrade.com>

--- qa/integration/src/test/java/org/eclipse/kapua/integration/service/user/RunUserCredentialsI9n.java
+++ qa/integration/src/test/java/org/eclipse/kapua/integration/service/user/RunUserCredentialsI9n.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Eurotech and/or its affiliates and others
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,7 +10,6 @@
  *     Eurotech - initial API and implementation
  *******************************************************************************/
 package org.eclipse.kapua.integration.service.user;
-
 import org.eclipse.kapua.qa.common.cucumber.CucumberProperty;
 import org.eclipse.kapua.qa.common.cucumber.CucumberWithProperties;
 import org.junit.runner.RunWith;
@@ -19,18 +18,12 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(CucumberWithProperties.class)
 @CucumberOptions(
-        features = "classpath:features/user/UserPermissionI9n.feature",
+        features = "classpath:features/user/UserCredentialsI9n.feature",
 
         glue = {"org.eclipse.kapua.qa.common",
                 "org.eclipse.kapua.service.account.steps",
                 "org.eclipse.kapua.service.user.steps",
                 "org.eclipse.kapua.service.authorization.steps",
-                "org.eclipse.kapua.service.device.registry.steps",
-                "org.eclipse.kapua.service.tag.steps",
-                "org.eclipse.kapua.service.job.steps",
-                "org.eclipse.kapua.service.datastore.steps",
-                "org.eclipse.kapua.service.scheduler.steps",
-                "org.eclipse.kapua.service.endpoint.steps"
         },
         plugin = {"pretty",
                 "html:target/cucumber/UserServiceI9n",
@@ -42,4 +35,5 @@ import cucumber.api.CucumberOptions;
 @CucumberProperty(key="datastore.client.class", value="org.eclipse.kapua.service.datastore.client.rest.RestDatastoreClient")
 @CucumberProperty(key="org.eclipse.kapua.qa.datastore.extraStartupDelay", value="5")
 @CucumberProperty(key="org.eclipse.kapua.qa.broker.extraStartupDelay", value="5")
-public class RunUserPermissionI9nTest {}
+public class RunUserCredentialsI9n {
+}
