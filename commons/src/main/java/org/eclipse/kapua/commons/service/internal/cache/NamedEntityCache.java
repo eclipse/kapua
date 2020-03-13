be31ac1966fd165fb4cd5af93b0ba1670e0930b1
Gianluca Barbon <gianluca.barbon@eurotech.com>
2020-03-13 13:57:41 +0100

    cache - Internal cache implementation
    Signed-off-by: Gianluca Barbon <gianluca.barbon@eurotech.com>

---
+++ commons/src/main/java/org/eclipse/kapua/commons/service/internal/cache/NamedEntityCache.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.commons.service.internal.cache;
+
+import org.eclipse.kapua.model.KapuaEntity;
+import org.eclipse.kapua.model.KapuaNamedEntity;
+import org.eclipse.kapua.model.id.KapuaId;
+
+import javax.cache.Cache;
+import java.io.Serializable;
+
+public class NamedEntityCache extends EntityCache {
+
+    protected Cache<Serializable, Serializable> nameCache;
+
+    public NamedEntityCache(String idCacheName, String nameCacheName) {
+        super(idCacheName);
+        nameCache = KapuaCacheManager.getCache(nameCacheName);
+    }
+
+    public KapuaEntity get(KapuaId scopeId, String name) {
+        if (name != null) {
+            KapuaId entityId = (KapuaId) nameCache.get(name);
+            return get(scopeId, entityId);
+        }
+        return null;
+    }
+
+    @Override
+    public void put(KapuaEntity entity) {
+        if (entity != null) {
+            idCache.put(entity.getId(), entity);
+            nameCache.put(((KapuaNamedEntity) entity).getName(), entity.getId());
+        }
+    }
+
+    @Override
+    public KapuaEntity remove(KapuaId scopeId, KapuaId kapuaId) {
+        KapuaEntity kapuaEntity = super.remove(scopeId, kapuaId);
+        if (kapuaEntity != null) {
+            nameCache.remove(((KapuaNamedEntity) kapuaEntity).getName());
+        }
+        return kapuaEntity;
+    }
+}
