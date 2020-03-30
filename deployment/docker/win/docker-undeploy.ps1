0fcd86e32cb197551ff6b814c2446baa9b278760
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2020-03-30 14:16:26 +0200

    Use Join-Path to combine paths in PowerShell scripts
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- deployment/docker/win/docker-undeploy.ps1
+++ deployment/docker/win/docker-undeploy.ps1
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2019 Eurotech and/or its affiliates and others
+# Copyright (c) 2019, 2020 Eurotech and/or its affiliates and others
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
@@ -11,12 +11,12 @@
 ###############################################################################
 
 $script_dir = Split-Path (Get-Variable MyInvocation).Value.MyCommand.Path
-$common_path = $script_dir + '\docker-common.ps1'
+$common_path = Join-Path $script_dir docker-common.ps1
 
 . $common_path
 
 Write-Host "Undeploying Eclipse Kapua..."
 
-docker-compose -f $script_dir'\..\compose\docker-compose.yml' down
+docker-compose -f (Join-Path $script_dir .. compose docker-compose.yml) down
 
 Write-Host "Undeploying Eclipse Kapua... DONE!"
