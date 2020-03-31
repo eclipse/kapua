d70ebbf2d97eb1ae3273fccb969015c67a7af4ae
coduz <alberto.codutti@eurotech.com>
2020-03-31 17:59:54 +0200

    Added option to open logs on Docker deploy - WIN
    Signed-off-by: coduz <alberto.codutti@eurotech.com>

---
+++ deployment/docker/win/docker-logs.ps1
@@ -0,0 +1,22 @@
+###############################################################################
+# Copyright (c) 2019, 2020 Eurotech and/or its affiliates and others
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Eurotech - initial implementation
+###############################################################################
+
+$script_dir = Split-Path (Get-Variable MyInvocation).Value.MyCommand.Path
+$common_path = Join-Path $script_dir docker-common.ps1
+
+. $common_path
+
+Write-Host "Opening Eclipse Kapua logs..."
+
+docker-compose -f (Join-Path $script_dir .. compose docker-compose.yml) logs -f
+
+Write-Host "Opening Eclipse Kapua logs... DONE!"
