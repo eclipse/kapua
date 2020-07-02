6f2597e099ed568f57f52596ba06a12e401db26e
Sonja <sonja.matic@comtrade.com>
2020-07-02 09:32:01 +0200

    Added Junit Tests
    Added Junit tests for EventStoreDAO class
    Added Junit tests for EventStoreFactoryImpl class
    Added Junit tests for EventStoreRecordImpl class
    Added Junit tests for EventStoreServiceImpl class
    
    Signed-off-by: Sonja <sonja.matic@comtrade.com>

---
+++ commons/src/test/java/org/eclipse/kapua/commons/service/event/store/internal/EventStoreFactoryImplTest.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.commons.service.event.store.internal;
+
+import org.eclipse.kapua.KapuaEntityCloneException;
+import org.eclipse.kapua.commons.model.id.KapuaEid;
+import org.eclipse.kapua.commons.service.event.store.api.EventStoreRecord;
+import org.eclipse.kapua.model.id.KapuaId;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.math.BigInteger;
+
+@Category(JUnitTests.class)
+public class EventStoreFactoryImplTest extends Assert {
+
+    @Test
+    public void newEntityTest() {
+        EventStoreFactoryImpl eventStoreFactoryImpl = new EventStoreFactoryImpl();
+        KapuaId[] scopeIdList = {null, new KapuaEid(BigInteger.ONE)};
+
+        for (KapuaId scopeId : scopeIdList) {
+            assertNotNull("Null not expected.", eventStoreFactoryImpl.newEntity(scopeId));
+            assertThat("EventStoreRecordImpl object expected.", eventStoreFactoryImpl.newEntity(scopeId), IsInstanceOf.instanceOf(EventStoreRecordImpl.class));
+        }
+    }
+
+    @Test
+    public void newCreatorTest() {
+        EventStoreFactoryImpl eventStoreFactoryImpl = new EventStoreFactoryImpl();
+        KapuaId[] scopeIdList = {null, new KapuaEid(BigInteger.ONE)};
+
+        for (KapuaId scopeId : scopeIdList) {
+            assertNotNull("Null not expected.", eventStoreFactoryImpl.newCreator(scopeId));
+            assertThat("EventStoreRecordImpl object expected.", eventStoreFactoryImpl.newCreator(scopeId), IsInstanceOf.instanceOf(EventStoreRecordCreatorImpl.class));
+        }
+    }
+
+    @Test
+    public void newQueryTest() {
+        EventStoreFactoryImpl eventStoreFactoryImpl = new EventStoreFactoryImpl();
+        KapuaId[] scopeIdList = {null, new KapuaEid(BigInteger.ONE)};
+
+        for (KapuaId scopeId : scopeIdList) {
+            assertNotNull("Null not expected.", eventStoreFactoryImpl.newQuery(scopeId));
+            assertThat("EventStoreQueryImpl object expected.", eventStoreFactoryImpl.newQuery(scopeId), IsInstanceOf.instanceOf(EventStoreQueryImpl.class));
+        }
+    }
+
+    @Test
+    public void newListResultTest() {
+        EventStoreFactoryImpl eventStoreFactoryImpl = new EventStoreFactoryImpl();
+
+        assertNotNull("Null not expected.", eventStoreFactoryImpl.newListResult());
+        assertThat("EventStoreRecordListResultImpl object expected.", eventStoreFactoryImpl.newListResult(), IsInstanceOf.instanceOf(EventStoreRecordListResultImpl.class));
+    }
+
+    @Test
+    public void cloneTest() {
+        EventStoreFactoryImpl eventStoreFactoryImpl = new EventStoreFactoryImpl();
+        EventStoreRecord eventStoreRecord = Mockito.mock(EventStoreRecord.class);
+        EventStoreRecord nullEventStoreRecord = null;
+        KapuaEntityCloneException kapuaEntityCloneException = new KapuaEntityCloneException(new Exception(), EventStoreRecord.TYPE, nullEventStoreRecord);
+
+        assertNotNull("Null not expected.", eventStoreFactoryImpl.clone(eventStoreRecord));
+        assertThat("EventStoreRecordImpl object expected.", eventStoreFactoryImpl.clone(eventStoreRecord), IsInstanceOf.instanceOf(EventStoreRecordImpl.class));
+
+        try {
+            eventStoreFactoryImpl.clone(nullEventStoreRecord);
+        } catch (Exception e) {
+            assertEquals("KapuaEntityCloneException expected.", kapuaEntityCloneException.toString(), e.toString());
+        }
+    }
+} 
---
+++ qa/integration/src/test/java/org/eclipse/kapua/integration/misc/EventStoreServiceImplTest.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.integration.misc;
+
+import org.eclipse.kapua.commons.jpa.EntityManagerFactory;
+import org.eclipse.kapua.commons.model.id.KapuaEid;
+import org.eclipse.kapua.commons.service.event.store.api.EventStoreRecordCreator;
+import org.eclipse.kapua.commons.service.event.store.internal.EventStoreRecordCreatorImpl;
+import org.eclipse.kapua.commons.service.event.store.internal.EventStoreServiceImpl;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.math.BigInteger;
+
+@Category(JUnitTests.class)
+public class EventStoreServiceImplTest extends Assert {
+
+    @Test
+    public void eventStoreServiceImplTest() {
+        EntityManagerFactory entityManagerFactory = Mockito.mock(EntityManagerFactory.class);
+        EventStoreServiceImpl eventStoreServiceImpl = new EventStoreServiceImpl(entityManagerFactory);
+        assertThat("EventStoreServiceImpl object expected.", eventStoreServiceImpl, IsInstanceOf.instanceOf(EventStoreServiceImpl.class));
+    }
+
+    @Test
+    public void createTest() {
+        EntityManagerFactory entityManagerFactory = Mockito.mock(EntityManagerFactory.class);
+        EventStoreServiceImpl eventStoreServiceImpl = new EventStoreServiceImpl(entityManagerFactory);
+        EventStoreRecordCreator[] creator = {null, new EventStoreRecordCreatorImpl(new KapuaEid(BigInteger.ONE))};
+        UnsupportedOperationException unsupportedOperationException = new UnsupportedOperationException();
+
+        for (EventStoreRecordCreator eventStoreRecordCreator : creator) {
+            try {
+                eventStoreServiceImpl.create(eventStoreRecordCreator);
+            } catch (Exception e) {
+                assertEquals("Expected and actual values should be the same.", unsupportedOperationException.toString(), e.toString());
+            }
+        }
+    }
+} 
