97a3eb71ec858e4252796cd06a329389fd7ff388
Sonja <sonja.matic@endava.com>
2020-09-07 09:06:45 +0200

    Added Junit Tests for Kura module
    Signed-off-by: Sonja <sonja.matic@endava.com>

---
+++ client/gateway/profile/kura/src/test/java/org/eclipse/kapua/client/gateway/kura/KuraBinaryPayloadCodecTest.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.client.gateway.kura;
+
+import java.nio.ByteBuffer;
+import java.time.Instant;
+
+import org.eclipse.kapua.client.gateway.Payload;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+@Category(JUnitTests.class)
+public class KuraBinaryPayloadCodecTest extends Assert {
+
+    KuraBinaryPayloadCodec.Builder builder;
+    Payload payload;
+
+    @Before
+    public void initialize() {
+        builder = new KuraBinaryPayloadCodec.Builder();
+        payload = Mockito.mock(Payload.class);
+    }
+
+    @Test
+    public void builderBuildTest() {
+        assertNotNull("Null not expected", builder);
+        assertThat("Instance of KuraBinaryPayloadCodec expected.", builder.build(), IsInstanceOf.instanceOf(KuraBinaryPayloadCodec.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void encodeNullPayloadTest() throws Exception {
+        ByteBuffer byteBuffer = Mockito.mock(ByteBuffer.class);
+        builder.build().encode(null, byteBuffer);
+    }
+
+    @Test
+    public void encodeNullBufferTest() throws Exception {
+        Mockito.when(payload.getTimestamp()).thenReturn(Instant.ofEpochSecond(10L));
+
+        assertNotNull("Null not expected.", builder.build().encode(payload, null));
+        assertThat("Instance of ByteBuffer expected.", builder.build().encode(payload, null), IsInstanceOf.instanceOf(ByteBuffer.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void encodeNullPayloadNullBufferTest() throws Exception {
+        builder.build().encode(null, null);
+    }
+
+    @Test
+    public void encodeMergedBufferTest() throws Exception {
+        ByteBuffer byteBuffer = Mockito.mock(ByteBuffer.class);
+        Mockito.when(payload.getTimestamp()).thenReturn(Instant.ofEpochSecond(10L));
+
+        assertNotNull("Null not expected.", builder.build().encode(payload, byteBuffer));
+        assertThat("Instance of ByteBuffer expected.", builder.build().encode(payload, byteBuffer), IsInstanceOf.instanceOf(ByteBuffer.class));
+    }
+
+    @Test
+    public void encodeTest() throws Exception {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+        Mockito.when(payload.getTimestamp()).thenReturn(Instant.ofEpochSecond(10L));
+
+        assertThat("Instance of ByteBuffer expected.", builder.build().encode(payload, byteBuffer), IsInstanceOf.instanceOf(ByteBuffer.class));
+        assertEquals("Expected and actual values should be the same.", 100, builder.build().encode(payload, byteBuffer).limit());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void decodeNullBufferTest() throws Exception {
+        builder.build().decode(null);
+    }
+
+    @Test
+    public void decodeTest() throws Exception {
+        ByteBuffer byteBuffer = Mockito.mock(ByteBuffer.class);
+
+        assertNotNull("Null not expected.", builder.build().decode(byteBuffer));
+        assertThat("Instance of Payload expected.", builder.build().decode(byteBuffer), IsInstanceOf.instanceOf(Payload.class));
+    }
+}
---
+++ client/gateway/profile/kura/src/test/java/org/eclipse/kapua/client/gateway/kura/KuraBirthCertificateModuleTest.java
@@ -0,0 +1,163 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.client.gateway.kura;
+
+import org.eclipse.kapua.client.gateway.mqtt.MqttModuleContext;
+import org.eclipse.kapua.client.gateway.spi.ModuleContext;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Optional;
+
+@Category(JUnitTests.class)
+public class KuraBirthCertificateModuleTest extends Assert {
+
+    Map<String, String> values;
+    Map<String, String> expectedValues;
+    KuraBirthCertificateModule.Builder builder;
+    Set<KuraBirthCertificateModule.Provider> expectedProviders;
+
+    @Before
+    public void initialize() {
+        values = new HashMap<>();
+        expectedValues = new HashMap<>();
+        builder = KuraBirthCertificateModule.newBuilder("Account Name");
+        expectedProviders = new HashSet<>();
+    }
+
+    @Test
+    public void providerProvideDataJVMTest() {
+        expectedValues.put("jvm_name", System.getProperty("java.vm.name"));
+        expectedValues.put("jvm_version", System.getProperty("java.version"));
+        expectedValues.put("os", System.getProperty("os.name"));
+        expectedValues.put("os_version", System.getProperty("os.version"));
+        expectedValues.put("os_arch", System.getProperty("os.arch"));
+
+        KuraBirthCertificateModule.Provider.JVM.provideData(values);
+
+        assertEquals("Expected and actual values should be the same.", expectedValues, values);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void providerProvideDataJVMNullValuesTest() {
+        KuraBirthCertificateModule.Provider.JVM.provideData(null);
+    }
+
+    @Test
+    public void provideDataProviderRUNTIMETest() {
+        expectedValues.put("available_processors", Integer.toString(Runtime.getRuntime().availableProcessors()));
+        expectedValues.put("total_memory", Long.toString(Runtime.getRuntime().totalMemory()));
+
+        KuraBirthCertificateModule.Provider.RUNTIME.provideData(values);
+
+        assertEquals("Expected and actual values should be the same.", expectedValues, values);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void providerProvideDataRUNTIMENullValuesTest() {
+        KuraBirthCertificateModule.Provider.RUNTIME.provideData(null);
+    }
+
+    @Test
+    public void builderDefaultProvidersTest() {
+        expectedProviders.add(KuraBirthCertificateModule.Provider.JVM);
+        expectedProviders.add(KuraBirthCertificateModule.Provider.RUNTIME);
+
+        builder.defaultProviders();
+
+        assertEquals("Expected and actual values should be the same.", expectedProviders, builder.providers());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void builderProviderNullParameterTest() {
+        builder.provider(null);
+    }
+
+    @Test
+    public void builderProviderTest() {
+        KuraBirthCertificateModule.Provider provider = Mockito.mock(KuraBirthCertificateModule.Provider.class);
+        expectedProviders.add(provider);
+
+        builder.provider(provider);
+
+        assertEquals("Expected and actual values should be the same.", expectedProviders, builder.providers());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void builderProvidersNullParameterTest() {
+        builder.providers(null);
+    }
+
+    @Test
+    public void builderProvidersTest() {
+        KuraBirthCertificateModule.Provider provider1 = Mockito.mock(KuraBirthCertificateModule.Provider.class);
+        KuraBirthCertificateModule.Provider provider2 = Mockito.mock(KuraBirthCertificateModule.Provider.class);
+        KuraBirthCertificateModule.Provider provider3 = Mockito.mock(KuraBirthCertificateModule.Provider.class);
+
+        Collection<KuraBirthCertificateModule.Provider> providers = new HashSet<>();
+        providers.add(provider1);
+        providers.add(provider2);
+        providers.add(provider3);
+        expectedProviders.add(provider1);
+        expectedProviders.add(provider2);
+        expectedProviders.add(provider3);
+
+        builder.providers(providers);
+
+        assertEquals("Expected and actual values should be the same.", expectedProviders, builder.providers());
+    }
+
+    @Test
+    public void builderBuildTest() {
+        assertThat("Instance of KuraBirthCertificateModule expected.", builder.build(), IsInstanceOf.instanceOf(KuraBirthCertificateModule.class));
+    }
+
+    @Test
+    public void newBuilderNullTest() {
+        assertThat("Instance of Builder expected.", KuraBirthCertificateModule.newBuilder(null), IsInstanceOf.instanceOf(KuraBirthCertificateModule.Builder.class));
+    }
+
+    @Test
+    public void initializeTest() {
+        ModuleContext moduleContext = Mockito.mock(ModuleContext.class);
+        Optional<MqttModuleContext> optional = Optional.of(Mockito.mock(MqttModuleContext.class));
+        Mockito.when(moduleContext.adapt(MqttModuleContext.class)).thenReturn(optional);
+        builder.defaultProviders();
+        try {
+            builder.build().initialize(moduleContext);
+        } catch (Exception e) {
+            fail("Exception not expected.");
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void initializeNullTest() {
+        builder.build().initialize(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void initializeIllegalStateExceptionTest() {
+        ModuleContext moduleContext = Mockito.mock(ModuleContext.class);
+        Mockito.when(moduleContext.adapt(MqttModuleContext.class)).thenReturn(Optional.empty());
+        builder.build().initialize(moduleContext);
+    }
+}
