d4c03fa9baadfab4e1949d1d275de1f0ab2a8083
code-incubus <nemanja.milutinovic@endava.com>
2020-10-06 14:38:15 +0200

    Added and refactored JUnit tests for API module
    Signed-off-by: code-incubus <nemanja.milutinovic@endava.com>

---
+++ client/gateway/api/src/test/java/org/eclipse/kapua/client/gateway/TransmissionExceptionTest.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.client.gateway;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(JUnitTests.class)
+public class TransmissionExceptionTest extends Assert {
+
+    TransmissionException transmissionException;
+    Throwable throwable;
+
+    String[] stringValues = {"", "!#$%&'()=?⁄@‹›€°·‚,.-;:_Èˇ¿<>«‘”’ÉØ∏{}|ÆæÒuF8FFÔÓÌÏÎÅ«»Ç◊Ñˆ¯Èˇ", "regularNaming", "regular Naming", "49", "regularNaming49", "NAMING", "246465494135646120009090049684646496468456468496846464968496844"};
+
+    @Before
+    public void createInstancesOfClasses() {
+        transmissionException = new TransmissionException("message", throwable);
+        throwable = new Throwable();
+    }
+
+    @Test
+    public void transmissionExceptionDefaultConstructorTest() {
+        TransmissionException transmissionException = new TransmissionException();
+        assertThat("Expected and actual values should be the same!", transmissionException, IsInstanceOf.instanceOf(TransmissionException.class));
+    }
+
+    @Test
+    public void transmissionExceptionTwoParameterConstructorValidTest() {
+        for (String value : stringValues) {
+            transmissionException = new TransmissionException(value, throwable);
+            assertEquals("Expected and actual values should be the same!", value, transmissionException.getMessage());
+            assertEquals("Expected and actual values should be the same!", throwable, transmissionException.getCause());
+        }
+    }
+
+    @Test
+    public void transmissionExceptionTwoParametersNullMessageTest() {
+        transmissionException = new TransmissionException(null, throwable);
+        assertNull("Null expected!", transmissionException.getMessage());
+        assertEquals("Expected and actual values should be the same!", throwable, transmissionException.getCause());
+    }
+
+    @Test
+    public void transmissionExceptionTwoParametersNullCauseTest() {
+        for (String value : stringValues) {
+            transmissionException = new TransmissionException(value, null);
+            assertEquals("Expected and actual values should be the same!", value, transmissionException.getMessage());
+            assertNull("Null expected!", transmissionException.getCause());
+        }
+    }
+
+    @Test
+    public void transmissionExceptionTwoParametersNullTest() {
+        transmissionException = new TransmissionException(null, null);
+        assertNull("Null expected!", transmissionException.getMessage());
+        assertNull("Null expected!", transmissionException.getCause());
+    }
+
+    @Test
+    public void transmissionExceptionMessageConstructorValidTest() {
+        for (String value : stringValues) {
+            transmissionException = new TransmissionException(value);
+            assertEquals("Expected and actual values should be the same!", value, transmissionException.getMessage());
+        }
+    }
+
+    @Test
+    public void transmissionExceptionMessageConstructorNullTest() {
+        transmissionException = new TransmissionException((String) null);
+        assertNull("Null expected!", transmissionException.getMessage());
+    }
+
+    @Test
+    public void transmissionExceptionThrowableConstructorValidTest() {
+        transmissionException = new TransmissionException(throwable);
+        assertEquals("Expected and actual values should be the same!", throwable, transmissionException.getCause());
+    }
+
+    @Test
+    public void transmissionExceptionThrowableConstructorNullTest() {
+        transmissionException = new TransmissionException(throwable);
+        assertEquals("Expected and actual values should be the same!", throwable, transmissionException.getCause());
+    }
+
+    @Test (expected = TransmissionException.class)
+    public void throwingExceptionTest() throws TransmissionException {
+        throw transmissionException;
+    }
+}
