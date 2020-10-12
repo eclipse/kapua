c79eb05439cb84089c18ad9d6786e7c592e6c59a
VesnaGrumic <vesna.grumic@endava.com>
2020-10-12 09:59:58 +0200

    Added JUnit tests for Client Module This commit contains tests for:  - tests for AbstractClient class  - tests for client/gateway/spi/util package
    Signed-off-by: VesnaGrumic <vesna.grumic@endava.com>
    Signed-off-by: Leonardo Gaube <leonardo.gaube@endava.com>

--- client/gateway/spi/src/test/java/org/eclipse/kapua/client/gateway/spi/util/BuffersTest.java
+++ client/gateway/spi/src/test/java/org/eclipse/kapua/client/gateway/spi/util/BuffersTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2020 Red Hat Inc and others.
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -8,59 +8,54 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Red Hat Inc - initial API and implementation
+ *     Eurotech - initial API and implementation
  *******************************************************************************/
 package org.eclipse.kapua.client.gateway.spi.util;
 
-import java.nio.ByteBuffer;
-
 import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.nio.ByteBuffer;
 
 @Category(JUnitTests.class)
-public class BuffersTest {
+public class BuffersTest extends Assert {
 
     @Test
-    public void test1() {
-        final ByteBuffer result = Buffers.wrap(new byte[0]);
-        Assert.assertNotNull(result);
-        Assert.assertFalse(result.hasRemaining());
+    public void buffersTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor<Buffers> constructor = Buffers.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
     }
 
     @Test
-    public void test2() {
-        final ByteBuffer result = Buffers.wrap(null);
-        Assert.assertNull(result);
+    public void wrapByteArrayDataNullTest() {
+        assertEquals("Expected and actual value should be the same", null, Buffers.wrap(null));
     }
 
     @Test
-    public void test3() {
-        final ByteBuffer result = Buffers.wrap(new byte[] { 12 });
-        Assert.assertNotNull(result);
-        result.flip();
-        Assert.assertEquals(1, result.remaining());
-        Assert.assertEquals(12, result.get());
+    public void wrapTest() {
+        final byte[] byteArray = new byte[]{1, 3, 5, 7, 9};
+        final ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
+        byteBuffer.position(byteBuffer.limit());
+        assertEquals("Expected and actual value should be the same", byteBuffer, Buffers.wrap(byteArray));
     }
 
     @Test
-    public void test4() {
-        final ByteBuffer input = ByteBuffer.allocate(1);
-        input.put((byte) 12);
-
-        input.flip();
-
-        final byte[] result = Buffers.toByteArray(input);
-        Assert.assertNotNull(result);
-        Assert.assertEquals(1, result.length);
-        Assert.assertEquals(12, result[0]);
+    public void toByteArrayBufferNullTest() {
+        assertEquals("Expected and actual value should be the same", null, Buffers.toByteArray(null));
     }
 
     @Test
-    public void test5() {
-        final byte[] result = Buffers.toByteArray(null);
-        Assert.assertNull(result);
+    public void toByteArrayTest() {
+        ByteBuffer byteBuffer = Mockito.mock(ByteBuffer.class);
+        assertThat("Instance of Buffers expected.", Buffers.toByteArray(byteBuffer), IsInstanceOf.instanceOf(byte[].class));
     }
-
 }
---
+++ client/gateway/spi/src/test/java/org/eclipse/kapua/client/gateway/spi/util/MoreExecutorsTest.java
@@ -0,0 +1,342 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2020 Red Hat Inc and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.client.gateway.spi.util;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Category(JUnitTests.class)
+public class MoreExecutorsTest extends Assert {
+
+    ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    ScheduledExecutorService executor = MoreExecutors.preventShutdown(executorService);
+
+    @Test
+    public void moreExecutorsTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor<MoreExecutors> constructor = MoreExecutors.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
+
+    @Test
+    public void preventShutdownTest() {
+        assertNotNull("Actual value shouldn't be null", MoreExecutors.preventShutdown(executorService));
+        assertThat("Instance of ScheduledExecutorService expected.", MoreExecutors.preventShutdown(executorService), IsInstanceOf.instanceOf(ScheduledExecutorService.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void preventShutdownNullExecutorTest() {
+        assertNotNull("Actual value shouldn't be null", MoreExecutors.preventShutdown(null));
+    }
+
+    @Test
+    public void scheduleRunnableIdTest() {
+        Runnable command = Mockito.mock(Runnable.class);
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule(command, delay, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleRunnableIdNullTest() {
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule((Runnable)null, delay, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleRunnableIdUnitNullTest() {
+        Runnable command = Mockito.mock(Runnable.class);
+        long delay = 1L;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule(command, delay, null), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test
+    public void scheduleRunnableIdDelayNullTest() {
+        Runnable command = Mockito.mock(Runnable.class);
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule(command, 0, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test
+    public void executeTest() {
+        Runnable command = Mockito.mock(Runnable.class);
+        executor.execute(command);
+        assertFalse(executor.isShutdown());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void executeNullTest() {
+        executor.execute(null);
+        assertFalse(executor.isShutdown());
+    }
+
+    @Test
+    public void scheduleCallableIdTest() {
+        Callable<Integer> callable = Mockito.mock(Callable.class);
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule(callable, delay, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleCallableIdNullTest() {
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule((Callable)null, delay, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleCallableIdUnitNullTest() {
+        Callable<Integer> callable = Mockito.mock(Callable.class);
+        long delay = 1L;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule(callable, delay, null), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test
+    public void scheduleCallableIdDelayNullTest() {
+        Callable<Integer> callable = Mockito.mock(Callable.class);
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.schedule(callable, 0, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test
+    public void scheduleAtFixedRateTest() {
+        Runnable command = Mockito.mock(Runnable.class);
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleAtFixedRate(command, delay, 1, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleAtFixedRateRunnableIdNullTest() {
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleAtFixedRate(null, delay, 1, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleAtFixedRateUnitIdNullTest() {
+        Runnable command = Mockito.mock(Runnable.class);
+        long delay = 1L;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleAtFixedRate(command, delay,1, null), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test
+    public void scheduleAtFixedRateDelayIdNullTest() {
+        Runnable command = Mockito.mock(Runnable.class);
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleAtFixedRate(command, 0,1, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shutdownTest() {
+        executor.shutdown();
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shutdownNowTest() {
+        executor.shutdownNow();
+    }
+
+    @Test
+    public void scheduleWithFixedDelayTest() {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        long initialDelay = 1L;
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleWithFixedDelay(runnable, initialDelay, delay, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleWithFixedDelayRunnableIdNullTest() {
+        long initialDelay = 1L;
+        long delay = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleWithFixedDelay(null, initialDelay, delay, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void scheduleWithFixedDelayUnitIdNullTest() {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        long initialDelay = 1L;
+        long delay = 1L;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleWithFixedDelay(runnable, initialDelay, delay, null), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test
+    public void scheduleWithFixedDelayInitialDelayIdNullTest() {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        TimeUnit unit = TimeUnit.MINUTES;
+        long delay = 1L;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleWithFixedDelay(runnable, 0, delay, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void scheduleWithFixedDelayDelayIdNullTest() {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        TimeUnit unit = TimeUnit.MINUTES;
+        long initialDelay = 1L;
+        assertThat("Instance of ScheduledFuture expected.", executor.scheduleWithFixedDelay(runnable, initialDelay, 0, unit), IsInstanceOf.instanceOf(ScheduledFuture.class));
+    }
+
+    @Test
+    public void isShutdownTest() {
+        assertFalse("Expected false", executor.isShutdown());
+    }
+
+    @Test
+    public void isTerminatedTest() {
+        assertFalse("Expected false", executor.isTerminated());
+    }
+
+    @Test
+    public void awaitTerminationTest() throws InterruptedException {
+        long timeout = 5L;
+        TimeUnit unit = TimeUnit.SECONDS;
+        assertFalse("Expected false", executor.awaitTermination(timeout, unit));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void awaitTerminationUnitIdNullTest() throws InterruptedException {
+        long timeout = 1L;
+        assertFalse("Expected false", executor.awaitTermination(timeout, null));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void submitCallableIdNullTest() {
+        assertThat("Instance of Future expected.", executor.submit((Callable)null), IsInstanceOf.instanceOf(Future.class));
+    }
+
+    @Test
+    public void submitCallableIdTest() {
+        Callable<Integer> callable = Mockito.mock(Callable.class);
+        assertThat("Instance of Future expected.", executor.submit(callable), IsInstanceOf.instanceOf(Future.class));
+    }
+
+    @Test
+    public void submitRunnableAndTIdTest() {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        String result = "result";
+        assertThat("Instance of Future expected.", executor.submit(runnable, result), IsInstanceOf.instanceOf(Future.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void submitRunnableNullAndTIdTest() {
+        String result = "result";
+        assertThat("Instance of Future expected.", executor.submit((Runnable)null, result), IsInstanceOf.instanceOf(Future.class));
+    }
+
+    @Test
+    public void submitRunnableAndTNullIdTest() {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        assertThat("Instance of Future expected.", executor.submit(runnable, null), IsInstanceOf.instanceOf(Future.class));
+    }
+
+    @Test
+    public void submitRunnableIdTest() {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        assertThat("Instance of Future expected.", executor.submit(runnable), IsInstanceOf.instanceOf(Future.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void submitRunnableIdNullTest() {
+        assertThat("Instance of Future expected.", executor.submit((Runnable)null), IsInstanceOf.instanceOf(Future.class));
+    }
+
+    @Test
+    public void invokeAllTasksIdTest() throws InterruptedException {
+        Collection<Callable<Object>> tasks = new ArrayList<>();
+        tasks.add(Mockito.mock(Callable.class));
+        tasks.add(Mockito.mock(Callable.class));
+        assertThat("Instance of List expected.", executor.invokeAll(tasks), IsInstanceOf.instanceOf(List.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void invokeAllTasksIdNullTest() throws InterruptedException {
+        assertThat("Instance of List expected.", executor.invokeAll(null), IsInstanceOf.instanceOf(List.class));
+    }
+
+    @Test
+    public void invokeAllTasksTimeoutAndUnitIdTest() throws InterruptedException {
+        Collection<Callable<Object>> tasks = new ArrayList<>();
+        tasks.add(Mockito.mock(Callable.class));
+        tasks.add(Mockito.mock(Callable.class));
+        long timeout = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of List expected.", executor.invokeAll(tasks, timeout, unit), IsInstanceOf.instanceOf(List.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void invokeAllTasksNullTimeoutAndUnitIdTest() throws InterruptedException {
+        long timeout = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertThat("Instance of List expected.", executor.invokeAll(null, timeout, unit), IsInstanceOf.instanceOf(List.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void invokeAllTasksTimeoutAndUnitNullIdTest() throws InterruptedException {
+        Collection<Callable<Object>> tasks = new ArrayList<>();
+        tasks.add(Mockito.mock(Callable.class));
+        tasks.add(Mockito.mock(Callable.class));
+        long timeout = 1L;
+        assertThat("Instance of List expected.", executor.invokeAll(tasks, timeout, null), IsInstanceOf.instanceOf(List.class));
+    }
+
+    @Test
+    public void invokeAnyTasksIdTest() throws ExecutionException, InterruptedException {
+        Collection<Callable<Object>> tasks = new ArrayList<>();
+        tasks.add(Mockito.mock(Callable.class));
+        tasks.add(Mockito.mock(Callable.class));
+        assertEquals("Actual and expected values are not the same", executor.invokeAny(tasks), executorService.invokeAny(tasks));
+    }
+
+    @Test
+    public void invokeAnyTasksTimeoutAndUnitIdTest() throws InterruptedException, ExecutionException, TimeoutException {
+        Collection<Callable<Object>> tasks = new ArrayList<>();
+        tasks.add(Mockito.mock(Callable.class));
+        tasks.add(Mockito.mock(Callable.class));
+        long timeout = 1L;
+        TimeUnit unit = TimeUnit.MINUTES;
+        assertEquals("Actual and expected values are not the same", executor.invokeAny(tasks, timeout, unit), executorService.invokeAny(tasks, timeout, unit));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void invokeAnyTasksTimeoutAndUnitNullIdTest() throws InterruptedException, ExecutionException, TimeoutException {
+        Collection<Callable<Object>> tasks = new ArrayList<>();
+        tasks.add(Mockito.mock(Callable.class));
+        tasks.add(Mockito.mock(Callable.class));
+        long timeout = 1L;
+        assertEquals("Actual and expected values are not the same", executor.invokeAny(tasks, timeout, null), executorService.invokeAny(tasks, timeout, null));
+    }
+}
