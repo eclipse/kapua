e4855408f1f195bf476d339e939a51a74c8dae6a
Sonja <sonja.matic@endava.com>
2020-10-12 12:35:47 +0200

    Added Junit tests for broker module
    Added Junit test for classes in:
    
    broker/api module:
    BrokerDomain class
    BrokerDomains class
    
    broker/core/message package:
    CamelKapuaMessage class
    CamelUtil class
    MessageConstants class
    DefaultSystemMessageCreator class
    
    broker/core/plugin/metric package:
    ClientMetric class
    LoginMetric class
    PublishMetric class
    SecurityMetrics class
    SubscribeMetric class
    
    Signed-off-by: Sonja <sonja.matic@endava.com>

---
+++ broker/api/src/test/java/org/eclipse/kapua/broker/BrokerDomainsTest.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.broker;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
+@Category(JUnitTests.class)
+public class BrokerDomainsTest extends Assert {
+
+    @Test
+    public void brokerDomainsTest() throws Exception {
+        Constructor<BrokerDomains> brokerDomains = BrokerDomains.class.getDeclaredConstructor();
+        assertTrue("True expected.", Modifier.isPrivate(brokerDomains.getModifiers()));
+        brokerDomains.setAccessible(true);
+        brokerDomains.newInstance();
+    }
+
+    @Test
+    public void brokerDomainTest() {
+        BrokerDomain brokerDomain = BrokerDomains.BROKER_DOMAIN;
+        assertNotNull("NotNull expected.", brokerDomain);
+    }
+}
