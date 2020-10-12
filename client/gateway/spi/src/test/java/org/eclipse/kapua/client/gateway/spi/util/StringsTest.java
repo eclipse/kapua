ec6cffbce18a68b2555c32ad4e7c245ddf59788d
VesnaGrumic <vesna.grumic@endava.com>
2020-10-12 09:59:58 +0200

    Added JUnit tests
    Signed-off-by: VesnaGrumic <vesna.grumic@endava.com>
    
    Added JUnit tests for Client Module
    This PR contains unit tests for:
    
     - client/gateway/spi/AbstractClient class
     - client/gateway/spi/util package
    
    This has been done by Vesna Grumic, but because she is not on project anymore, I am adding this in her name.
    
    Signed-off-by: Leonardo Gaube leonardo.gaube@endava.com

--- client/gateway/spi/src/test/java/org/eclipse/kapua/client/gateway/spi/util/StringsTest.java
+++ client/gateway/spi/src/test/java/org/eclipse/kapua/client/gateway/spi/util/StringsTest.java
@@ -8,29 +8,60 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Red Hat Inc - initial API and implementation
+ *     Eurotech - initial API and implementation
  *******************************************************************************/
 package org.eclipse.kapua.client.gateway.spi.util;
 
 import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @Category(JUnitTests.class)
-public class StringsTest {
+@RunWith(value = Parameterized.class)
+public class StringsTest extends Assert {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test1() {
-        Strings.nonEmptyText(null, "foo");
+    private final String string;
+
+    private final String fieldName;
+
+    public StringsTest(String string, String fieldName) {
+        this.string = string;
+        this.fieldName = fieldName;
+    }
+
+    @Parameters
+    public static Collection<Object[]> names() {
+        return Arrays.asList(new Object[][]{
+                {"String", null},
+                {"String", ""},
+                {"string", "fieldName"},
+                {"STRING", "FIELDNAME"},
+                {"string name", "field name"},
+                {"0123456789", "0123456789"},
+                {"!#$%&'()=?⁄@‹›€°·‚,.-;:_Èˇ¿<>«‘”’ÉØ∏{}|ÆæÒuF8FFÔÓÌÏÎÅ«»Ç◊Ñˆ¯Èˇ", "!#$%&'()=?⁄@‹›€°·‚,.-;:_Èˇ¿<>«‘”’ÉØ∏{}|ÆæÒuF8FFÔÓÌÏÎÅ«»Ç◊Ñˆ¯Èˇ"},
+                {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefg", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefg"},
+        });
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void test2() {
-        Strings.nonEmptyText("", "foo");
+    public void nonEmptyTextNullStringTest() {
+        Strings.nonEmptyText(null, fieldName);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nonEmptyTextEmptyStringTest() {
+        Strings.nonEmptyText("", fieldName);
     }
 
     @Test
-    public void test3() {
-        Strings.nonEmptyText("foo", "foo");
+    public void nonEmptyTextTest() {
+        Strings.nonEmptyText(string, fieldName);
     }
 }
