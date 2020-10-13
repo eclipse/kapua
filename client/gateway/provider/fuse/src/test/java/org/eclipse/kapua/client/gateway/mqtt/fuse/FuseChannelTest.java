c3a8420c259ae768c5fa5f49fe09b02468821243
VesnaGrumic <vesna.grumic@endava.com>
2020-10-13 10:44:37 +0200

    Added JUnit tests for provider package
    Signed-off-by: VesnaGrumic <vesna.grumic@endava.com>

---
+++ client/gateway/provider/fuse/src/test/java/org/eclipse/kapua/client/gateway/mqtt/fuse/FuseChannelTest.java
@@ -0,0 +1,216 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.client.gateway.mqtt.fuse;
+
+import org.eclipse.kapua.client.gateway.BinaryPayloadCodec;
+import org.eclipse.kapua.client.gateway.Credentials;
+import org.eclipse.kapua.client.gateway.mqtt.MqttMessageHandler;
+import org.eclipse.kapua.client.gateway.mqtt.MqttNamespace;
+import org.eclipse.kapua.client.gateway.spi.Channel;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.fusesource.hawtbuf.Buffer;
+import org.fusesource.mqtt.client.Callback;
+import org.fusesource.mqtt.client.CallbackConnection;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.util.HashSet;
+import java.util.Set;
+
+@Category(JUnitTests.class)
+public class FuseChannelTest extends Assert {
+
+    private FuseChannel.Builder builder;
+
+    @Before
+    public void init() throws URISyntaxException {
+        builder = new FuseChannel.Builder();
+        builder.broker(new URI("string"));
+        builder.clientId("clientId");
+        builder.namespace(Mockito.mock(MqttNamespace.class));
+        builder.codec(Mockito.mock(BinaryPayloadCodec.class));
+        builder.credentials(Mockito.mock(Credentials.UserAndPassword.class));
+    }
+
+    @Test
+    public void builderBuilderTest() {
+        FuseChannel.Builder builderExpected = new FuseChannel.Builder();
+        assertEquals("Actual and expected values should be the same.", builderExpected, builderExpected.builder());
+    }
+
+    @Test(expected = NoClassDefFoundError.class)
+    public void builderBuildTest() throws Exception {
+        builder.credentials(Credentials.userAndPassword("kapua-sys", new char[]{'a', 'b', 'c'}));
+        assertThat("Instance of FuseChannel expected.", builder.build(), IsInstanceOf.instanceOf(FuseChannel.class));
+    }
+
+    @Test(expected = NoClassDefFoundError.class)
+    public void builderBuildCredentialsNullTest() throws Exception {
+        builder.credentials(null);
+        assertThat("Instance of FuseChannel expected.", builder.build(), IsInstanceOf.instanceOf(FuseChannel.class));
+    }
+
+    @Test
+    public void handleConnectedTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        Channel.Context context = Mockito.mock(Channel.Context.class);
+        fuseChannel.handleInit(context);
+        fuseChannel.handleConnected();
+        Mockito.verify(context).notifyConnected();
+    }
+
+    @Test
+    public void handleDisconnectedTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        Channel.Context context = Mockito.mock(Channel.Context.class);
+        fuseChannel.handleInit(context);
+        fuseChannel.handleClose(context);
+        fuseChannel.handleDisconnected();
+        Mockito.verify(context).notifyDisconnected();
+    }
+
+    @Test
+    public void handleInitTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        Channel.Context context = Mockito.mock(Channel.Context.class);
+        fuseChannel.handleInit(context);
+        final CallbackConnection connection = Mockito.mock(CallbackConnection.class);
+        Mockito.verify(connection, Mockito.times(0)).connect(null);
+    }
+
+    @Test
+    public void handleInitContextNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        fuseChannel.handleInit(null);
+        final CallbackConnection connection = Mockito.mock(CallbackConnection.class);
+        Mockito.verify(connection, Mockito.times(0)).connect(null);
+    }
+
+    @Test
+    public void handleCloseTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        Channel.Context context = Mockito.mock(Channel.Context.class);
+        fuseChannel.handleClose(context);
+        final CallbackConnection connection = Mockito.mock(CallbackConnection.class);
+        Mockito.verify(connection, Mockito.times(0)).disconnect(null);
+    }
+
+    @Test
+    public void handleCloseContextNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        fuseChannel.handleClose(null);
+        final CallbackConnection connection = Mockito.mock(CallbackConnection.class);
+        Mockito.verify(connection, Mockito.times(0)).disconnect(null);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void publishMqttTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final String topic = "string";
+        fuseChannel.handleInit(Mockito.mock(Channel.Context.class));
+        final ByteBuffer payload = Mockito.mock(ByteBuffer.class);
+        fuseChannel.publishMqtt(topic, payload);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void publishMqttTopicNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        fuseChannel.handleInit(Mockito.mock(Channel.Context.class));
+        final ByteBuffer payload = Mockito.mock(ByteBuffer.class);
+        fuseChannel.publishMqtt(null, payload);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void publishMqttPayloadNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final String topic = "string";
+        fuseChannel.handleInit(Mockito.mock(Channel.Context.class));
+        fuseChannel.publishMqtt(topic, null);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void subscribeMqttTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final String topic = "string";
+        final MqttMessageHandler messageHandler = Mockito.mock(MqttMessageHandler.class);
+        fuseChannel.subscribeMqtt(topic, messageHandler);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void subscribeMqttTopicNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final MqttMessageHandler messageHandler = Mockito.mock(MqttMessageHandler.class);
+        fuseChannel.subscribeMqtt(null, messageHandler);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void subscribeMqttMessageHandlerNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final String topic = "string";
+        fuseChannel.subscribeMqtt(topic, null);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void unsubscribeMqttTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        Set<String> topics = new HashSet<>();
+        topics.add("India");
+        fuseChannel.unsubscribeMqtt(topics);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void unsubscribeMqttTopicsNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        fuseChannel.unsubscribeMqtt(null);
+    }
+
+    @Test
+    public void handleMessageArrivedTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final String topic = "string";
+        final Buffer payload = Mockito.mock(Buffer.class);
+        final Callback<Callback<Void>> ack = Mockito.mock(Callback.class);
+        fuseChannel.handleMessageArrived(topic, payload, ack);
+    }
+
+    @Test
+    public void handleMessageArrivedTopicNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final Buffer payload = Mockito.mock(Buffer.class);
+        final Callback<Callback<Void>> ack = Mockito.mock(Callback.class);
+        fuseChannel.handleMessageArrived(null, payload, ack);
+    }
+
+    @Test
+    public void handleMessageArrivedPayloadNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final String topic = "string";
+        final Callback<Callback<Void>> ack = Mockito.mock(Callback.class);
+        fuseChannel.handleMessageArrived(topic, null, ack);
+    }
+
+    @Test
+    public void handleMessageArrivedAckNullTest() throws Exception {
+        FuseChannel fuseChannel = builder.build();
+        final String topic = "string";
+        final Buffer payload = Mockito.mock(Buffer.class);
+        final Callback<Callback<Void>> ack = Mockito.mock(Callback.class);
+        fuseChannel.handleMessageArrived(topic, payload, null);
+    }
+}
---
+++ client/gateway/provider/fuse/src/test/java/org/eclipse/kapua/client/gateway/mqtt/fuse/internal/CallbacksTest.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.client.gateway.mqtt.fuse.internal;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.fusesource.mqtt.client.Callback;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.concurrent.CompletableFuture;
+
+@Category(JUnitTests.class)
+public class CallbacksTest extends Assert {
+
+    @Test
+    public void callbacksTest() throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        Constructor<Callbacks> constructor = Callbacks.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
+
+    @Test
+    public void asCallbackTest() {
+        final CompletableFuture<Object> future = Mockito.mock(CompletableFuture.class);
+        Throwable value = Mockito.mock(Throwable.class);
+        Callback<Object> callbacks = Callbacks.asCallback(future);
+        callbacks.onSuccess(future);
+        callbacks.onFailure(value);
+        Mockito.verify(future).complete(future);
+        Mockito.verify(future).completeExceptionally(value);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void asCallbackFutureNullTest() {
+        Callback<Object> callbacks = Callbacks.asCallback(null);
+        callbacks.onSuccess(null);
+    }
+
+    @Test
+    public void asCallbackThrowableNullTest() {
+        final CompletableFuture<Object> future = Mockito.mock(CompletableFuture.class);
+        Callback<Object> callbacks = Callbacks.asCallback(future);
+        callbacks.onSuccess(future);
+        callbacks.onFailure(null);
+        Mockito.verify(future).complete(future);
+        Mockito.verify(future).completeExceptionally(null);
+    }
+}
