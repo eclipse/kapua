9ac4790255700a429f0705ad7a0c89fce2b9d371
Sonja <sonja.matic@endava.com>
2020-10-16 14:43:02 +0200

    Added Junit tests for broker/core/plugin package
    Added Junit tests for class in broker/core/plugin package:
    ConnectorDescriptorProviders class
    ConnectorDescriptor class
    DefaultBrokerIdResolver class
    DefaultBrokerIpResolver class
    KapuaDuplicateClientIdException class
    KapuaPrincipalImpl class
    KapuaSecurityContext class
    
    Signed-off-by: Sonja <sonja.matic@endava.com>

---
+++ broker/core/src/test/java/org/eclipse/kapua/broker/core/plugin/DefaultBrokerIdResolverTest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.broker.core.plugin;
+
+import org.apache.activemq.broker.BrokerFilter;
+import org.apache.activemq.command.BrokerId;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+@Category(JUnitTests.class)
+public class DefaultBrokerIdResolverTest extends Assert {
+
+    DefaultBrokerIdResolver defaultBrokerIdResolver;
+
+    @Before
+    public void initialize() {
+        defaultBrokerIdResolver = new DefaultBrokerIdResolver();
+    }
+
+    @Test
+    public void getBrokerIdTest() {
+        String[] ids = {"", "broker id", "id1234567890", "id_)(*&^%$#@!?.,,.|", "broker-id123", "321broker_id!@", " bRoKeR&ID=99", ".broker1!*", "BROKER <id1> "};
+        BrokerFilter brokerFilter = Mockito.mock(BrokerFilter.class);
+
+        for (String id : ids) {
+            BrokerId brokerId = new BrokerId(id);
+            Mockito.when(brokerFilter.getBrokerId()).thenReturn(brokerId);
+            assertEquals("Expected and actual values should be the same", id, defaultBrokerIdResolver.getBrokerId(brokerFilter));
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getBrokerIdNullTest() {
+        defaultBrokerIdResolver.getBrokerId(null);
+    }
+}
