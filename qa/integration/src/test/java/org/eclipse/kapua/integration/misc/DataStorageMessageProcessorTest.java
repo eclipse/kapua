0b243f2fa29c648a52787fa09bf877b2a67659bf
Sonja <sonja.matic@endava.com>
2020-11-17 14:30:51 +0100

    Added JUnit tests for classes in listener package
    Added JUnit tests for classes in broker/core/listener package:
    AbstractListener class
    AbstractProcessor class
    CamelConstants class
    DataStorageMessageProcessor class
    DataStoreMetrics class
    DeviceManagementNotificationMessageProcessor class
    DeviceManagementRegistryNotificationMetrics class
    ErrorMessageListener class
    
    Signed-off-by: Sonja <sonja.matic@endava.com>

---
+++ qa/integration/src/test/java/org/eclipse/kapua/integration/misc/DataStorageMessageProcessorTest.java
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.integration.misc;
+
+import com.codahale.metrics.Counter;
+import org.apache.camel.Exchange;
+import org.eclipse.kapua.KapuaException;
+import org.eclipse.kapua.broker.core.listener.DataStorageMessageProcessor;
+import org.eclipse.kapua.broker.core.listener.DataStoreMetrics;
+import org.eclipse.kapua.broker.core.message.CamelKapuaMessage;
+import org.eclipse.kapua.commons.metric.MetricServiceFactory;
+import org.eclipse.kapua.message.KapuaMessage;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.util.UUID;
+
+@Category(JUnitTests.class)
+public class DataStorageMessageProcessorTest extends Assert {
+
+    DataStorageMessageProcessor dataStorageMessageProcessor;
+    CamelKapuaMessage camelKapuaMessage;
+    KapuaMessage kapuaMessage;
+    Exchange exchange;
+    Counter queueConfigurationErrorCount, queueGenericErrorCount;
+
+    @Before
+    public void initialize() {
+        dataStorageMessageProcessor = new DataStorageMessageProcessor();
+        camelKapuaMessage = Mockito.mock(CamelKapuaMessage.class);
+        kapuaMessage = Mockito.mock(KapuaMessage.class);
+        exchange = Mockito.mock(Exchange.class);
+        queueConfigurationErrorCount = MetricServiceFactory.getInstance().getCounter(DataStoreMetrics.METRIC_MODULE_NAME, DataStoreMetrics.METRIC_COMPONENT_NAME, DataStoreMetrics.METRIC_STORE, DataStoreMetrics.METRIC_QUEUE, DataStoreMetrics.METRIC_CONFIGURATION, DataStoreMetrics.METRIC_ERROR, DataStoreMetrics.METRIC_COUNT);
+        queueGenericErrorCount = MetricServiceFactory.getInstance().getCounter(DataStoreMetrics.METRIC_MODULE_NAME, DataStoreMetrics.METRIC_COMPONENT_NAME, DataStoreMetrics.METRIC_STORE, DataStoreMetrics.METRIC_QUEUE, DataStoreMetrics.METRIC_GENERIC, DataStoreMetrics.METRIC_ERROR, DataStoreMetrics.METRIC_COUNT);
+
+        Mockito.when(camelKapuaMessage.getMessage()).thenReturn(kapuaMessage);
+        Mockito.when(kapuaMessage.getId()).thenReturn(new UUID(10, 1));
+        Mockito.when(camelKapuaMessage.getDatastoreId()).thenReturn("datastore ID");
+    }
+
+    @Test
+    public void processConfigurationErrorMessageTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        dataStorageMessageProcessor.processConfigurationErrorMessage(exchange, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueConfigurationErrorCount.getCount());
+
+        queueConfigurationErrorCount.inc();
+    }
+
+    @Test
+    public void processConfigurationErrorMessageNullExchangeTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        dataStorageMessageProcessor.processConfigurationErrorMessage(null, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueConfigurationErrorCount.getCount());
+
+        queueConfigurationErrorCount.inc();
+    }
+
+    @Test
+    public void processConfigurationErrorNullMessageTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        try {
+            dataStorageMessageProcessor.processConfigurationErrorMessage(exchange, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+    }
+
+    @Test
+    public void processConfigurationErrorNullTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        try {
+            dataStorageMessageProcessor.processConfigurationErrorMessage(null, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+    }
+
+    @Test
+    public void processGenericErrorMessageTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        dataStorageMessageProcessor.processGenericErrorMessage(exchange, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueGenericErrorCount.getCount());
+
+        queueGenericErrorCount.inc();
+    }
+
+    @Test
+    public void processGenericErrorMessageNullExchangeTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        dataStorageMessageProcessor.processGenericErrorMessage(null, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueGenericErrorCount.getCount());
+
+        queueGenericErrorCount.inc();
+    }
+
+    @Test
+    public void processGenericErrorNullMessageTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        try {
+            dataStorageMessageProcessor.processGenericErrorMessage(exchange, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+    }
+
+    @Test
+    public void processGenericErrorNullTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        try {
+            dataStorageMessageProcessor.processGenericErrorMessage(null, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+    }
+}
---
+++ qa/integration/src/test/java/org/eclipse/kapua/integration/misc/DeviceManagementNotificationMessageProcessorTest.java
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.integration.misc;
+
+import com.codahale.metrics.Counter;
+import org.apache.camel.Exchange;
+import org.eclipse.kapua.KapuaException;
+import org.eclipse.kapua.broker.core.listener.DeviceManagementNotificationMessageProcessor;
+import org.eclipse.kapua.broker.core.listener.DeviceManagementRegistryNotificationMetrics;
+import org.eclipse.kapua.broker.core.message.CamelKapuaMessage;
+import org.eclipse.kapua.commons.metric.MetricServiceFactory;
+import org.eclipse.kapua.message.KapuaMessage;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+import java.util.UUID;
+
+@Category(JUnitTests.class)
+public class DeviceManagementNotificationMessageProcessorTest extends Assert {
+
+    DeviceManagementNotificationMessageProcessor deviceManagementNotificationMessageProcessor;
+    CamelKapuaMessage camelKapuaMessage;
+    Exchange exchange;
+    KapuaMessage message;
+    Counter queueConfigurationErrorCount, queueGenericErrorCount;
+
+    @Before
+    public void initialize() {
+        deviceManagementNotificationMessageProcessor = new DeviceManagementNotificationMessageProcessor();
+        camelKapuaMessage = Mockito.mock(CamelKapuaMessage.class);
+        exchange = Mockito.mock(Exchange.class);
+        message = Mockito.mock(KapuaMessage.class);
+        queueConfigurationErrorCount = MetricServiceFactory.getInstance().getCounter(DeviceManagementRegistryNotificationMetrics.METRIC_MODULE_NAME, DeviceManagementRegistryNotificationMetrics.METRIC_COMPONENT_NOTIFICATION, DeviceManagementRegistryNotificationMetrics.METRIC_PROCESS_QUEUE, DeviceManagementRegistryNotificationMetrics.METRIC_CONFIGURATION, DeviceManagementRegistryNotificationMetrics.METRIC_ERROR, DeviceManagementRegistryNotificationMetrics.METRIC_COUNT);
+        queueGenericErrorCount = MetricServiceFactory.getInstance().getCounter(DeviceManagementRegistryNotificationMetrics.METRIC_MODULE_NAME, DeviceManagementRegistryNotificationMetrics.METRIC_COMPONENT_NOTIFICATION, DeviceManagementRegistryNotificationMetrics.METRIC_PROCESS_QUEUE, DeviceManagementRegistryNotificationMetrics.METRIC_GENERIC, DeviceManagementRegistryNotificationMetrics.METRIC_ERROR, DeviceManagementRegistryNotificationMetrics.METRIC_COUNT);
+
+        Mockito.when(camelKapuaMessage.getDatastoreId()).thenReturn("datastoreID");
+        Mockito.when(camelKapuaMessage.getMessage()).thenReturn(message);
+        Mockito.when(message.getId()).thenReturn(new UUID(1, 10));
+    }
+
+    @Test
+    public void processConfigurationErrorMessageTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        deviceManagementNotificationMessageProcessor.processConfigurationErrorMessage(exchange, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueConfigurationErrorCount.getCount());
+
+        queueConfigurationErrorCount.inc();
+    }
+
+    @Test
+    public void processConfigurationErrorMessageNullExchangeTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        deviceManagementNotificationMessageProcessor.processConfigurationErrorMessage(null, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueConfigurationErrorCount.getCount());
+
+        queueConfigurationErrorCount.inc();
+    }
+
+    @Test
+    public void processConfigurationErrorNullMessageTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        try {
+            deviceManagementNotificationMessageProcessor.processConfigurationErrorMessage(exchange, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+    }
+
+    @Test
+    public void processConfigurationErrorNullTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+        try {
+            deviceManagementNotificationMessageProcessor.processConfigurationErrorMessage(null, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueConfigurationErrorCount.getCount());
+    }
+
+    @Test
+    public void processGenericErrorMessageTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        deviceManagementNotificationMessageProcessor.processGenericErrorMessage(exchange, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueGenericErrorCount.getCount());
+
+        queueGenericErrorCount.inc();
+    }
+
+    @Test
+    public void processGenericErrorMessageNullExchangeTest() throws KapuaException {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        deviceManagementNotificationMessageProcessor.processGenericErrorMessage(null, camelKapuaMessage);
+        assertEquals("Expected and actual values should be the same.", -1L, queueGenericErrorCount.getCount());
+
+        queueGenericErrorCount.inc();
+    }
+
+    @Test
+    public void processGenericErrorNullMessageTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        try {
+            deviceManagementNotificationMessageProcessor.processGenericErrorMessage(exchange, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+    }
+
+    @Test
+    public void processGenericErrorNullTest() {
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+        try {
+            deviceManagementNotificationMessageProcessor.processGenericErrorMessage(null, null);
+            fail("NullPointerException expected.");
+        } catch (Exception e) {
+            assertEquals("NullPointerException expected.", new NullPointerException().toString(), e.toString());
+        }
+        assertEquals("Expected and actual values should be the same.", 0L, queueGenericErrorCount.getCount());
+    }
+}
