074863fa47ef5c6c67221a462bcdf5f6ea3e6917
Sonja <sonja.matic@endava.com>
2021-01-28 09:13:52 +0100

    Added JUnit tests for exceptions, registration, setting, utils packages
    Added JUnit tests for classes is shiro module in packages:
    
     - exceptions
         AuthenticationRuntimeException class
         ExpiredAccountException class
         JwtCertificateNotFoundException class
         MfaRequiredException class
         TemporaryLockedAccountException class
    
     - registration
        RegistrationServiceImpl class
    
     - setting
        KapuaAuthenticationSettingKeys class
        KapuaAuthenticationSetting class
        KapuaCryptoSettingKeys class
        KapuaCryptoSetting class
    
     - utils
        AuthenticationUtils class
        JwtProcessors class
    
    Signed-off-by: Sonja <sonja.matic@endava.com>

---
+++ service/security/shiro/src/test/java/org/eclipse/kapua/service/authentication/shiro/registration/RegistrationServiceImplTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.authentication.shiro.registration;
+
+import org.eclipse.kapua.KapuaException;
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.eclipse.kapua.service.authentication.JwtCredentials;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+
+@Category(JUnitTests.class)
+public class RegistrationServiceImplTest extends Assert {
+
+    @Test
+    public void registrationServiceImplTest() {
+        try {
+            new RegistrationServiceImpl();
+        } catch (Exception e) {
+            fail("Exception not expected.");
+        }
+    }
+
+    @Test
+    public void closeTest() throws Exception {
+        RegistrationServiceImpl registrationServiceImpl = new RegistrationServiceImpl();
+        try {
+            registrationServiceImpl.close();
+        } catch (Exception e) {
+            fail("Exception not expected.");
+        }
+    }
+
+    @Test
+    public void isAccountCreationEnabledTrueEmptyProcessorsTest() throws KapuaException {
+        System.setProperty("authentication.registration.service.enabled", "true");
+        RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+        assertFalse("False expected.", registrationService.isAccountCreationEnabled());
+    }
+
+    @Test
+    public void isAccountCreationEnabledFalseTest() throws KapuaException {
+        System.setProperty("authentication.registration.service.enabled", "false");
+        RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+        assertFalse("False expected.", registrationService.isAccountCreationEnabled());
+    }
+
+    @Test
+    public void createAccountCreationNotEnabledTest() throws KapuaException {
+        JwtCredentials jwtCredentials = Mockito.mock(JwtCredentials.class);
+        assertFalse("False expected.", new RegistrationServiceImpl().createAccount(jwtCredentials));
+    }
+
+    @Test
+    public void createAccountNullTest() throws KapuaException {
+        assertFalse("False expected.", new RegistrationServiceImpl().createAccount(null));
+    }
+}
