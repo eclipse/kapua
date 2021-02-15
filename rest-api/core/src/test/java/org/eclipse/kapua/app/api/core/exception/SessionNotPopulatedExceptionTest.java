0e7c7cf9f41f11fde6c0c7ea2ac9ac9dd7f3fe08
Sonja <sonja.matic@endava.com>
2021-02-15 13:50:52 +0100

    Added JUnit tests for exception and model packages
    Added JUnit tests for classes in exception and model packages in
    rest-api/core module:
    
     - exception package:
    RestApiRuntimeException class
    SessionNotPopulatedException class
    
    - model package
    ByteArrayParam class
    CountResult class
    DateParam class
    EntityId class
    MetricType class
    ScopeId class
    StorableEntityId class
    
    Added JUnit tests for class AbstractKapuaResource
    
    Signed-off-by: Sonja <sonja.matic@endava.com>

---
+++ rest-api/core/src/test/java/org/eclipse/kapua/app/api/core/exception/SessionNotPopulatedExceptionTest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.app.api.core.exception;
+
+        import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+        import org.junit.Assert;
+        import org.junit.Test;
+        import org.junit.experimental.categories.Category;
+
+@Category(JUnitTests.class)
+public class SessionNotPopulatedExceptionTest extends Assert {
+
+    @Test
+    public void sessionNotPopulatedExceptionTest() {
+        SessionNotPopulatedException sessionNotPopulatedException = new SessionNotPopulatedException();
+
+        assertEquals("Expected and actual values should be the same.", RestApiErrorCodes.SESSION_NOT_POPULATED, sessionNotPopulatedException.getCode());
+        assertNull("Null expected.", sessionNotPopulatedException.getCause());
+        assertEquals("Expected and actual values should be the same.", "Error: ", sessionNotPopulatedException.getMessage());
+    }
+}  
---
+++ rest-api/core/src/test/java/org/eclipse/kapua/app/api/core/model/CountResultTest.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.app.api.core.model;
+
+import org.eclipse.kapua.qa.markers.junit.JUnitTests;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(JUnitTests.class)
+public class CountResultTest extends Assert {
+
+    @Test
+    public void countResultWithoutParameterTest() {
+        CountResult countResult = new CountResult();
+        assertEquals("Expected and actual values should be the same.", 0, countResult.getCount());
+    }
+
+    @Test
+    public void countResultWithParameterTest() {
+        long[] countValues = {-9223372036854775808L, -10000000L, -10, -1L, 0L, 1L, 10, 9223372036854775807L};
+
+        for (long countValue : countValues) {
+            CountResult countResult = new CountResult(countValue);
+            assertEquals("Expected and actual values should be the same.", countValue, countResult.getCount());
+        }
+    }
+
+    @Test
+    public void setAndGetCount() {
+        long[] countValues = {-9223372036854775808L, -10000000L, -10L, -1L, 0L, 1L, 10L, 9223372036854775807L};
+        CountResult countResult1 = new CountResult();
+        CountResult countResult2 = new CountResult(100L);
+
+        for (long countValue : countValues) {
+            countResult1.setCount(countValue);
+            countResult2.setCount(countValue);
+
+            assertEquals("Expected and actual values should be the same.", countValue, countResult1.getCount());
+            assertEquals("Expected and actual values should be the same.", countValue, countResult2.getCount());
+        }
+    }
+} 
