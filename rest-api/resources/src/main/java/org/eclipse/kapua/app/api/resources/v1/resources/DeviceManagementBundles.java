427713edbaa7f8ab62977437c8038831585a2945
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2021-07-06 18:00:23 +0200

    Add sortParam and sortDir on DeviceBundles simple query
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- rest-api/resources/src/main/java/org/eclipse/kapua/app/api/resources/v1/resources/DeviceManagementBundles.java
+++ rest-api/resources/src/main/java/org/eclipse/kapua/app/api/resources/v1/resources/DeviceManagementBundles.java
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package org.eclipse.kapua.app.api.resources.v1.resources;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -26,10 +27,14 @@ import org.eclipse.kapua.app.api.core.resources.AbstractKapuaResource;
 import org.eclipse.kapua.app.api.core.model.EntityId;
 import org.eclipse.kapua.app.api.core.model.ScopeId;
 import org.eclipse.kapua.locator.KapuaLocator;
+import org.eclipse.kapua.model.query.SortOrder;
 import org.eclipse.kapua.service.KapuaService;
+import org.eclipse.kapua.service.device.management.bundle.DeviceBundle;
 import org.eclipse.kapua.service.device.management.bundle.DeviceBundleManagementService;
 import org.eclipse.kapua.service.device.management.bundle.DeviceBundles;
 import org.eclipse.kapua.service.device.registry.Device;
+
+import com.google.common.base.Strings;
 
 @Path("{scopeId}/devices/{deviceId}/bundles")
 public class DeviceManagementBundles extends AbstractKapuaResource {
@@ -44,6 +49,10 @@ public class DeviceManagementBundles extends AbstractKapuaResource {
      *            The {@link ScopeId} of the {@link Device}.
      * @param deviceId
      *            The id of the device
+     * @param sortParam
+     *            The name of the parameter that will be used as a sorting key
+     * @param sortDir
+     *            The sort direction. Can be ASCENDING (default), DESCENDING. Case-insensitive.
      * @param timeout
      *            The timeout of the operation in milliseconds
      * @return The list of Bundles
@@ -56,8 +65,28 @@ public class DeviceManagementBundles extends AbstractKapuaResource {
     public DeviceBundles get(
             @PathParam("scopeId") ScopeId scopeId,
             @PathParam("deviceId") EntityId deviceId,
+            @QueryParam("sortParam") String sortParam,
+            @QueryParam("sortDir") @DefaultValue("ASCENDING") SortOrder sortDir,
             @QueryParam("timeout") Long timeout) throws KapuaException {
-        return bundleService.get(scopeId, deviceId, timeout);
+        DeviceBundles deviceBundles = bundleService.get(scopeId, deviceId, timeout);
+        if (!Strings.isNullOrEmpty(sortParam)) {
+            deviceBundles.getBundles().sort((b1, b2) -> sortBundles(sortParam, sortDir, b1, b2));
+        }
+        return deviceBundles;
+    }
+
+    private int sortBundles(String sortParam, SortOrder sortDir, DeviceBundle b1, DeviceBundle b2) {
+        switch(sortParam.toUpperCase()) {
+            default:
+            case "ID":
+                return (sortDir == SortOrder.DESCENDING ? (int) (b2.getId() - b1.getId()) : (int) (b1.getId() - b2.getId()));
+            case "NAME":
+                return (sortDir == SortOrder.DESCENDING ? b2.getName().compareToIgnoreCase(b1.getName()) : b1.getName().compareToIgnoreCase(b2.getName()));
+            case "STATE":
+                return (sortDir == SortOrder.DESCENDING ? b2.getState().compareToIgnoreCase(b1.getState()) : b1.getState().compareToIgnoreCase(b2.getState()));
+            case "VERSION":
+                return (sortDir == SortOrder.DESCENDING ? b2.getVersion().compareToIgnoreCase(b1.getVersion()) : b1.getVersion().compareToIgnoreCase(b2.getVersion()));
+        }
     }
 
     /**
