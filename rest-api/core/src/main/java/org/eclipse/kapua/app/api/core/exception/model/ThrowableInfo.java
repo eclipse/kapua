38fbb9c5d7ec9e661835ca9eda31da683d17fd30
Alberto Codutti <alberto.codutti@eurotech.com>
2021-09-29 17:07:05 +0200

    Refactored and added javadoc to ExceptionInfo and ThrowableInfo
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- rest-api/core/src/main/java/org/eclipse/kapua/app/api/core/exception/model/ThrowableInfo.java
+++ rest-api/core/src/main/java/org/eclipse/kapua/app/api/core/exception/model/ThrowableInfo.java
@@ -12,16 +12,18 @@
  *******************************************************************************/
 package org.eclipse.kapua.app.api.core.exception.model;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import org.eclipse.kapua.app.api.core.settings.KapuaApiCoreSetting;
+import org.eclipse.kapua.app.api.core.settings.KapuaApiCoreSettingKeys;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import org.eclipse.kapua.app.api.core.settings.KapuaApiCoreSetting;
-import org.eclipse.kapua.app.api.core.settings.KapuaApiCoreSettingKeys;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @XmlRootElement(name = "throwableInfo")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -38,14 +40,28 @@ public class ThrowableInfo {
     @XmlElement(name = "stackTrace")
     private String stackTrace;
 
+    /**
+     * Constructor.
+     *
+     * @since 1.0.0
+     */
     protected ThrowableInfo() {
         super();
     }
 
-    public ThrowableInfo(Status httpStatus, Throwable throwable) {
+    /**
+     * Constructor.
+     *
+     * @param httpStatus The {@link Status} of the {@link Response}
+     * @param throwable  The cause of the error.
+     * @since 1.0.0
+     */
+    public ThrowableInfo(@NotNull Status httpStatus, Throwable throwable) {
         this.httpErrorCode = httpStatus.getStatusCode();
+
         if (throwable != null) {
             this.message = throwable.getMessage();
+
             // Print stack trace
             if (SHOW_STACKTRACE) {
                 StringWriter stringWriter = new StringWriter();
@@ -55,26 +71,62 @@ public class ThrowableInfo {
         }
     }
 
+    /**
+     * Gets the {@link Status#getStatusCode()} of the {@link Response}.
+     *
+     * @return The {@link Status#getStatusCode()} of the {@link Response}.
+     * @since 1.0.0
+     */
     public int getHttpErrorCode() {
         return httpErrorCode;
     }
 
+    /**
+     * Sets the {@link Status#getStatusCode()} of the {@link Response}.
+     *
+     * @param httpErrorCode The {@link Status#getStatusCode()} of the {@link Response}.
+     * @since 1.0.0
+     */
     public void setHttpErrorCode(Status httpErrorCode) {
         this.httpErrorCode = httpErrorCode.getStatusCode();
     }
 
+    /**
+     * Gets the {@link Throwable#getMessage()}.
+     *
+     * @return The {@link Throwable#getMessage()}.
+     * @since 1.0.0
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets the {@link Throwable#getMessage()}.
+     *
+     * @param message The {@link Throwable#getMessage()}.
+     * @since 1.0.0
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Gets the {@link Throwable#getStackTrace()} if {@code api.exception.stacktrace.show} is {@code true}.
+     *
+     * @return The {@link Throwable#getStackTrace()} if {@code api.exception.stacktrace.show} is {@code true}.
+     * @since 1.0.0
+     */
     public String getStackTrace() {
         return stackTrace;
     }
 
+    /**
+     * Sets the {@link Throwable#getStackTrace()}.
+     *
+     * @param stackTrace The {@link Throwable#getStackTrace()}.
+     * @since 1.0.0
+     */
     private void setStackTrace(String stackTrace) {
         this.stackTrace = stackTrace;
     }
