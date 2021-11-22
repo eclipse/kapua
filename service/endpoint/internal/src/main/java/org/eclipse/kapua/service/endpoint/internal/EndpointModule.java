be4ef4ca6bc0fd52f39cbe751150b5164f4504ff
MDeLuise <massimiliano.deluise@eurotech.com>
2021-11-22 11:31:47 +0100

    decentralized Endpoint module

---
+++ service/endpoint/internal/src/main/java/org/eclipse/kapua/service/endpoint/internal/EndpointModule.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.service.endpoint.internal;
+
+import org.eclipse.kapua.commons.core.AbstractKapuaModule;
+import org.eclipse.kapua.service.endpoint.EndpointInfoFactory;
+import org.eclipse.kapua.service.endpoint.EndpointInfoService;
+
+public class EndpointModule extends AbstractKapuaModule {
+    @Override
+    protected void configureModule() {
+        bind(EndpointInfoFactory.class).to(EndpointInfoFactoryImpl.class);
+        bind(EndpointInfoService.class).to(EndpointInfoServiceImpl.class);
+    }
+}
