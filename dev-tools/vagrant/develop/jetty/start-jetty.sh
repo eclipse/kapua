924b235d00ccbf6bc895e727893d34b66b7019cf
Alberto Codutti <alberto.codutti@eurotech.com>
2022-02-17 10:17:20 +0100

    Updated copyright headers to 2022
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- dev-tools/vagrant/develop/jetty/start-jetty.sh
+++ dev-tools/vagrant/develop/jetty/start-jetty.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #*******************************************************************************
-# Copyright (c) 2018, 2021 Eurotech and/or its affiliates and others
+# Copyright (c) 2018, 2022 Eurotech and/or its affiliates and others
 #
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
--- dev-tools/vagrant/ssh.sh
+++ dev-tools/vagrant/ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #*******************************************************************************
-# Copyright (c) 2020, 2021 Eurotech and/or its affiliates
+# Copyright (c) 2020, 2022 Eurotech and/or its affiliates
 #
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
