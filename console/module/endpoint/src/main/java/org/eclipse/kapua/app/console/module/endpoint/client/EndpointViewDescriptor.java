4e986739f13b53c220c6463ebd2c45df8a51861b
MDeLuise <massimiliano.deluise@eurotech.com>
2022-06-22 14:35:05 +0200

    endpoint view available only for root account

--- console/module/endpoint/src/main/java/org/eclipse/kapua/app/console/module/endpoint/client/EndpointViewDescriptor.java
+++ console/module/endpoint/src/main/java/org/eclipse/kapua/app/console/module/endpoint/client/EndpointViewDescriptor.java
@@ -48,6 +48,6 @@ public class EndpointViewDescriptor extends AbstractEntityViewDescriptor<GwtEndp
 
     @Override
     public Boolean isEnabled(GwtSession currentSession) {
-        return currentSession.hasPermission(EndpointSessionPermission.readAll());
+        return currentSession.getRootAccountId().equals(currentSession.getSelectedAccountId());
     }
 }
