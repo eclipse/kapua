ef819f736af90e5d7eb6164460847dac31c730b1
dseurotech <davide.salvador@eurotech.com>
2022-11-03 17:51:44 +0100

    :ref: extracted a portion of userservice in a separate one in order to have an injectable object to be user by other services (via the AbstractKapuaConfigurableService superclass)

--- service/tag/test/src/test/java/org/eclipse/kapua/service/tag/test/TagLocatorConfiguration.java
+++ service/tag/test/src/test/java/org/eclipse/kapua/service/tag/test/TagLocatorConfiguration.java
@@ -54,7 +54,6 @@ import org.eclipse.kapua.service.tag.TagService;
 import org.eclipse.kapua.service.tag.internal.TagEntityManagerFactory;
 import org.eclipse.kapua.service.tag.internal.TagFactoryImpl;
 import org.eclipse.kapua.service.tag.internal.TagServiceImpl;
-import org.eclipse.kapua.service.user.UserService;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 
@@ -96,7 +95,7 @@ public class TagLocatorConfiguration {
                 bind(AccountFactory.class).toInstance(Mockito.spy(new AccountFactoryImpl()));
                 bind(RootUserTester.class).toInstance(Mockito.mock(RootUserTester.class));
 
-                bind(UserService.class).toInstance(Mockito.mock(UserService.class));
+                bind(RootUserTester.class).toInstance(Mockito.mock(RootUserTester.class));
 
                 // Inject actual Tag service related services
                 bind(TagEntityManagerFactory.class).toInstance(new TagEntityManagerFactory());
