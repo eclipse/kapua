0ea96c905033ba188d04f824b47e1616beb18fd9
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2022-11-23 10:51:57 +0100

    Add tests

--- service/device/registry/test-steps/src/main/java/org/eclipse/kapua/service/device/registry/steps/BrokerSteps.java
+++ service/device/registry/test-steps/src/main/java/org/eclipse/kapua/service/device/registry/steps/BrokerSteps.java
@@ -438,19 +438,14 @@ public class BrokerSteps extends TestBase {
     public void clientConnect(String clientName, String clientId, String user, String password) throws Exception {
         MqttClient mqttClient = null;
         MqttConnectOptions clientOpts = new MqttConnectOptions();
+        primeException();
         try {
-            mqttClient = new MqttClient(BROKER_URI, clientId,
-                    new MemoryPersistence());
-        } catch (MqttException e) {
-            logger.error("Error: {}", e.getMessage(), e);
-        }
-        clientOpts.setUserName(user);
-        clientOpts.setPassword(password.toCharArray());
-        try {
+            mqttClient = new MqttClient(BROKER_URI, clientId, new MemoryPersistence());
+            clientOpts.setUserName(user);
+            clientOpts.setPassword(password.toCharArray());
             mqttClient.connect(clientOpts);
         } catch (MqttException e) {
-            logger.error("Error: {}", e.getMessage(), e);
-            throw new Exception("Mqtt test client not connected.");
+            verifyException(e);
         }
         stepData.put(clientName, mqttClient);
     }
