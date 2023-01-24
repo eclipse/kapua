7b1e1624c786f0bd99b1f01ecdbed516b431c539
Alberto Codutti <alberto.codutti@eurotech.com>
2023-01-24 12:21:47 +0100

    Bumped Glassfish HK2 to 2.6.1
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- rest-api/web/src/main/java/org/eclipse/kapua/app/api/web/RestApisApplication.java
+++ rest-api/web/src/main/java/org/eclipse/kapua/app/api/web/RestApisApplication.java
@@ -12,28 +12,27 @@
  *******************************************************************************/
 package org.eclipse.kapua.app.api.web;
 
-import java.util.HashMap;
-
-import javax.ws.rs.core.MediaType;
-import javax.xml.bind.JAXBException;
-
 import org.eclipse.kapua.app.api.core.KapuaSerializableBodyWriter;
 import org.eclipse.kapua.app.api.core.ListBodyWriter;
 import org.eclipse.kapua.app.api.core.MoxyJsonConfigContextResolver;
 import org.eclipse.kapua.commons.util.xml.XmlUtil;
-
 import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.filter.UriConnegFilter;
 import org.glassfish.jersey.server.spi.Container;
 import org.glassfish.jersey.server.spi.ContainerLifecycleListener;
 
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBException;
+import java.util.HashMap;
+
 public class RestApisApplication extends ResourceConfig {
 
     public RestApisApplication() throws JAXBException {
         packages("org.eclipse.kapua.app.api",
-                 "org.eclipse.kapua.service");
+                "org.eclipse.kapua.service");
 
         // Bind media type to resource extension
         HashMap<String, MediaType> mappedMediaTypes = new HashMap<>();
@@ -42,18 +41,21 @@ public class RestApisApplication extends ResourceConfig {
 
         property(ServerProperties.MEDIA_TYPE_MAPPINGS, mappedMediaTypes);
         property(ServerProperties.WADL_FEATURE_DISABLE, true);
+
+        // Manually adding MOXyJSONFeature
+        register(MoxyJsonFeature.class);
+        register(MoxyJsonConfigContextResolver.class);
         register(UriConnegFilter.class);
         register(JaxbContextResolver.class);
         register(RestApiJAXBContextProvider.class);
         register(KapuaSerializableBodyWriter.class);
         register(ListBodyWriter.class);
-        register(MoxyJsonConfigContextResolver.class);
 
         register(new ContainerLifecycleListener() {
 
             @Override
             public void onStartup(Container container) {
-                ServiceLocator serviceLocator = container.getApplicationHandler().getServiceLocator();
+                ServiceLocator serviceLocator = container.getApplicationHandler().getInjectionManager().getInstance(ServiceLocator.class);
 
                 RestApiJAXBContextProvider provider = serviceLocator.createAndInitialize(RestApiJAXBContextProvider.class);
                 XmlUtil.setContextProvider(provider);
