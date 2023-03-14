e8db655e48b7849a428e40d8592bc39729289738
dseurotech <davide.salvador@eurotech.com>
2023-03-14 10:44:25 +0100

    query optimization
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

--- service/scheduler/api/src/main/java/org/eclipse/kapua/service/scheduler/trigger/TriggerRepository.java
+++ service/scheduler/api/src/main/java/org/eclipse/kapua/service/scheduler/trigger/TriggerRepository.java
@@ -12,12 +12,11 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.scheduler.trigger;
 
-import org.eclipse.kapua.KapuaException;
 import org.eclipse.kapua.model.id.KapuaId;
 import org.eclipse.kapua.storage.KapuaNamedEntityRepository;
 import org.eclipse.kapua.storage.TxContext;
 
 public interface TriggerRepository
         extends KapuaNamedEntityRepository<Trigger, TriggerListResult> {
-    void deleteAllByJobId(TxContext tx, KapuaId scopeId, KapuaId jobId) throws KapuaException;
+    void deleteAllByJobId(TxContext tx, KapuaId scopeId, KapuaId jobId);
 }
