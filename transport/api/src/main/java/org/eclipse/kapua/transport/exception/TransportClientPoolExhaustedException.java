ca868f8449ba8e39da86336d02d29546d2b82865
Alberto Codutti <alberto.codutti@eurotech.com>
2023-03-17 15:26:14 +0100

    Added TransportClientPoolExhaustedException at transport level
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

---
+++ transport/api/src/main/java/org/eclipse/kapua/transport/exception/TransportClientPoolExhaustedException.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2022 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.transport.exception;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * The {@link TransportException} to throw when is not possible to get an instance of the {@link org.eclipse.kapua.transport.TransportFacade} within the configured timeout
+ *
+ * @since 2.0.0
+ */
+public class TransportClientPoolExhaustedException extends TransportException {
+
+    private final String serverIp;
+    private final Long borrowWaitTimeout;
+
+    /**
+     * Constructor.
+     *
+     * @param serverIp The serverIp to which connect the {@link org.eclipse.kapua.transport.TransportFacade}
+     * @since 2.0.0
+     */
+    public TransportClientPoolExhaustedException(@NotNull String serverIp, Long borrowWaitTimeout) {
+        super(TransportErrorCodes.CLIENT_POOL_EXHAUSTED, serverIp, borrowWaitTimeout);
+
+        this.serverIp = serverIp;
+        this.borrowWaitTimeout = borrowWaitTimeout;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param cause    the root cause of the {@link Exception}.
+     * @param serverIp The serverIp to which connect the {@link org.eclipse.kapua.transport.TransportFacade}
+     * @since 2.0.0
+     */
+    public TransportClientPoolExhaustedException(@NotNull Throwable cause, @NotNull String serverIp, Long borrowWaitTimeout) {
+        super(TransportErrorCodes.CLIENT_POOL_EXHAUSTED, cause, serverIp, borrowWaitTimeout);
+
+        this.serverIp = serverIp;
+        this.borrowWaitTimeout = borrowWaitTimeout;
+    }
+
+    /**
+     * Gets the IP to which we wanted unsuccessfully to connect.
+     *
+     * @return The IP to which we wanted unsuccessfully to connect.
+     * @since 2.0.0
+     */
+    public String getRequestMessage() {
+        return serverIp;
+    }
+
+    /**
+     * Gets the configured timeout for the borrow operation.
+     *
+     * @return The configured timeout for the borrow operation.
+     * @since 2.0.0
+     */
+    public Long getBorrowWaitTimeout() {
+        return borrowWaitTimeout;
+    }
+}
--- transport/mqtt/src/main/java/org/eclipse/kapua/transport/mqtt/MqttClientFactoryImpl.java
+++ transport/mqtt/src/main/java/org/eclipse/kapua/transport/mqtt/MqttClientFactoryImpl.java
@@ -17,6 +17,7 @@ import org.eclipse.kapua.commons.setting.system.SystemSetting;
 import org.eclipse.kapua.commons.setting.system.SystemSettingKey;
 import org.eclipse.kapua.transport.TransportClientFactory;
 import org.eclipse.kapua.transport.exception.TransportClientGetException;
+import org.eclipse.kapua.transport.exception.TransportClientPoolExhaustedException;
 import org.eclipse.kapua.transport.message.mqtt.MqttMessage;
 import org.eclipse.kapua.transport.message.mqtt.MqttPayload;
 import org.eclipse.kapua.transport.message.mqtt.MqttTopic;
@@ -33,7 +34,7 @@ import java.util.Map;
 public class MqttClientFactoryImpl implements TransportClientFactory<MqttTopic, MqttPayload, MqttMessage, MqttMessage, MqttFacade, MqttClientConnectionOptions> {
 
     @Override
-    public MqttFacade getFacade(Map<String, Object> configParameters) throws TransportClientGetException {
+    public MqttFacade getFacade(Map<String, Object> configParameters) throws TransportClientGetException, TransportClientPoolExhaustedException {
         String host = (String) configParameters.get("serverAddress");
 
         if (Strings.isNullOrEmpty(host)) {
