3d068f6748fc364c9fb371c9789e88b10d92a98b
dseurotech <davide.salvador@eurotech.com>
2023-03-21 16:23:15 +0100

    just providing a silly in-memory implementation of basic repositories
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

---
+++ service/api/src/main/java/org/eclipse/kapua/storage/KapuaNamedEntityInMemoryRepository.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2022 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.storage;
+
+import org.eclipse.kapua.model.KapuaNamedEntity;
+import org.eclipse.kapua.model.id.KapuaId;
+import org.eclipse.kapua.model.query.KapuaListResult;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class KapuaNamedEntityInMemoryRepository<E extends KapuaNamedEntity, L extends KapuaListResult<E>>
+        extends KapuaUpdatableEntityInMemoryRepository<E, L>
+        implements KapuaNamedEntityRepository<E, L> {
+    public KapuaNamedEntityInMemoryRepository(Class<E> clazz, Supplier<L> listSupplier) {
+        super(clazz, listSupplier);
+    }
+
+    @Override
+    public Optional<E> findByName(TxContext txContext, String value) {
+        if (value == null) {
+            return Optional.empty();
+        }
+        return this.entities
+                .stream()
+                .filter(e -> value.equals(e.getName()))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<E> findByName(TxContext txContext, KapuaId scopeId, String value) {
+        if (value == null) {
+            return Optional.empty();
+        }
+        return this.entities
+                .stream()
+                .filter(e -> value.equals(e.getName()))
+                .filter(e -> scopeId == null || KapuaId.ANY.equals(scopeId) ? true : scopeId.equals(e.getScopeId()))
+                .findFirst();
+    }
+}
---
+++ service/api/src/main/java/org/eclipse/kapua/storage/KapuaUpdatableEntityInMemoryRepository.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2022 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.storage;
+
+import org.eclipse.kapua.KapuaException;
+import org.eclipse.kapua.model.KapuaUpdatableEntity;
+import org.eclipse.kapua.model.query.KapuaListResult;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class KapuaUpdatableEntityInMemoryRepository<E extends KapuaUpdatableEntity, L extends KapuaListResult<E>>
+        extends KapuaEntityInMemoryRepository<E, L>
+        implements KapuaUpdatableEntityRepository<E, L> {
+
+    public KapuaUpdatableEntityInMemoryRepository(Class<E> clazz, Supplier<L> listSupplier) {
+        super(clazz, listSupplier);
+    }
+
+    @Override
+    public E update(TxContext txContext, E entity) throws KapuaException {
+        final Optional<E> current = doFind(entity.getScopeId(), entity.getId());
+        current.ifPresent(e -> this.entities.remove(e));
+        this.entities.add(entity);
+        return entity;
+    }
+
+    @Override
+    public E update(TxContext txContext, E currentEntity, E updatedEntity) {
+        this.entities.remove(currentEntity);
+        this.entities.add(updatedEntity);
+        return updatedEntity;
+    }
+}
