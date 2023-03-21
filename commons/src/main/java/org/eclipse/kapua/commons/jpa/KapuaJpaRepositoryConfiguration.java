a20cba5fd114ace4389a2aff4bdf92da418c21d7
dseurotech <davide.salvador@eurotech.com>
2023-03-21 16:50:35 +0100

    :enh: documenting some repository aspects

--- commons/src/main/java/org/eclipse/kapua/commons/jpa/KapuaJpaRepositoryConfiguration.java
+++ commons/src/main/java/org/eclipse/kapua/commons/jpa/KapuaJpaRepositoryConfiguration.java
@@ -13,6 +13,14 @@
 package org.eclipse.kapua.commons.jpa;
 
 public class KapuaJpaRepositoryConfiguration {
+    /**
+     * Customization constructor for this class. Allows to fine-tune RDBMS-specific configurations
+     *
+     * @param escape                Escape sequence used by the specific SQL dialect of the used RDBMS
+     * @param like                  "like" operator, {@literal %} in most dialects
+     * @param any                   "any character" operator,  {@literal _} in most dialects
+     * @param maxInsertAllowedRetry Number of allowed retries in case of a key collision when persisting a new entity for the first time
+     */
     public KapuaJpaRepositoryConfiguration(String escape, String like, String any, int maxInsertAllowedRetry) {
         this.escape = escape;
         this.like = like;
@@ -20,6 +28,16 @@ public class KapuaJpaRepositoryConfiguration {
         this.maxInsertAllowedRetry = maxInsertAllowedRetry;
     }
 
+    /**
+     * Default constructor. The following values are used:
+     *
+     * <ul>
+     * <li><b>escape sequence:</b> {@literal \\}</li>
+     * <li><b>like operator:</b> {@literal %}</li>
+     * <li><b>any character operator:</b> {@literal _}</li>
+     * <li><b>max number of retries on inserts:</b> 3</li>
+     * </ul>
+     */
     public KapuaJpaRepositoryConfiguration() {
         this.escape = "\\";
         this.like = "%";
