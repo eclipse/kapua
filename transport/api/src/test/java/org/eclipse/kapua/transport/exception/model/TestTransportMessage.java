68690854e18fdd253644cc8cbddb06742b60eda2
Alberto Codutti <alberto.codutti@eurotech.com>
2023-04-03 16:19:25 +0200

    Refactored TransportExceptionTest
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

---
+++ transport/api/src/test/java/org/eclipse/kapua/transport/exception/model/TestTransportMessage.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2023, 2022 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Eurotech - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kapua.transport.exception.model;
+
+import org.eclipse.kapua.transport.message.TransportMessage;
+
+/**
+ * {@link TransportMessage} for testing.
+ *
+ * @since 2.0.0
+ */
+public class TestTransportMessage implements TransportMessage {
+}
