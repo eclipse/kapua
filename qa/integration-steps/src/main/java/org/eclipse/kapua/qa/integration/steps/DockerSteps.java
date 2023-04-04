c009b7eacea4e4bdde3fb922fd0905107720f291
riccardomodanese <riccardo.modanese@eurotech.com>
2023-04-04 11:41:47 +0200

    Artemis - restore 2.0.0-SNAPSHOT version
    Signed-off-by: riccardomodanese <riccardo.modanese@eurotech.com>

--- qa/integration-steps/src/main/java/org/eclipse/kapua/qa/integration/steps/DockerSteps.java
+++ qa/integration-steps/src/main/java/org/eclipse/kapua/qa/integration/steps/DockerSteps.java
@@ -66,7 +66,7 @@ public class DockerSteps {
     private static final Logger logger = LoggerFactory.getLogger(DockerSteps.class);
 
     private static final String NETWORK_PREFIX = "kapua-net";
-    private static final String KAPUA_VERSION = "2.0.0-ARTEMIS-SNAPSHOT";
+    private static final String KAPUA_VERSION = "2.0.0-SNAPSHOT";
     private static final String ES_IMAGE = "elasticsearch:7.8.1";
     private static final String BROKER_IMAGE = "kapua-broker-artemis";
     private static final String LIFECYCLE_CONSUMER_IMAGE = "kapua-consumer-lifecycle";
