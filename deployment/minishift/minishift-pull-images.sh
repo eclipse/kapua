8b8ea30d39ac60d8d7bb50db34a024618698f57e
angelo.andreussi <angelo.andreussi@eurotech.com>
2023-05-22 11:57:20 +0200

    changed IMAGE_VERSION env. variable default value

--- deployment/minishift/minishift-pull-images.sh
+++ deployment/minishift/minishift-pull-images.sh
@@ -17,7 +17,7 @@
 #
 
 DOCKER_ACCOUNT=${DOCKER_ACCOUNT:=kapua}
-IMAGE_VERSION=${IMAGE_VERSION:=latest}
+IMAGE_VERSION=${IMAGE_VERSION:=2.0.0-SNAPSHOT}
 SERVICES=("console" "api" "sql" "broker" "events-broker")
 
 echo "Pulling Kapua images..."
