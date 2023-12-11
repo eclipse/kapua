e610d218c5d9313094c22541eaf91d4cc2bf24f3
Eclipse Kapua committers <kapua-dev@eclipse.org>
2023-12-11 16:04:50 +0000

    [maven-release-plugin] prepare release 1.6.10

--- deployment/minishift/minishift-importer.sh
+++ deployment/minishift/minishift-importer.sh
@@ -29,7 +29,7 @@
 
 ERROR=0
 DOCKER_ACCOUNT=${DOCKER_ACCOUNT:=kapua}
-IMAGE_VERSION=${IMAGE_VERSION:=1.6.10-SNAPSHOT}
+IMAGE_VERSION=${IMAGE_VERSION:=1.6.10}
 SERVICES=("console" "api" "sql" "broker" "events-broker")
 TMP_DIR="/tmp/kapua-containers-$(date +%s)"
 
