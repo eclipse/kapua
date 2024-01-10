fb3d6230f1566de17ad0bd4322302316fe29462e
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2024-01-10 14:19:40 +0100

    fix(api)!: device configuration put now is encoded and decoded correctly

--- service/api/src/main/java/org/eclipse/kapua/service/config/ServiceConfiguration.java
+++ service/api/src/main/java/org/eclipse/kapua/service/config/ServiceConfiguration.java
@@ -26,7 +26,7 @@ import org.eclipse.kapua.KapuaSerializable;
  *
  * @since 1.0
  */
-@XmlRootElement(name = "configurations")
+@XmlRootElement(name = "serviceConfigurations")
 @XmlAccessorType(XmlAccessType.PROPERTY)
 @XmlType(factoryClass = ServiceConfigurationXmlRegistry.class, factoryMethod = "newConfiguration")
 public interface ServiceConfiguration extends KapuaSerializable {
