af858b7cdcb5cd478b5f836853436bf7a1350f8f
Alberto Codutti <alberto.codutti@eurotech.com>
2024-01-12 10:04:19 +0100

    fix(console): fixed error reporting for DeviceAssetChannel
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/server/GwtDeviceAssetServiceImpl.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/server/GwtDeviceAssetServiceImpl.java
@@ -29,7 +29,6 @@ import org.eclipse.kapua.locator.KapuaLocator;
 import org.eclipse.kapua.model.id.KapuaId;
 import org.eclipse.kapua.service.device.management.asset.DeviceAsset;
 import org.eclipse.kapua.service.device.management.asset.DeviceAssetChannel;
-import org.eclipse.kapua.service.device.management.asset.DeviceAssetChannelMode;
 import org.eclipse.kapua.service.device.management.asset.DeviceAssetManagementService;
 import org.eclipse.kapua.service.device.management.asset.DeviceAssets;
 import org.eclipse.kapua.service.device.management.asset.store.DeviceAssetStoreFactory;
@@ -88,12 +87,11 @@ public class GwtDeviceAssetServiceImpl extends KapuaRemoteServiceServlet impleme
                 DeviceAsset assetMetadata = assetsMetadata.getAssets().get(assetIndex);
                 DeviceAsset assetValues = assetsValues.getAssets().get(assetIndex);
                 for (DeviceAssetChannel channelMetadata : assetMetadata.getChannels()) {
-                    if (channelMetadata.getMode().equals(DeviceAssetChannelMode.READ) || channelMetadata.getMode().equals(DeviceAssetChannelMode.READ_WRITE)) {
-                        for (DeviceAssetChannel channelValue : assetValues.getChannels()) {
-                            if (channelValue.getName().equals(channelMetadata.getName())) {
-                                channelMetadata.setValue(channelValue.getValue());
-                                break;
-                            }
+                    for (DeviceAssetChannel channelValue : assetValues.getChannels()) {
+                        if (channelValue.getName().equals(channelMetadata.getName())) {
+                            channelMetadata.setValue(channelValue.getValue());
+                            channelMetadata.setError(channelValue.getError());
+                            break;
                         }
                     }
                 }
@@ -101,14 +99,14 @@ public class GwtDeviceAssetServiceImpl extends KapuaRemoteServiceServlet impleme
 
             gwtAssets = KapuaGwtDeviceModelConverter.convertDeviceAssets(assetsMetadata);
         } catch (Throwable t) {
-            KapuaExceptionHandler.handle(t);
+            throw KapuaExceptionHandler.buildExceptionFromError(t);
         }
         return gwtAssets.getAssets();
     }
 
     //
     // Asset Store Settings
-    //
+    @Override
     public boolean isStoreServiceEnabled(String scopeIdString, String deviceIdString) throws GwtKapuaException {
         try {
             KapuaId scopeId = GwtKapuaCommonsModelConverter.convertKapuaId(scopeIdString);
