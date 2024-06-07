56adf32e99ae60cfc617fd23be50a9d23594f326
Alberto Codutti <alberto.codutti@eurotech.com>
2024-06-07 16:23:36 +0200

    [maven-release-plugin] prepare release 1.6.11
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- deployment/minishift/minishift-importer.sh
+++ deployment/minishift/minishift-importer.sh
@@ -29,7 +29,7 @@
 
 ERROR=0
 DOCKER_ACCOUNT=${DOCKER_ACCOUNT:=kapua}
-IMAGE_VERSION=${IMAGE_VERSION:=1.6.11-SNAPSHOT}
+IMAGE_VERSION=${IMAGE_VERSION:=1.6.11}
 SERVICES=("console" "api" "sql" "broker" "events-broker")
 TMP_DIR="/tmp/kapua-containers-$(date +%s)"
 
