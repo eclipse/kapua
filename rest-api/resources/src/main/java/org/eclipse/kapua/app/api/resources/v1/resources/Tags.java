3dfb200d94ca98392e9861715053b238143520fe
angelo.andreussi <angelo.andreussi@eurotech.com>
2024-07-08 15:23:49 +0200

    feature: added "askTotalCount" parameter for tags api

--- rest-api/resources/src/main/java/org/eclipse/kapua/app/api/resources/v1/resources/Tags.java
+++ rest-api/resources/src/main/java/org/eclipse/kapua/app/api/resources/v1/resources/Tags.java
@@ -67,6 +67,7 @@ public class Tags extends AbstractKapuaResource {
     public TagListResult simpleQuery(
             @PathParam("scopeId") ScopeId scopeId,
             @QueryParam("name") String name,
+            @QueryParam("askTotalCount") boolean askTotalCount,
             @QueryParam("offset") @DefaultValue("0") int offset,
             @QueryParam("limit") @DefaultValue("50") int limit) throws KapuaException {
         TagQuery query = tagFactory.newQuery(scopeId);
@@ -79,6 +80,7 @@ public class Tags extends AbstractKapuaResource {
 
         query.setOffset(offset);
         query.setLimit(limit);
+        query.setAskTotalCount(askTotalCount);
 
         return query(scopeId, query);
     }
