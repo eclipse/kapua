32356c94d8ff6693168c17c276011dc0a96a7379
dseurotech <davide.salvador@eurotech.com>
2024-07-08 10:35:15 +0200

    :bug: current account update was not checking for not null organisation name or email
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

--- service/account/internal/src/main/java/org/eclipse/kapua/service/account/internal/AccountMapper.java
+++ service/account/internal/src/main/java/org/eclipse/kapua/service/account/internal/AccountMapper.java
@@ -13,15 +13,19 @@
 package org.eclipse.kapua.service.account.internal;
 
 import org.eclipse.kapua.commons.model.mappers.KapuaBaseMapper;
+import org.eclipse.kapua.locator.KapuaLocator;
 import org.eclipse.kapua.service.account.Account;
+import org.eclipse.kapua.service.account.AccountFactory;
 import org.eclipse.kapua.service.account.AccountUpdateRequest;
 import org.eclipse.kapua.service.account.CurrentAccountUpdateRequest;
+import org.eclipse.kapua.service.account.Organization;
 import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(uses = KapuaBaseMapper.class, componentModel = MappingConstants.ComponentModel.JSR330, injectionStrategy = InjectionStrategy.CONSTRUCTOR, collectionMappingStrategy = CollectionMappingStrategy.TARGET_IMMUTABLE)
 public interface AccountMapper {
@@ -36,6 +40,14 @@ public interface AccountMapper {
     @Mapping(target = "parentAccountPath", ignore = true)
     @Mapping(target = "childAccounts", ignore = true)
     void merge(@MappingTarget Account account, AccountUpdateRequest request);
+
+    default Organization createOrganisation() {
+        return KapuaLocator.getInstance().getComponent(AccountFactory.class).newOrganization();
+    }
+
+    @Mapping(target = "name", source = "name", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "email", source = "email", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void merge(@MappingTarget Organization account, Organization request);
 
     //For backward compatibility only
     AccountUpdateRequest mapChildUpdate(Account account);
