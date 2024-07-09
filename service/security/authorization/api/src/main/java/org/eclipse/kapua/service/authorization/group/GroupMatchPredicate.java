3a25325cd1f9e6f901b9a37809eba48bba24e469
angelo.andreussi <angelo.andreussi@eurotech.com>
2024-07-09 11:57:03 +0200

    feature: introduced matchTerm for groups

--- service/security/authorization/api/src/main/java/org/eclipse/kapua/service/authorization/group/GroupMatchPredicate.java
+++ service/security/authorization/api/src/main/java/org/eclipse/kapua/service/authorization/group/GroupMatchPredicate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2022 Eurotech and/or its affiliates and others
+ * Copyright (c) 2017, 2022 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -12,16 +12,8 @@
  *******************************************************************************/
 package org.eclipse.kapua.service.authorization.group;
 
-import org.eclipse.kapua.model.KapuaNamedEntityAttributes;
+import org.eclipse.kapua.model.query.predicate.MatchPredicate;
 
-/**
- * {@link GroupAttributes} attributes.
- *
- * @see org.eclipse.kapua.model.KapuaEntityAttributes
- * @since 1.0.0
- */
-public class GroupAttributes extends KapuaNamedEntityAttributes {
+public interface GroupMatchPredicate<T> extends MatchPredicate<T> {
 
-    public static final String DESCRIPTION = "description";
-    public static final String NAME = "name";
 }
