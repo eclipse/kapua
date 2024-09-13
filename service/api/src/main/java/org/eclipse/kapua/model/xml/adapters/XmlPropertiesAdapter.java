be257b651dbe04491d8898fccdcec2c7fab80086
angelo.andreussi <angelo.andreussi@eurotech.com>
2024-09-13 16:28:45 +0200

    fix: first fix to alert when "type" param is missing in a service/device property

--- service/api/src/main/java/org/eclipse/kapua/model/xml/adapters/XmlPropertiesAdapter.java
+++ service/api/src/main/java/org/eclipse/kapua/model/xml/adapters/XmlPropertiesAdapter.java
@@ -11,14 +11,12 @@
  *     Eurotech - initial API and implementation
  *******************************************************************************/
 package org.eclipse.kapua.model.xml.adapters;
-
 import org.eclipse.kapua.model.xml.XmlPropertyAdapted;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -38,45 +36,26 @@ public class XmlPropertiesAdapter<T extends Enum<T>, V extends XmlPropertyAdapte
 
     @Override
     public Map<String, Object> unmarshal(V[] properties) {
-        Map<String, Object> unmarshalledProperties = new HashMap<>();
-
-         Optional.ofNullable(properties)
+        if (properties != null) {
+            for (V prop : properties) {
+                if (prop.getType() == null) {
+                    throw new InternalError("null value for property.type parameter");
+                }
+            }
+        }
+        Map<String, Object> unmarshalledProperties;
+        unmarshalledProperties = Optional.ofNullable(properties)
                 .map(Arrays::asList)
                 .orElse(Collections.emptyList())
                 .stream()
-                .filter(adaptedProp -> adaptedProp.getType() == null || xmlPropertyAdapters.containsKey((adaptedProp.getType())))
-                .forEach(adaptedProp -> unmarshalledProperties.put(adaptedProp.getName(), unmarshalProperty(adaptedProp)));
-
-        // This was a better way to do it, but Collectors.toMap does not accept `null` values.
-        // See Collectors#153 for reference.
-        //                .collect(Collectors.toMap(
-        //                        adaptedProp -> adaptedProp.getName(),
-        //                        adaptedProp -> {
-        //                            final XmlPropertyAdapter xmlPropertyAdapter = xmlPropertyAdapters.get(adaptedProp.getType());
-        //                            return xmlPropertyAdapter.unmarshallValues(adaptedProp);
-        //                        }));
+                .collect(Collectors.toMap(
+                        XmlPropertyAdapted::getName,
+                        adaptedProp -> {
+                            final XmlPropertyAdapter xmlPropertyAdapter = xmlPropertyAdapters.get(adaptedProp.getType());
+                            return xmlPropertyAdapter.unmarshallValues(adaptedProp);
+                        }));
 
         return unmarshalledProperties;
-    }
-
-    /**
-     * It unmarshal the given {@link XmlPropertyAdapted}.
-     * <p>
-     * If {@link XmlPropertyAdapted#getType()} is {@code null} it means that {@link XmlPropertyAdapted#getValues()} was {@code null}, so we can't determine its {@link XmlPropertyAdapted#getType()}.
-     * By the way, if original value was {@code null} we can safely return {@code null}.
-     *
-     * @param adaptedProp The {@link XmlPropertyAdapted} to unmarshal
-     * @return The unmarshalled {@link XmlPropertyAdapted}
-     * @since 2.1.0
-     */
-    public Object unmarshalProperty(V adaptedProp) {
-        if (adaptedProp.getType() != null) {
-            XmlPropertyAdapter xmlPropertyAdapter = xmlPropertyAdapters.get(adaptedProp.getType());
-            return xmlPropertyAdapter.unmarshallValues(adaptedProp);
-        }
-        else {
-            return null;
-        }
     }
 
     @Override
