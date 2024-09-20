d73d615dec51067d9261e0c4155f66f904e60df8
Alberto Codutti <alberto.codutti@eurotech.com>
2024-09-20 19:22:13 +0200

    :bookmark: Set modules version to 2.0.0-BETA-M10
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- deployment/minishift/minishift-pull-images.sh
+++ deployment/minishift/minishift-pull-images.sh
@@ -17,7 +17,7 @@
 #
 
 DOCKER_ACCOUNT=${DOCKER_ACCOUNT:=kapua}
-IMAGE_VERSION=${IMAGE_VERSION:=2.0.0-SNAPSHOT}
+IMAGE_VERSION=${IMAGE_VERSION:=2.0.0-BETA-M10}
 SERVICES=("console" "api" "sql" "broker" "events-broker")
 
 echo "Pulling Kapua images..."
